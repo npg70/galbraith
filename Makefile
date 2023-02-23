acd442dc9a8b4306934c4bc8b73a9503ff837c82
nickg <nickg@client9.com>
2023-02-23 12:13:46 -0800

    compute roots automatically

--- Makefile
+++ Makefile
@@ -6,10 +6,13 @@ hugo/content/people:
 
 build: hugo/content/people
 	go run . -out hugo/content/people galbreath-james-1659-nielson
-	go run . -out hugo/content/people galbreath-donald-1793-bell
 
 serve:
 	(cd hugo && hugo server -D)
 clean:
 	rm -rf hugo/public/*
 	rm -rf hugo/content/people/*
+
+roots:
+	go run . -root
+
--- main.go
+++ main.go
@@ -9,91 +9,119 @@ import (
 	"strings"
 )
 
+func computeRoots() []string {
+	db := make(Root)
+	pmap := make(map[string]bool)
+	files, err := filepath.Glob("./people/*.sh")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, f := range files {
+		uid := strings.TrimSuffix(filepath.Base(f), ".sh")
+		_, nextg := db.generateOne(uid)
+		for _, child := range nextg {
+			pmap[child] = true
+		}
+		if _, ok := pmap[uid]; !ok {
+			pmap[uid] = false
+		}
+	}
+
+	out := []string{}
+	for k, v := range pmap {
+		if !v {
+			out = append(out, k)
+		}
+	}
+	return out
+}
 func init() {
 	// flag stuff
 }
 func main() {
 	outdir := ""
+	rootsOnly := false
 	flag.StringVar(&outdir, "out", "", "out directory")
+	flag.BoolVar(&rootsOnly, "root", false, "run and display roots")
 	flag.Parse()
-	args := flag.Args()
-	if len(args) == 0 {
-		log.Fatalf("Expected UID as first arg")
-	}
 
-	// root is the full path and suffix
-	root := args[0]
-	// remove any directory, and remove trailing suffix
-	rootid := strings.TrimSuffix(filepath.Base(root), ".sh")
-
-	db := make(Root)
-	kidsq := []string{rootid}
-	db.LoadAll(rootid)
-	suffix := ".html"
-	singlePage := false
-
+	// soon to be obsolete
 	log.Printf("Loading OPR Database")
 	oprdb, err := LoadOPRBaptisms()
 	if err != nil {
 		log.Fatalf("can't load opr baptism", err)
 	}
-	doc := ""
 
-	if singlePage {
+	db := make(Root)
+	roots := computeRoots()
+	if rootsOnly {
+		return
+	}
+	for _, rootid := range roots {
+		log.Printf("ROOT---------> %s", rootid)
+		kidsq := []string{rootid}
+		db.LoadAll(rootid)
+		suffix := ".html"
+		singlePage := false
+
+		doc := ""
+
+		if singlePage {
+			for len(kidsq) > 0 {
+				uid := kidsq[0]
+				kidsq = kidsq[1:]
+				intermediate, nextg := db.generateOne(uid)
+				kidsq = append(kidsq, nextg...)
+				if len(kidsq) == 0 {
+					break
+				}
+				doc += intermediate
+			}
+
+			p := Tokenizer{}
+			root := p.Parse(strings.NewReader(doc))
+
+			if err := footnoter(root); err != nil {
+				log.Fatalf("Footnoter failed: %s", err)
+			}
+
+			out := emitHTMLHugo(root, oprdb, singlePage)
+			if outdir != "" {
+				fullpath := filepath.Join(outdir, "test"+suffix)
+				log.Printf("Writing %q", fullpath)
+				err := os.WriteFile(fullpath, []byte(out), 0666)
+				if err != nil {
+					log.Fatalf("couldn't write %q: %s", fullpath, err)
+				}
+			}
+			return
+		}
+
+		// one person, one page
 		for len(kidsq) > 0 {
 			uid := kidsq[0]
 			kidsq = kidsq[1:]
-			intermediate, nextg := db.generateOne(uid)
+			doc, nextg := db.generateOne(uid)
 			kidsq = append(kidsq, nextg...)
-			if len(kidsq) == 0 {
-				break
+
+			//		fmt.Println(doc)
+			log.Printf("Processing %s", uid)
+			p := Tokenizer{}
+			root := p.Parse(strings.NewReader(doc))
+			if err := footnoter(root); err != nil {
+				log.Fatalf("Footnoter failed: %s", err)
 			}
-			doc += intermediate
-		}
 
-		p := Tokenizer{}
-		root := p.Parse(strings.NewReader(doc))
+			out := emitHTMLHugo(root, oprdb, singlePage)
+			if outdir == "" {
+				fmt.Println(out)
+				continue
+			}
 
-		if err := footnoter(root); err != nil {
-			log.Fatalf("Footnoter failed: %s", err)
-		}
-
-		out := emitHTMLHugo(root, oprdb, singlePage)
-		if outdir != "" {
-			fullpath := filepath.Join(outdir, "test"+suffix)
-			log.Printf("Writing %q", fullpath)
-			err := os.WriteFile(fullpath, []byte(out), 0666)
-			if err != nil {
+			fullpath := filepath.Join(outdir, uid+suffix)
+			if err := os.WriteFile(fullpath, []byte(out), 0666); err != nil {
 				log.Fatalf("couldn't write %q: %s", fullpath, err)
 			}
-		}
-		return
-	}
-
-	// one person, one page
-	for len(kidsq) > 0 {
-		uid := kidsq[0]
-		kidsq = kidsq[1:]
-		doc, nextg := db.generateOne(uid)
-		kidsq = append(kidsq, nextg...)
-
-		//		fmt.Println(doc)
-		log.Printf("Processing %s", uid)
-		p := Tokenizer{}
-		root := p.Parse(strings.NewReader(doc))
-		if err := footnoter(root); err != nil {
-			log.Fatalf("Footnoter failed: %s", err)
-		}
-
-		out := emitHTMLHugo(root, oprdb, singlePage)
-		if outdir == "" {
-			fmt.Println(out)
-			continue
-		}
-
-		fullpath := filepath.Join(outdir, uid+suffix)
-		if err := os.WriteFile(fullpath, []byte(out), 0666); err != nil {
-			log.Fatalf("couldn't write %q: %s", fullpath, err)
 		}
 	}
 }
