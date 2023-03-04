9eb35b963db45d8f4bf41076190edbd7e4a60643
nickg <nickg@client9.com>
2023-03-03 23:03:59 -0800

    add line

---
+++ people/galbraith-archibald-1800-wilkinson.sh
@@ -0,0 +1,136 @@
+name Archibald Galbraith
+baptism -date 29-oct-1800 -location 'killean and kilchenzie' -ref archibald1800-baptism
+death -date 13-feb-1855 -location campbeltown -ref archibald1800-death
+partner {
+    name Barbara Wilkinson
+    marriage -date 1827 -location 'killean and kilchenzie'
+    death -date 13-mar-1886 -location 'mid calder, midlothian' -ref barbara-death 
+    body {
+        Maybe the daughted of Duncan McQuilkan and Margaret McMath, baptized 6 Jun 1799 in Killean and Kilcehnzie.
+    }
+    child {
+        name Duncan Galbreath
+        baptism -date 17-jan-1828 -location 'killean and kilchenzie' -ref duncan1828-baptism
+    }
+    child {
+        name Archibald Galbreath
+        baptism -date 29-apr-1830 -location campbeltown -ref archibald1830-baptism
+        death -date 1860 -location Kilmarnock,Ayr -ref archibald1830-death 
+        body {
+            death can't be correct, as another son archibald was born in 1839
+        }
+    }
+    child {
+        name Alexander Galbreath
+        baptism -date 11-Nov-1832 -location campbeltown -ref alexander1832-baptism
+        death -date 3-nov-1875 -location denningstoun,lanark -ref alexander1832-death
+        partner {
+            name Margaret McArthur
+        }
+        body {
+            Died to fractured ribs "due to violence." Marriage may be incorrect.
+        }
+    }
+    child {
+        name Angus Galbreath
+        baptism -date 14-Feb-1835 -location campbeltown -ref angus1835-baptism
+    }
+    child {
+        name Margaret Galbraith
+        baptism -date 6-Jul-1837 -location campbeltown -ref margaret1837-baptism
+        partner {
+            name William Wilson
+        }
+    }
+    child {
+        name Archibald Galbreath
+        baptism -date 22-Aug-1839 -location campbeltown -ref archibald1839-baptism
+        death -date 1857 -location 'milton,glasgow' -ref archibald1839-death
+    }
+    child {
+        name Angus Galbreath
+        baptism -date 5-Dec-1841 -location campebeltown -ref angus1841-baptism
+        death -date 1876 -location 'mid calder, midlothian' -ref angus1841-death
+    }
+}
+body {
+   $p{
+    Barbara may have been name McQuilkan, which is somewhat similar to Wilkinson.
+    Perhaps an Angelized version.
+
+    maybe parents:
+    MCQUILKAN
+DUNCAN
+MARGARET MCMATH/FR351 (FR351)
+06/06/1799
+519
+10 / 341
+KILLEAN AND KILCHENZIE
+    }
+    $ul{
+        $li{1851 Archibald listed as Mashman}
+        $li{1861 - Barbra https://www.findmypast.com/transcript?id=GBC%2F1861%2F0023675920}
+        $li{1871 - Barbra https://www.findmypast.com/transcript?id=GBC%2F1871%2F0024730641}
+    }
+}
+footnotes {
+    archibald1800-birth {
+        $opr-ref-link[b-1800-519-000-0010-0148 "Archibald Galbreath"]
+    }
+    archibald1800-death {
+        $sp-ref-link[d-1855-507-01-0011 0004 "Archibald Galbraith"]{
+        11. Archibald Galbraith, Mashman
+        Death: 1855 February Thirdteeth, Age 56
+        Father: Duncan Galbraith, Laboroer;
+        Mother: Mary Galbraith, Maiden Name Kelly;
+        Spouse: Barbara Galbraith, Maiden Name Wilkenson;
+        Present: Alex Galbraith, son;
+        $i{list of children, other details}
+        }
+    }
+    barbara-death {
+        $sp-ref-link[d-1886-694-00-0010 0004 "Barbara Galbraith"]{
+        10. Barbara Galbraith, Widow of Archibald Galbraith, Brewer;
+        Death: 1886 March Thirdteenth, age 86 [$i{birth 1800}];
+        Father: Duncan Wilkinson, farmer, deceased;
+        Mother: Margaret Wilkison, M.S. Mathieson, deceased;
+        Present: Jane Galbraith, daughter in law;
+        }
+    }
+    duncan1828-baptism {
+        $opr-ref[b-1828-519-000-0020-0066 "Duncan Galbreath"]
+        Mother idnexed as  "Barbra McCuilkan"
+    }
+    archibald1830-baptism {
+        $opr-ref-link[b-1830-507-000-0060-0203 "Archibald Galbreath"]
+        Mother indexed as "Barbra McQuilkan"
+    }
+    archibald1830-death {
+        $sp-ref-link[d-1860-597-00-0051 0017 "Archibald Galbraith"]
+    }
+    alexander1832-baptism {
+        $opr-ref-link[b-1832-507-000-0070-0018 "Alexander Galbreath"]
+    }
+    alexander1832-death {
+        $sp-ref-link[d-1875-644-03-1699 0567 "Alexander Galbraith"]
+        correction present.. unclear what changed.
+    }
+    angus1835-baptism {
+        $opr-ref[b-1835-507-000-0070-0057 "Angus Galbreath"]
+    }
+    margaret1837-baptism {
+        $opr-ref-link[b-1837-507-000-0070-0105 "Margaret Galbreath"]
+    }
+    archibald1839-baptism {
+        $opr-ref-link[b-1839-507-000-0070-0154 "Archibald Galbreath"]
+    }
+    archibald1839-death {
+        $sp-ref[d-1857-644-07-0338 "Archibald Galbraith"]
+    }
+    angus1841-baptism {
+        $opr-ref-link[b-1841-507-000-0070-0209 "Angus Galbreath"]
+    }
+    angus1841-death {
+        $sp-ref[d-1876-694-00-0024 "Angus Galbraith"]
+    }
+}
---
+++ people/galbreath-duncan-1770-kelly.sh
@@ -0,0 +1,76 @@
+name Duncan Galbreath
+birth -date "about 1770"
+partner {
+    name Mary Kelly
+    marriage -date 26-mar-1794 -location 'killean and kilchenzie' -ref marriage
+
+    child {
+        name Margaret Galbreath
+        baptism -date 15-dec-1795 -location 'killean and kilchenzie' -ref margaret1795-baptism
+    }
+    child {
+        name Sarah Galbreath
+        baptism -date 16-nov-1798 -location 'killean and kilchenzie' -ref sarah1798-baptism
+    }
+    child galbraith-archibald-1800-wilkinson
+    child {
+        name Donald Galbreath
+        baptism -date 5-Mar-1802 -location 'killean and kilchenzie' -ref donald1802-baptism
+    }
+    child {
+        name Mary Galbreath
+        baptism -date 14-feb-1803 -location 'killean and kilchenzie' -ref mary1803-baptism
+    }
+    child {
+        name Alexander Galbreath
+        baptism -date 14-jun-1805 -location 'killean and kilchenzie' -ref alexander1805-baptism
+    }
+    child {
+        name Kate Galbreath
+        baptism -date 27-may-1807 -location 'killean and kilchenzie' -ref kay1807-baptism
+    }
+    child {
+        name Susanna Galbreath
+        baptism -date 10-aug-1809 -location 'killean and kilchenzie' -ref susanna1809-baptism
+    }
+    child {
+        name Flora Galbreath
+        baptism -date 1-oct-1811 -location 'killean and kilchenzie' -ref flora1811-baptism
+    }
+    child {
+        name Christian Galbreath
+        baptism -date 12-may-1815 -location 'killean and kilchenzie' -ref christian1815-baptism
+    }
+}
+footnotes {
+    marriage {
+        $opr-ref-link[m-1794-519-000-0010-0331 "Duncan Galbreath" "Mary Kelly"]
+    }
+    margaret1795-baptism {
+        $opr-ref[b-1795-519-000-0010-0111 "Margaret Galbreath"]
+    }
+    sarah1798-baptism {
+        $opr-ref[b-1798-519-000-0010-0131 "Sarah Galbreath"]
+    }
+    donald1802-baptism {
+        $opr-ref[b-1802-519-000-0010-0162 "Donald Galbreath"]
+    }
+    mary1803-baptism {
+        $opr-ref[b-1803-519-000-0010-0169 "Mary Galbreath"]
+    }
+    alexander1805-baptism {
+        $opr-ref[b-1805-519-000-0010-0190 "Alexander Galbreath"]
+    }
+    kate1807-baptism {
+        $opr-ref[b-1807-519-000-0010-0209 "Kate Galbreath"]
+    }
+    susanna1809-baptism {
+        $opr-ref-link[b-1809-519-000-0010-0229 "Susanna Galbreath"]
+    }
+    flora1811-baptism {
+        $opr-ref[b-1811-519-000-0010-0248 "Flora Galbreath"]
+    }
+    christian1815-baptism {
+        $opr-ref-link[b-1815-519-000-0010-0279 "Christian Galbreath"]
+    }
+}
