7d3ba7cc133541adcbd222a000d2efd50ebdd972
nickg <nickg@client9.com>
2023-03-10 17:52:15 -0800

    add line

---
+++ people/galbreath-alexander-1816-mccualskey.sh
@@ -0,0 +1,58 @@
+name Alexander Galbreath
+baptism -date 20-apr-1816 -location southend -ref alexander1816-baptism
+death -date 1898 -location Washington,Ohio,USA
+
+external {
+    familysearch 9NN7-9JW
+}
+partner {
+    name Elisabeth McCualskey
+    marriage -date 6-Feb-1845 -location campbeltown -ref marriage
+    birth -date 6-may-1815 -location Lochgilphead
+    baptism -date 8-may-1815 -location campbeltown
+    death -date 1904 -location Parkersburg,Wood,West Virginia,USA"
+
+    body {
+    daugbhter of Alexander McCualskey, b ~1778 from Alexander jr b. 1730 & Margaret McNaught b. 1734 in Campbeltown 4, and Margaret Raeside/Ryside, b. 1787 "Formerly a weaver's wife".
+    }
+
+    child {
+        name Archibald Galbreath
+        baptism -date 20-jan-1846 -location southend -ref archibald1846-baptism
+    }
+    child {
+        name Margaret Galbreath
+        baptism -date 15-oct-1848 -location campbeltown -ref margaret1848-baptism
+    }
+    child {
+        name Alexander Galbreath
+        baptism -date 14-nov-1850
+        death -date 1-July-1925 -location "Parkersburg,Wood,West Virginia,USA"
+        body {
+            unconfirmed
+        }
+    }    
+    child {
+        name Margaret Elizabeth Galbreath
+        birth -date 1858 -location veto,ohio,usa
+        death -date 12-jul-1937 -location "wood,west virginia,usa"
+    }
+}
+
+footnotes {
+    alexander1816-baptism  {
+        $opr-ref-link[b-1816-532-000-0010-0102 "Alexander Galbreath"]
+    }
+    marriage {
+        $opr-ref-link[m-1845-532-000-0020-0167 "Alexander Galbraith" "Elisabeth McCualskey"]
+        $opr-ref-link[m-1845-507-000-0060-0405 "Alexander Galbreath" "Elizabeth McCualesky"] 
+    }
+    archibald1846-baptism {
+        $opr-ref-link[b-1846-532-000-0020-0117 "Archibald Galbraith"]
+    }
+    margaret1848-baptism {
+        $opr-ref-link[b-1848-507-000-0070-0342 "Margaret Galbreath"]
+    }
+}
+
+
---
+++ people/galbreath-archibald-1780-unknown.sh
@@ -0,0 +1,54 @@
+name Archibald Galbreath
+birth -date 'about 1780'
+external {
+    familysearch MZKS-H6C
+}
+partner {
+    name _____ _____
+
+    child {
+        name Cristian Galbreath
+        baptism -date 16-Apr-1804 -location southend -ref cristian1804-baptism
+    }
+    child {
+        name Marey Galbreath
+        baptism -date 16-Feb-1806 -location southend -ref mary1806-baptism
+    }
+    child {
+        name Archibald Galbreath
+        baptism -date 16-Oct-1807 -location southend -ref archibald1807-baptism
+    }
+    child {
+        name Ronald Galbreath
+        baptism -date 20-Dec-1809 -location southend -ref ronald1809-baptism
+    }
+    child {
+        name Neil Galbreath
+        baptism -date 9-Nov-1811 -location southend -ref neil1811-baptism
+    }
+    child {
+        name Duncan Galbreath
+        baptism -date 19-May-1814 -location southend -ref duncan1814-baptism
+    }
+    child galbreath-alexander-1816-mccualskey
+}
+footnotes {
+    cristian1804-baptism {
+        $opr-ref-link[b-1804-532-000-0010-0078 "Cristian Galbreath"]
+    }
+    mary1806-baptism {
+        $opr-ref-link[b-1806-532-000-0010-0081 "Marey Galbreath"]
+    }
+    archibald1807-baptism {
+        $opr-ref-link[b-1808-532-000-0010-0084 "Archibald Galbreath"]
+    }
+    ronald1809-baptism {
+        $opr-ref-link[b-1809-532-000-0010-0089 "Ronald Galbreath"]
+    }
+    neil1811-baptism {
+        $opr-ref-link[b-1811-532-000-0010-0093 "Neil Galbreath"]
+    }
+    duncan1814-baptism {
+        $opr-ref-link[b-1814-532-000-0010-0098 "Duncan Galbreath"]
+    }
+}
