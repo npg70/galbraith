ca334616cf5a805675a3a57dd12be291c7373b67
nickg <nickg@client9.com>
2023-03-13 21:56:19 -0700

    add

---
+++ people/galbreath-john-abt-1775-mcgill.sh
@@ -0,0 +1,81 @@
+name John Galbreath
+birth -date 'about 1775'
+
+note {
+    No record found after baptism of last child in 1819. Nothing in 1841 or 1851 census, nothing in
+    statutory records.
+}
+
+external {
+    familysearch K8SX-L6H
+}
+partner {
+    name Margaret McGill
+    marriage -date 6-jun-1800 -location 'killean and kilchenzie' -ref marriage
+
+
+    child {
+        name Archibald Galbreath
+        baptism -date 22-oct-1802 -location 'killean and kilchenzie' -ref archibald1802-baptism
+    }
+    child {
+        name Donald Galbreath
+        baptism -date 20-sep-1804 -location 'killean and kilchenzie' -ref donald1804-baptism
+    }
+    child {
+        name Jean Galbreath
+        baptism -date 20-nov-1806 -location 'killean and kilchenzie' -ref jean1806-baptism
+    }
+    child {
+        name David Galbreath
+        baptism -date 13-may-1809 -location 'killean and kilchenzie' -ref david1809-baptism
+    }
+    child {
+        name Alexander McDonald Galbreath
+        baptism -date 25-jun-1811 -location 'killean and kilchenzie' -ref alexander1811-baptism
+    }
+    child {
+        name John Galbreath
+        baptism -date 24-jan-1815 -location 'killean and kilchenzie' -ref john1815-baptism
+    }
+    child {
+        name Rodger Galbreath
+        baptism -date 20-sep-1816 -location 'killean and kilchenzie' -ref rodger1816-baptism
+    }
+    child {
+        name Margaret Galbreath
+        baptism -date 1-feb-1819 -location 'killean and kilchenzie' -ref margaret1819-baptism
+    }
+}
+footnotes {
+
+    marriage {
+        $opr-ref-link[m-1800-519-000-0010-0342 "John Galbreath" "Margarate McGill"]{
+            June 6 | John Galbreath and Margarate McGill at $i{illegible place name}
+        }
+    }
+    archibald1802-baptism {
+        $opr-ref[b-1802-519-000-0010-0167 "Archibald Galbreath"]
+    }
+    donald1804-baptism {
+        $opr-ref[b-1804-519-000-0010-0183 "Donald Galbreath"]
+    }
+    jean1806-baptism {
+        $opr-ref[b-1806-519-000-0010-0203 "Jean Galbreath"]
+    }
+    david1809-baptism {
+        $opr-ref[b-1809-519-000-0010-0226 "David Galbreath"]
+    }
+    alexander1811-baptism {
+        $opr-ref[b-1811-519-000-0010-0246 "Alexander McDonald Galbreath"]
+    }
+    john1815-baptism {
+        $opr-ref[b-1815-519-000-0010-0277 "John Galbreath"]
+    }
+    rodger1816-baptism {
+        $opr-ref[b-1816-519-000-0010-0287 "Rodger Galbreath"]
+    }
+    margaret1819-baptism {
+        $opr-ref[b-1819-519-000-0010-0302 "Margarate Galbreath"]
+    }
+}
