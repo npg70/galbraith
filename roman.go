844cc02d4eb8902f6f30c16714cb3ca26631ff01
nickg <nickg@client9.com>
2023-03-14 22:54:12 -0700

    more roman!

--- roman.go
+++ roman.go
@@ -2,5 +2,5 @@ package main
 
 func toRoman(num int) string {
 	return []string{"", "i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix",
-		"x", "xi", "xii", "xiii", "xiv", "xv"}[num]
+		"x", "xi", "xii", "xiii", "xiv", "xv", "xvi", "xvii", "xviii", "xix", "xx"}[num]
 }
