128bf1c199beb2182b8c859459a4607b7606fb8c
npg70 <77904339+npg70@users.noreply.github.com>
2023-03-16 21:40:23 -0400

    Update galbraith-david-steward-1782-fraser.sh

--- people/galbraith-david-steward-1782-fraser.sh
+++ people/galbraith-david-steward-1782-fraser.sh
@@ -1,5 +1,5 @@
 name David Stewart Galbraith
-baptism -date 9-oct-1782 -location 'killean and kilchenzie' -ref david1782-baptism
+birth -date 9-oct-1782 -location 'killean and kilchenzie' -ref david1782-baptism
 death -date 27-dec-1863 -location Edinburgh -ref david1782-death
 external {
   familysearch KZDT-66V
@@ -7,15 +7,25 @@ external {
 partner {
   name Elizabeth Fraser
   marriage -date 9-june-1812 -location edinburgh -ref fraser-marriage
+  
+  child {
+  	name David Stewart Galbraith
+    baptism -date 22-aug-1827 -location campbeltown -ref david1827-baptism
+  }  
 }
 footnotes {
   david1782-baptism {
-  
+     TBD: unclear where this date or place came from.
   }
   david1782-death {
-  
+    $sp-ref-link[d-1864-684-02-0096 0032 "David Stewart Galbraith"]
   }
   fraser-marriage {
-  
+    $opr-ref-link[m-1812-685-001-0540-0039 "David Stewart Galbreath" "Eliza Fraser"] 
+  }
+  david1827-baptism {
+  	$opr-ref[b-1827-507-000-0060-0148 "David Stewart Galbreath"]
+    $opr-ref[b-1827-507-000-0060-0188 "David Stewart Galbreath"]
+    TBD: why two records
   }
 }
