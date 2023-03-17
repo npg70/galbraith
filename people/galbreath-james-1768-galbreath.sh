9e2d72f1ff2028f619f943c65610bed8b1bb02a3
npg70 <77904339+npg70@users.noreply.github.com>
2023-03-16 23:35:23 -0400

    Update galbreath-james-1768-galbreath.sh

--- people/galbreath-james-1768-galbreath.sh
+++ people/galbreath-james-1768-galbreath.sh
@@ -27,6 +27,15 @@ partner {
     baptism -date 31-sep-1801 -location campbeltown -ref janet1801-baptism
 	}
   child {
+  	name Orsella Galbreath
+    baptism -date 24-nov-1805 -location campbeltown -ref orsella1805-baptism
+    death -date 2-dec-1898 -location campbeltown -ref orsella1805-death
+    partner {
+    	name James Templeton
+      marriage -date 3-feb-1829 -location campbeltown -ref orsella1805-marriage
+    }
+  }
+  child {
   	name Martha Galbreath
     baptism -date 16-dec-1807 -location campbeltown -ref martha1807-baptism
   }
@@ -81,6 +90,15 @@ footnotes {
   }
   janet1801-baptism {
   	$opr-ref-link[b-1801-507-000-0040-0219 "Janet Galbreath"]
+  }
+  orsella1805-baptism {
+  	$opr-ref-link[b-1805-507-000-0040-0282 "Orsella Galbreath"]
+  }
+  orsella1805-death {
+   	$sp-ref-link[d-1898-507-00-0137 0046 "Orsella Galbraith"]
+  }
+  orsella1805-marriage {
+    $opr-ref-link[m-1829-507-000-0060-0288 "Orsla Galbreath" "James Templeton"]
   }
   martha1807-baptism {
   	$opr-ref-link[b-1807-507-000-0040-0312 "Martha Galbreath"]
