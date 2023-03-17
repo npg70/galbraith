3f86ee29209276646b5ac5f78d1c1cc066ce082c
npg70 <77904339+npg70@users.noreply.github.com>
2023-03-16 21:59:25 -0400

    Update galbraith-david-steward-1782-fraser.sh

--- people/galbraith-david-steward-1782-fraser.sh
+++ people/galbraith-david-steward-1782-fraser.sh
@@ -9,9 +9,29 @@ partner {
   marriage -date 9-june-1812 -location edinburgh -ref fraser-marriage
   
   child {
+  	name James Fraser Galbreath
+    baptism -date 31-aug-1813 -location campbeltown -ref james1813-baptism
+  }
+  child {
+    name Alexander MacDonald Galbreath
+    baptism -date 16-sep-1819 -location campbeltown -ref alexander1819-baptism
+  }
+  child {
   	name David Stewart Galbraith
     baptism -date 22-aug-1827 -location campbeltown -ref david1827-baptism
-  }  
+  }
+  child {
+    name Adelaide Galbreath
+    baptism -date 23-sep-1831 -location campbeltown -ref adelaide1831-baptism
+  }
+  child {
+    name John Campbell Galbreath
+    baptism -date 9-aug-1833 -location campbeltown -ref john1833-baptism
+  }
+  child {
+    name Henry Louis Galbreath
+    baptism -date 12-jun-1837 -location campbeltown -ref henry1837-baptism
+  }
 }
 footnotes {
   david1782-baptism {
@@ -23,9 +43,24 @@ footnotes {
   fraser-marriage {
     $opr-ref-link[m-1812-685-001-0540-0039 "David Stewart Galbreath" "Eliza Fraser"] 
   }
+  james1813-baptism {
+  	$opr-ref[b-1813-507-000-0040-0403 "James Fraser Galbreath"]
+  }
+  alexander1819-baptism {
+    $opr-ref[b-1819-507-000-0040-0498 "Alexander MacDonald Galbreath"]
+  }
   david1827-baptism {
   	$opr-ref[b-1827-507-000-0060-0148 "David Stewart Galbreath"]
     $opr-ref[b-1827-507-000-0060-0188 "David Stewart Galbreath"]
     TBD: why two records
+  }
+  adelaide1831-baptism {
+  	$opr-ref[b-1831-507-000-0060-0228 "Adelaide Galbreath"]
+  }
+  john1833-baptism {
+    $opr-ref[b-1833-507-000-0070-0029 "John Campbell Galbreath"]
+  }
+  henry1837-baptism {
+  	$opr-ref[b-1837-507-000-0070-0103 "Henry Louis Galbreath"]
   }
 }
