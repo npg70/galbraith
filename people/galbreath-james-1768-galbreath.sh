24ce783b7f0281c382ff2b44c5266c01ba28d99b
npg70 <77904339+npg70@users.noreply.github.com>
2023-03-16 23:03:13 -0400

    Create

---
+++ people/galbreath-james-1768-galbreath.sh
@@ -0,0 +1,32 @@
+name James Galbreath
+birth -date 'about 1768'
+
+external {
+  familysearch KZ84-RRK
+}
+body {
+$p{
+He died between 1829 and 1837 due to marriage records of his daughters Orsella and Catherine. The same daughters may have married bothers James and Hugh. (See ScotlandsPeoples)
+}
+$p{
+Martha Galbreath and their youngest daughter Mary are recorded in the 1841 Census as being in Long Row, Campbeltown.2 Later records for either of them have not been found.
+}
+$p{
+From the baptism records of children, he was a farmer in Skebolin.
+}
+$p{
+He won a prize for turnip growning in 1804.
+}
+}
+
+partner {
+  name Margaret Galbreath
+  marriage -date 20-mar-1800 -location campbeltown -ref galbreath-marraige  
+  
+}
+
+footnotes {
+  galbreath-marriage {
+  
+  }
+}
