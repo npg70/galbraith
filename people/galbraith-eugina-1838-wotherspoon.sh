250e1b7cf84e11c50d9d1e80a1465ecaa3bcab01
npg70 <77904339+npg70@users.noreply.github.com>
2023-03-17 06:48:46 -0400

    Create

---
+++ people/galbraith-eugina-1838-wotherspoon.sh
@@ -0,0 +1,23 @@
+name Eugina Galbraith
+baptism -date 25-jan-1838 -location campbeltown -ref eugina1838-baptism
+death -date 5-oct-1920 -location clachan -ref eugina1838-death
+external {
+  familysearch KZLL-XNS
+  findagrave 115118723
+}
+partner {
+  name James Wotherspoon
+  marriage -date 24-apr-1866 -ref wotherspoon-marriage
+}
+
+footnotes {
+  eugina1838-baptism {
+    $opr-ref-link[]
+  }
+  eugina1838-death {
+  
+  }
+  wotherspoon-marriage {
+  
+  }
+}
