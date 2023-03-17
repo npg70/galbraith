34a0c930f7df4f6e2be4381ced5494b87dfb2018
npg70 <77904339+npg70@users.noreply.github.com>
2023-03-17 06:26:33 -0400

    Update galbreath-archibald-1803-mccallum-loynachan.sh

--- people/galbreath-archibald-1803-mccallum-loynachan.sh
+++ people/galbreath-archibald-1803-mccallum-loynachan.sh
@@ -3,6 +3,11 @@ baptism -date 11-sep-1803 -location campbeltown -ref archibald1803-baptism
 partner {
   name Eugenia McCallum
   marriage -date 28-Feb-1835 -location kilcalmonel -ref mccallum-marriage
+  child {
+  	name Martha Galbreath
+    baptism -date 30-Nov-1835 -location campbeltown -ref martha1835-baptism
+    death -date 9-Jul-1922 -location campbeltown -ref martha1835-death -note umn
+  }
 }
 partner {
   name Margaret Loynachan
@@ -13,11 +18,17 @@ footnotes {
     $opr-ref-link[b-1803-507-000-0040-0252 "Archibald Galbreath"]
   }
   mccallum-marriage {
-    $opr-ref-link[m-1835-516-000-0020-0248 "Archibald Galbraith" "Eugina McCallum"]
+    $opr-ref-link[m-1835-516-000-0020-0248 "Archibald Galbraith" "Eugenia Macallum"]
     
     Cross-recorded at Campbeltown on 5 Mar 1835:
   
     $opr-ref-link[m-1835-507-000-0060-0322 "Archibald Galbreath" "Eugina McCallum"]
+  }
+  martha1835-baptism {
+  
+  }
+  martha1835-death {
+  
   }
   loynachan-marriage {
     $opr-ref-link[m-1839-532-000-0020-0164 "Archibald Galbraith" "Margaret Lynachan"]
