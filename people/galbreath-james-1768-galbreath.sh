58320e7e53d1639505126e3644d95af5ebb45a28
npg70 <77904339+npg70@users.noreply.github.com>
2023-03-16 23:24:41 -0400

    Update galbreath-james-1768-galbreath.sh

--- people/galbreath-james-1768-galbreath.sh
+++ people/galbreath-james-1768-galbreath.sh
@@ -22,11 +22,88 @@ He won a prize for turnip growning in 1804.
 partner {
   name Margaret Galbreath
   marriage -date 20-mar-1800 -location campbeltown -ref galbreath-marraige  
-  
+  child {
+  	name Janet Galbreath
+    baptism -date 31-sep-1801 -location campbeltown -ref janet1801-baptism
+	}
+  child {
+  	name Martha Galbreath
+    baptism -date 16-dec-1807 -location campbeltown -ref martha1807-baptism
+  }
+  child {
+  	name Catharine Galbreath
+    baptism -date 25-apr-1809 -location campbeltown -ref cathatine1809-baptism
+    partner {
+    	name Hugh Templeton
+      body {
+      	maltster
+      }
+    }
+  }
+  child {
+  	name Samuel Galbreath
+    baptism -date 20-feb-1811 -location campbeltown -ref samuel1811-baptism
+  }
+  child {
+  	name John Galbreath
+    baptism -date 5-jan-1813 -location campbeltown -ref john1813-baptism
+  }
+  child {
+  	name James Galbreath
+    baptism -date 15-dec-1814 -location campbeltown -ref james1814-baptism
+  }
+  child {
+  	name Mary Galbreath
+    baptism -date 19-jun-1816 -location campbeltown -ref mary1816-baptism
+	}
+  child {
+  	name Jean Galbreath
+    baptism -date 3-dec-1817 -location campbeltown -ref jean1817-baptism
+    partner {
+    	name Robert Kelly
+      body {
+      		Shipmaster
+      }
+    }
+	}
+  child {
+  	name Mary Galbreath
+    baptism -date 13-mar-1821 -location campbeltown -ref mary1821-baptism
+  }
 }
 
 footnotes {
   galbreath-marriage {
-  
+   $opr-ref-link[]{
+  		James Galbreath and Martha Galbreath both of this parish 26 [March 1800]
+  	}
+    Indexed as 20-mar-1800, but appears to be 26-mar-1800.
   }
+  janet1801-baptism {
+  	$opr-ref-link[b-1801-507-000-0040-0219 "Janet Galbreath"]
+  }
+  martha1807-baptism {
+  	$opr-ref-link[b-1807-507-000-0040-0312 "Martha Galbreath"]
+  }
+  catharine1809-baptism {
+  	$opr-ref-link[b-1809-507-000-0040-0333 "Catharine Galbreath"]
+  }
+  samuel1811-baptism {
+  	$opr-ref-link[b-1811-507-000-0040-0365 "Samuel Galbreath"]
+  }
+  john1813-baptism {
+  	$opr-ref-link[b-1813-507-000-0040-0392 "John Galbreath"]
+  }
+  james1814-baptism {
+  	$opr-ref-link[b-1814-507-000-0040-0423 "James Galbreath"]
+  }
+  mary1816-baptism {
+  	$opr-ref-link[b-1816-507-000-0040-0446 "Mary Galbreath"]
+  }
+  jean1817-baptism {
+  	$opr-ref-link[b-1817-507-000-0040-0469 "Jean Galbreath"]
+  }
+  mary1821-baptism {
+  	$opr-ref-link[b-1821-507-000-0060-0031 "Mary Galbreath"]
+  } 
 }
