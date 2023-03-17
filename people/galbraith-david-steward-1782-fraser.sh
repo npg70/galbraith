98ea7cbeeb93345cfd012878df913b665fdfdaf9
npg70 <77904339+npg70@users.noreply.github.com>
2023-03-16 21:20:42 -0400

    Create

---
+++ people/galbraith-david-steward-1782-fraser.sh
@@ -0,0 +1,21 @@
+name David Stewart Galbraith
+baptism -date 9-oct-1782 -location 'killean and kilchenzie' -ref david1782-baptism
+death -date 27-dec-1863 -location Edinburgh -ref david1782-death
+external {
+  familysearch KZDT-66V
+}
+partner {
+  name Elizabeth Fraser
+  marriage -date 9-june-1812 -location edinburgh -ref fraser-marriage
+}
+footnotes {
+  david1782-baptism {
+  
+  }
+  david1782-death {
+  
+  }
+  fraser-marriage {
+  
+  }
+}
