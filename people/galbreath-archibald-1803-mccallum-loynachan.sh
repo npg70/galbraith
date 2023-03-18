f44fa38bf223f99c60de3bf4c6add018c4779daf
npg70 <77904339+npg70@users.noreply.github.com>
2023-03-18 00:32:10 -0400

    Update galbreath-archibald-1803-mccallum-loynachan.sh

--- people/galbreath-archibald-1803-mccallum-loynachan.sh
+++ people/galbreath-archibald-1803-mccallum-loynachan.sh
@@ -13,7 +13,69 @@ partner {
 partner {
   name Margaret Loynachan
   marriage -date 25-Aug-1839 -location southend -ref loynachan-marriage   
+  death -date 14-nov-1886 -location dumbarton -ref loynachan-death
+  
+  child {
+  	name James Galbreath
+    birth -date 23-sep-1841
+    baptism -date 7-nov-1841 -location campbeltown -ref james1841-baptism
+    death -date 6-dec-1875 -note umn
+    body {
+    	seaman
+    }
+  }
+  child {
+  	name Archibald Galbraith
+    birth -date 17-jul-1843
+    baptism -date 13-aug-1843 -location campbeltown -ref archibald1843-baptism
+    partner {
+    	name Mary Paterson
+    }
+  }
+  child {
+  	name Neill Galbraith
+    birth -date 20-may-1845
+    baptism -date 15-jun-1845 -location campbeltown -ref neil1845-baptism
+    death -date 3-dec-1909 -location dumbarton -ref neil1845-death
+    partner {
+    	name Margaret Blue
+    }
+    partner {
+    	name Margaret McCall/Watt
+    }
+  }
+  child {
+  	name Agnes Galbreath
+    baptism -date 1-nov-1847 -location campbeltown -ref agnes1847-baptism
+  }
+  child {
+  	name Samuel Galbraith
+    birth -date 17-nov-1850
+    baptism -location campbeltown
+    partner {
+    	name Mary Ellen Reynolds
+    }
+  }
+  child {
+  	name Margaret Galbraith
+    birth -date 39-apr-1853
+    baptism -date 10-jul-1853 -location campbeltown -ref margaret1853-baptism
+  }
+  child {
+  	name Agnes Galbraith
+    birth -date 9-jun-1855 -location campbeltown -ref agnes1855-birth
+    death -date 4-jan-1944 -location glasgow -ref agnes1855-death
+    partner {
+    	name James Mitchell
+    }
+  }
+  child {
+  	name John Galbraith
+    birth -date 17-oct-1857 -location campbeltown -ref john1857-birth
+    death -date 7-mar-1864 -location campbeltown -ref john1857-death
+  } 
 }
+
 footnotes {
   archibald1803-baptism {
     $opr-ref-link[b-1803-507-000-0040-0252 "Archibald Galbreath"]
@@ -29,7 +91,7 @@ footnotes {
   	$opr-ref-link[b-1835-507-000-0070-0072 "Martha Galbreath"]
   }
   martha1835-death {
-  
+  	$sp-ref-link[d-1922-507-00-0092 0031 "Martha Galbraith"]
   }
   loynachan-marriage {
     $opr-ref-link[m-1839-532-000-0020-0164 "Archibald Galbraith" "Margaret Lynachan"]
@@ -37,5 +99,38 @@ footnotes {
     Cross-recorded at Campbeltown on 25 Aug 1940:
     
     $opr-ref-link[m-1840-507-000-0060-0367 "Archibald Galbreath" "Margaret Loynachan"]
+  }
+  loynachan-death {
+  	$sp-ref-link[d-1886-496-00-0309 0103 "Margaret Galbraith"]
+  }
+  james1841-baptism {
+  	$opr-ref-link[b-1841-507-000-0070-0207 "James Galbreath"]
+  }
+  archibald1843-baptism {
+  	$opr-ref-link[b-1843-507-000-0070-0244 "Archibald Galbreath"]
+  }
+  neil1845-baptism {
+  	$opr-ref-link[b-1845-507-000-0070-0285 "Neill Galbreath"]
+  }
+	neil1845-death {
+  	$sp-ref-link[d-1909-496-00-0308 0103 "Neil Galbraith"]
+  }
+  agnes1847-baptism {
+  	$opr-ref-link[b-1847-507-000-0070-0327 "Agnes Galbreath"]
+  }
+  margaret1853-baptism {
+  	$opr-ref-link[b-1853-507-000-0070-0416 "Margaret Galbraith"]
+  }
+  agnes1855-birth {
+  	$sp-ref-link[b-1855-507-01-0065 0013 "Agnes Galbraith"]
+  }
+  agnes1855-death {
+  	$sp-ref-link[d-1944-644-07-0009 0003 "Agnes Galbraith"]
+  }
+  john1857-birth {
+  	$sp-ref-link[b-1857-507-00-0212 0071 "John Galbraith"]
+  }
+  john1857-death {
+  	$sp-ref-link[d-1864-507-00-0049 0017 "John Galbraith"]
   }
 }
