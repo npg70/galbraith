2cabd32129b38cbcd2cfe6bdb7e8446acfb89840
nickg <nickg@client9.com>
2023-03-21 09:22:15 -0700

    new lines and people

---
+++ people/galbreath-patrick-abt-1700-maxwell.sh
@@ -0,0 +1,24 @@
+name Patrick Galbreath
+birth -date 'about 1700'
+note {
+    Two children baptized in Campbeltown in 1700s with no other records.
+}
+partner {
+    name Mary Maxwell
+    child {
+        name Mary Galbreath
+        baptism -date 29-jan-1727 -location campbeltown -ref mary1727-baptism
+    }
+    child {
+        name John Galbreath
+        baptism -date 24-feb-1734 -location campbeltown -ref john1734-baptism
+    }
+}
+footnotes {
+    mary1727-baptism {
+        $opr-ref-link[b-1727-507-000-0011-0070 "Mary Galbreath"]
+    }
+    john1734-baptism {
+        $opr-ref-link[b-1734-507-000-0011-0202 "John Galbreath"]
+    }
+}
---
+++ people/galbreath-william-abt-1701-watson.sh
@@ -0,0 +1,71 @@
+name William Galbreath
+birth -date 'about 1701'
+external {
+    familysearch K8TT-KB2
+}
+note {
+    No records beyond a marriage and baptism of six children.
+}
+
+partner {
+    name Margaret Watson 
+
+    marriage -date 4-may-1746 -location campbeltown -ref watson-marriage
+
+    child {
+        name Patrick Galbreath
+        baptism -date 14-may-1747 -location campbeltown -ref patrick1747-baptism
+    }    
+    child {
+        name Margaret Galbreath
+        baptism -date 30-oct-1748 -location campbeltown -ref margaret1748-baptism
+    }
+    child {
+        name Margaret Galbreath
+        baptism -date 30-jul-1752 -location campbeltown -ref margaret1752-baptism
+    }
+    child {
+        name Isobell Galbreath
+        baptism -date 30-nov-1753 -location campbeltown -ref isobell1753-baptism
+    }
+    child {
+        name Martha Galbreath
+        baptism -date 1-aug-1756 -location campbeltown -ref martha1756-baptism
+    }
+    child {
+        name Mary Galbreath 
+        baptism -date 12-dec-1761 -location campbeltown -ref mary1761-baptism
+    }
+}
+
+footnotes {
+
+    watson-marriage {
+        $opr-ref-link[m-1746-507-000-0011-0449 "William Calbreath" "Margaret Watson"]{
+            William Calbreath + Margaret Watson | May 4th
+        }
+        $opr-ref[m-1746-507-000-0010-0179 "William Calbraith" "Margaret Watson"]
+    }
+    patrick1747-baptism {
+        $opr-ref[b-1747-507-000-0011-0259 "Patrick Galbreath"]
+    }
+    margaret1748-baptism {
+        $opr-ref[b-1748-507-000-0011-0266 "Margaret Galbreath"]
+    }
+    margaret1752-baptism {
+        $opr-ref[b-1752-507-000-0011-0283 "Margaret Galbreath"]
+        $opr-ref[b-1752-507-000-0010-0209 "Margaret Galbraith"]
+    }
+    isobell1753-baptism {
+        $opr-ref[b-1753-507-000-0011-0292 "Isobell Galbreath"]
+        $opr-ref[b-1753-507-000-0010-0219 "Isobell Galbraith"]
+    }
+    martha1756-baptism {
+        $opr-ref[b-1756-507-000-0011-0302 "Martha Galbreath"]
+        $opr-ref[b-1756-507-000-0010-0228 "Martha Galbraith"]
+    }
+    mary1761-baptism {
+        $opr-ref[b-1761-507-000-0011-0327 "Mary Galbreath"]
+        $opr-ref[b-1761-507-000-0010-0262 "Mary Galbraith"]
+    }
+}
