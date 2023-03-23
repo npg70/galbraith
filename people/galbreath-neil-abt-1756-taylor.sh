d464277e7d8219a3f0bb3ecbf7a350d05b6d50f8
nickg <nickg@client9.com>
2023-03-23 13:15:30 -0700

    add new

---
+++ people/galbreath-neil-abt-1756-taylor.sh
@@ -0,0 +1,53 @@
+name Neil Galbreath
+birth -date 'about 1756'
+
+external {
+    familysearch K821-T1P
+}
+
+partner {
+    name Grizzle Taylor
+    marriage -date 9-mar-1781 -location campbeltown -ref taylor-marriage
+
+    child {
+        name Neil Galbraith
+        baptism -date 'about 1790' -location campbeltown
+        death -date 6-dec-1865 -location campbeltown -ref neil1790-death -note 'in poorhouse'
+        partner {
+            name Mary Harris
+        }
+    }
+
+    child {
+        name Katherine Galbreath
+        baptism -date 14-mar-1794 -location campbeltown -ref kate1794-baptism
+    }
+    child {
+        name Hester Galbreath
+        baptism -date 27-jul-1796 -location campbeltown -ref hester1796-baptism
+    }
+    child {
+        name Charles Galbreath
+        baptism -date 1-mar-1801 -location campbeltown -ref charles1801-baptism
+    }
+}
+
+footnotes {
+    taylor-marriage {
+        $opr-ref[m-1781-507-000-0050-0014 "Neill Galbreath" "Grissell Taylor"]
+    }
+
+    neil1790-death {
+        $sp-ref-link[d-1865-507-00-0152 0051 "Neil Galbraith"]
+    }
+    kate1794-baptism {
+        $opr-ref-link[b-1794-507-000-0040-0114 "Katherin Galbreath"]
+    }
+    hester1796-baptism {
+        $opr-ref[b-1796-507-000-0040-0141 "Hester Galbreath"]
+        Image is missing.  Name is likely "Hector" not "Hester"
+    }
+    charles1801-baptism {
+        $opr-ref[b-1801-507-000-0040-0213 "Charles Galbreath"]
+    }
+}
