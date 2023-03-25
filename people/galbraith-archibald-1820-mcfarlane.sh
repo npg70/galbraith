73707752c50a0388c4843aca26995f9d4d7430d2
nickg <nickg@client9.com>
2023-03-25 09:45:10 -0700

    additions

---
+++ people/galbraith-archibald-1820-mcfarlane.sh
@@ -0,0 +1,143 @@
+name Archibald Galbraith
+baptism -date 29-jun-1817 -location Killean -ref archibald1817-baptism
+death -date 9-oct-1892 -location 'kinning park, glasgow' -ref archibald1820-death
+external {
+    familysearch 9KMJ-5ZH
+}
+note {
+    FamilySearch March 2023 has Archibald pointing to the wrong parents.
+}
+body {
+
+Census 1871, Harmony Row, 44, Govan, Govan Church, Lanarkshire, Scotland:$ref[census1871]
+
+$csvtable{
+First,Last,Role,Job,Age,Year,Birth
+Archibald,Galbraith,Head,Railway Plate Layer,52,1819,Killean
+Agnes,Galbraith,Wife,Platelayers wife,51,1820,Killean
+Maggie,Galbraith,Dau,Asst Housekeeper,15,1856,Dundonald
+Agnes,Galbraith,Dau,Scholar,11,1860,Dundonald
+Archibald,Galbraith,Son,Scholar,9,1862,Dundonald
+Isabella A,Galbraith,Dau,Scholar,7,1964,Dundonald
+}
+
+Census 1881, Harmony Row, 53, Govan, Govan Church, Lanarkshire, Scotland:$ref[census1881]
+
+$csvtable{
+First,Last,Role,Job,Age,Year,Birth
+Archibald,Galbraith,Head,Plate Layer,60,1821,Killean
+Agnes,Galbraith,Wife,-,59,1822,Killean
+Agnes,Galbraith,Dau,-,20,1861,Dundonald
+Archibald,Galbraith,Son,Joiner,18,1863,Dundonald
+Donald,Dick,Grandson,-,4,1877,Govan
+}
+}
+partner {
+    name Agnes McFarlane
+    marriage -date 20-nov-1842 -location Kingarth,bute -ref mcfarlane-marriage
+    death -date 12-aug-1887 -location govan -ref mcfarlane-death
+    child {
+        name Alexander Galbraith
+        baptism -date 6-sep-1843 -location Rothesay,Bute -ref alexander1843-baptism
+    }
+    child {
+        name Donald Galbreath
+        baptism -date 22-dec-1844 -location campbeltown -ref donald1844-baptism
+    }
+    child {
+        name Alexander Galbreath
+        baptism -date 27-feb-1848 -location campbeltown -ref alexander1848-baptism
+    }
+    child {
+        name Mary Galbreath
+        baptism -date 10-Sep-1850 -location Kilmory,Bute -ref mary1850-baptism
+    }
+    child {
+        name Agnes Galbraith
+        baptism -date 12-mar-1854 -location Dundonald,Ayr -ref agnes1854-baptism
+    }
+    child {
+        name Margaret Galbraith
+        birth -date 22-sep-1855 -location Dundonald,Ayr -ref margaret1855-birth
+    }
+    child {
+        name Agnes Galbraith
+        birth -date 8-feb-1859 -location Dundonald,Ayr -ref agnes1859-birth
+    }
+    child {
+        name Archibald Galbraith
+        birth -date 19-may-1861 -location Dundonald,Ayr -ref archibald1861-birth
+    }
+    child {
+        name Isabella McAlister Galbraith
+        birth -date 22-sep-1863 -location Dundonald,Ayr -ref isabella1863-birth
+    }
+}
+
+footnotes {
+    archibald1820-death {
+        $sp-ref-link[d-1892-644-14-0428 0143 "Archibald Galbraith"]
+        Unfortunately, the entry for mother and father appear to be
+        the name of the deceased and his wife.   Age listed as 72.
+    }
+    mcfarlane-marriage {
+        $opr-ref-link[m-1842-555-000-0020-0092 "Archibald Galbraith" "Agnes Mc Farlane"]
+    }
+    mcfarlane-death {
+        $sp-ref-link[d-1887-646-02-0575 0192 "Agnes Galbraith"]{
+        575: Agnes Galbraith (Married to Archibald Galbraith Railway Plate Layer (foreman)
+        Death: 1887 August Twelveth, Age 67? 61?;
+        Father: Alexander McFarland, Joiner (master), deceased;
+        Mother: Mary McLean, deceased;
+        Present: Archibald Galbraith, son
+        }
+    }
+
+    alexander1843-baptism {
+        $opr-ref[b-1843-558-000-0040-0188 "Alexander Galbraith"]
+    }
+    donald1844-baptism {
+        $opr-ref[b-1844-507-000-0070-0274 "Donald Galbreath"]
+    }
+    alexander1848-baptism {
+        $opr-ref[b-1848-507-000-0070-0332 "Alexander Galbreath"]
+    }
+    mary1850-baptism {
+        $opr-ref-link[b-1850-554-000-0030-0069 "Mary Galbreath"]{
+            $i{in section for 1854}
+            Archd Galbreath and Augness McFarland his wife $i{illegible}
+            Daughter baptized Septr 10th 1850 and named Mary
+            another Daughter baptized March 12th 1854 and named Augness
+            both the same parents
+        }
+    }
+    agnes1854-baptism {
+        $opr-ref-link[b-1854-554-000-0030-0069 "Augness Galbreath"] See entry for Mary above.
+    }
+    margaret1855-birth {
+        $sp-ref-link[b-1855-590-00-0119 0024 "Margaret Galbraith"]{
+            119. Galbraith, Margaret 
+            Born: 1855 September Twenty Second;
+            father: Archibald Galbraith, Agricultural Laborer, 31[?] years, Killean, Argyllshire;
+            marriage: 1842 Kingarth;
+            mother: Agnes Galbraith, maiden name, McFarlane, $i{illegible}, 34 years, Killean, Argyllshire;
+        }
+    }
+    agnes1859-birth {
+        $sp-ref[b-1859-590-00-0016 "Agnes Galbraith"]
+    }
+    archibald1861-birth {
+        $sp-ref[b-1861-590-00-0057 "Archibald Galbraith"]
+    }
+    isabella1863-birth {
+        $sp-ref[b-1863-590-00-0113 "Isabella McAlister Galbraith"]
+    }
+
+    census1871 {
+        https://www.findmypast.com/transcript?id=GBC/1871/0024930477&expand=true
+    }
+    census1881 {
+        https://www.findmypast.com/transcript?id=GBC/1881/0028622878&expand=true
+    }
+}
+
---
+++ people/galbreath-neil-1801-blue.sh
@@ -0,0 +1,47 @@
+name Neil Galbreath
+baptism -date 10-oct-1801 -location gigha -ref neil1801-baptism
+death -date 27-oct-1890 -location campebtlwon -ref neil1801-baptism
+note {
+    Children uninvestigated except for birth and death
+}
+partner {
+    name Elizabeth Blue
+    marriage -date 29-aug-1842 -location campbeltown -ref blue-marriage
+    body {
+        daughter of Duncan Blue
+    }
+    child {
+        name Isabella Galbreath
+        baptism -date 29-may-1843 -location campbeltown -ref isabella1843-baptism
+        death -date 1921 -location campbeltown -ref isabella1843-death
+    }
+    child {
+        name Dugald Galbreath
+        baptism -date 31-Jul-1845 -location campbeltown -ref dugald1845-baptim
+        death -date 1908 -location campbeltown -ref dugald1845-death
+    }
+}
+footnotes {
+    neil1801-baptism {
+        $opr-ref[b-1801-537-000-0010-0021 "Neil Galbreath"]
+    }
+    neil1801-death {
+        $sp-ref-link[d-1890-507-00-0139 0047 "Neil Galbraith"]
+    }
+    blue-marriage {
+        $opr-ref[m-1842-507-000-0060-0383 "Neil Galbreath" "Elizabeth Blue"]
+    }
+    isabella1843-baptism {
+        $opr-ref[b-1843-507-000-0070-0240 "Isabella Galbreath"]
+    }
+    isabella1843-death {
+        $sp-ref[d-1921-507-00-0100 "Isabella Galbraith"]
+    }
+    dugald1845-baptism {
+        $opr-ref[b-1845-507-000-0070-0288 "Dugald Galbreath"]
+    }
+    dugald1845-death {
+        $sp-ref[d-1908-507-00-0040 "Dugald Galbraith"]
+    }
+}
+
