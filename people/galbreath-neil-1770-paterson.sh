4f93e19a37f33081e5c84812dee6050ad9d70c57
nickg <nickg@client9.com>
2023-03-25 22:44:38 -0700

    cleanups, non functional

---
+++ people/galbreath-neil-1770-paterson.sh
@@ -0,0 +1,80 @@
+name Neil Galbreath
+birth -date 'about 1770'
+
+external {
+    familysearch 9M9Z-V9G
+}
+
+note {
+    last child Neil who perhaps married Mary Hamilton need investigating
+}
+
+partner {
+    name Flora Paterson
+    marriage -date 31-jul-1790 -location campbeltown -ref paterson-marriage
+
+    child {
+        name Margaret Galbreath
+        baptism -date 5-may-1791 -location campbeltown -ref margaret1791-baptism
+    }
+    child {
+        name Lachlan Galbreath
+        baptism -date 14-may-1793 -location campbeltown -ref lachlan1793-baptism
+    }
+    child {
+        name John Galbreath
+        baptism -date 14-oct-1795 -location campbeltown -ref elizabeth1795-baptism
+    }
+    child {
+        name Elizabeth Galbreath
+        baptism -date 14-oct-1795 -location campbeltown -ref elizabeth1795-baptism
+    }
+    child {
+        name Donald Galbreath
+        baptism -date 2-feb-1804 -location campbeltown -ref donald1804-baptism
+    }
+    child {
+        name Hugh Galbreath
+        baptism -date 4-may-1807 -location campbeltown -ref hugh1807-baptism
+    }
+    child {
+        name Catharine Galbreath
+        baptism -date 30-jul-1809 -location campbeltown -ref catharine1809-baptism
+    }
+    child {
+        name Neill Galbreath
+        baptism -date 23-feb-1812 -location campbeltown -ref neill1812-baptism
+    }
+}
+
+footnotes {
+
+    paterson-marriage {
+        $opr-ref-link[m-1790-507-000-0020-0173 "Neil Galbreath" "Flora Paterson"]{
+            Neil Galbreath + Flora Paterson both of this Parish contracted
+            31th July 1790 were married
+        }
+    }
+
+    margaret1791-baptism {
+        $opr-ref[b-1791-507-000-0020-0131 "Margaret Galbreath"]
+    }
+    lachlan1793-baptism {
+        $opr-ref[b-1793-507-000-0040-0107 "Lachlan Galbreath"]
+    }
+    elizabeth1795-baptism {
+        $opr-ref-link[b-1795-507-000-0040-0131 "John + Elizabeth Galbreath"]
+    }
+    donald1804-baptism {
+        $opr-ref[b-1804-507-000-0040-0257 "Donald Galbreath"]
+    }
+    hugh1807-baptism {
+        $opr-ref[b-1807-507-000-0040-0302 "Hugh Galbreath"]
+    }
+    catharine1809-baptism {
+        $opr-ref[b-1809-507-000-0040-0338 "Catharine Galbreath"]
+    }
+    neill1812-baptism {
+        $opr-ref-link[b-1812-507-000-0040-0380 "Neill Galbreath"]
+    }
+}
