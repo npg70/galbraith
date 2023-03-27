279b62f9b50ba290efdb2f961f2ccc91af1a1497
nickg <nickg@client9.com>
2023-03-26 22:29:13 -0700

    add new

---
+++ people/galbreath-neil-abt-1885-brown.sh
@@ -0,0 +1,83 @@
+name Neil Galbreath
+birth -date 'say 1885'
+
+external {
+    familysearch KCMF-RBY
+}
+
+body {
+The census of 1851 in campbeltown has a household consisting of just a "Marion Galbraith", age 68 (~1783), widowed, listing a birthplace of Gigha.
+}
+
+partner {
+    name Marion Brown
+    birth -date 'about 1786'
+    marriage -date 14-nov-1809 -location campbeltown -ref brown-marriage
+    child {
+        name John Galbreath
+        birth -date 8-oct-1811
+        baptism -date 10-oct-1811 -location campbeltown -ref john1811-baptism
+    }
+    child {
+        name Catharine Galbreath
+        birth -date 28-nov-1814
+        baptism -date 1-dec-1814 -location campbeltown -ref catharine1814-baptism
+    }
+    child {
+        name Euphemia Galbraith
+        birth -date 13-jan-1817 
+        baptism -date 14-jan-1817 -location campbeltown -ref euphemia1817-baptism
+        death -date 18-oct-1871 -location Kilbarchan,Renfrewshire -ref euphemia1817-death
+        partner {
+            name Malcolm Baxter
+            marriage -date 14-dec-1843 -location campbeltown -ref baxter-marriage
+        }
+    }
+    child {
+        name Catharine Galbraith
+        birth -date 5-aug-1819
+        baptism -date 6-aug-1819 -location campbeltown -ref catharine1819-baptism
+    }
+    child {
+        name Neil Galbreath
+        birth -date 22-may-1822
+        baptism -date 23-may-1822 -location campbeltown -ref neil1822-baptism
+    }
+}
+
+footnotes {
+    john1811-baptism {
+        $opr-ref[b-1811-507-000-0040-0376 "John Galbreath"]
+    }
+    brown-marriage {
+        $opr-ref-link[m-1809-507-000-0050-0088 "Neill Galbreath" "More Brown"]{
+           Neill Galbreath Sailor and More Brown Daughter to John Brown Carter
+           Campbeltown both of this Parish were married 14th November 1809. 
+        }
+    }
+    catharine1814-baptism {
+        $opr-ref[b-1814-507-000-0040-0422 "Catharine Galbreath"]
+    }
+    euphemia1817-baptism {
+        $opr-ref[b-1817-507-000-0040-0455 "Effy Galbreath"]
+    }
+    euphemia1817-death {
+        $sp-ref-link[d-1871-568-00-0100 0034 "Euphemia Galbraith"]
+    }
+    baxter-marriage {
+        $opr-ref-link[m-1843-507-000-0060-0393 "Euphemia Galbreath" "Malcolm Baxter"]{
+            Malcolm Baxter, Engineer, Drumlamble,  and Euphemia
+            Galbreath, daughter of the late Neill 
+            Galbreath, Mariner, both of this Parish
+            were married fourtenth Dec 1843
+        }
+    }
+    catharine1819-baptism {
+        $opr-ref[b-1819-507-000-0040-0496 "Catharine Galbreath"]
+    }
+    neil1822-baptism {
+        $opr-ref[b-1822-507-000-0060-0052 "Neill Galbreath"]
+        Image missing on Scotlands People.
+    }
+}
+
