5e63a71366438cb7bdb8fd2e378eb8b97d99c43a
nickg <nickg@client9.com>
2023-04-01 23:37:31 -0700

    update

--- people/galbreath-neil-1841-stewart.sh
+++ people/galbreath-neil-1841-stewart.sh
@@ -1,5 +1,7 @@
 name Neil Galbreath
-baptism -date 18-aug-1841  -location campbeltown
+birth -date 18-aug-1841
+baptism -date 30-dec-1841 -location campbeltown -ref neil1841-baptism
+death -date 21-apr-1873 -location Saddell -ref neil1841-death -note "by drowning / suicide"
 external {
     familysearch K466-KG9
 }
@@ -9,9 +11,31 @@ note {
 partner {
     name Martha Stewart
     marriage -date 31-dec-1861 -location glasgow
+
+    child {
+        name Agnes Galbraith
+        birth -date 23-jan-1863 -location Saddell -ref agnes1863-birth
+        death -date 28-oct-1886 -location campbeltown -ref agnes1863-death
+        burial -name Kilkerran
+        partner {
+            name Malcolm McNeilage
+        }
+    }
+    child {
+        name Neil Galbraith
+        birth -date 23-may-1869 -location campbeltown -ref neil1869-birth
+    }
+    child {
+        name Margaret Galbraith
+        birth -date 28-oct-1871 -location campbeltown -ref margaret1871-birth
+        death -date 13-jul-1873 -location campbeltown -ref margaret1871-death
+    }
 }
 footnotes {
     neil1841-baptism {
         $opr-ref-link[b-1841-507-000-0070-0210 "Neill Galbreath"]    
+    }
+    neil1841-death {
+        $sp-ref-link[d-1873-531-01-0006 0002 "Neil Galbraith"]
     }
 }
