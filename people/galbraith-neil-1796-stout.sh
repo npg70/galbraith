4ad5aa439aa008177a3aa652d4d1687457da0984
nickg <nickg@client9.com>
2023-04-14 22:28:49 -0700

    updates

---
+++ people/galbraith-neil-1796-stout.sh
@@ -0,0 +1,146 @@
+name Neil Galbraith
+birth -date 30-nov-1794 -location 'South Knapdale' -ref neil1794-baptism -note 'the son of Duncan Galbraith and Julia McMillan'
+death -date 11-aug-1879 -location Kilcalmonell -ref neil1794-death
+external {
+    familysearch M1YK-YRN
+}
+tags 'Kilcalmonell and Kilberry' Incomplete
+note {
+    Incomplete. Decendants of Duncan and John not investigated.  Daughters all got married.
+}
+body {
+He was a mason.
+}
+partner {
+    name Elisabeth Stout
+    birth -date 1818 -location Kilcalmonell
+    death -date 13-may-1894 -location renfrew,renfrewshire
+    marriage -date 28-nov-1840 -location 'Kilcalmonell and Kilberry' -ref stout-marriage
+
+    child {
+        name Duncan Galbraith
+        birth -date 26-jun-1841 
+        baptism -date 14-sep-1846 -location 'Kilcalmonell and Kilberry' -ref duncan1841-baptism
+        death -date 10-jul-1885 -location Greenock -ref duncan1841-death
+        partner {
+            name Mary ???
+        }
+    }
+    child {
+        name Betty Galbraith
+        birth -date 3-sep-1845
+        baptism -date 14-sep-1846  -location 'Kilcalmonell and Kilberry'  -ref betty1845-baptism
+    }
+    child {
+        name Mary Galbraith
+        baptism -date 24-jul-1847 -location 'Kilcalmonell and Kilberry' -ref mary1847-baptism
+    }
+    child {
+        name Jean Galbraith
+        baptism -date 5-aug-1850 -location 'Kilcalmonell and Kilberry' -ref jean1850-baptism
+    }
+    child {
+        name Julia Galbraith
+        baptism -date 5-aug-1850 -location 'Kilcalmonell and Kilberry' -ref julia1850-baptism
+    }
+    child {
+        name John Galbraith
+        baptism -date 1-jan-1854 -location 'Kilcalmonell and Kilberry' -ref john1854-baptism
+        death -date 4-mar-1921 -location Ayr,Ayr -ref john1854-death
+        partner {
+            name Agnes McClure
+        }
+    }
+}
+
+body {
+Census 1841, Scotland, Argyllshire, Kilcalmonell:$ref[census1841]
+
+$csvtable{
+First,Last,Age,Year,Place
+Neil, Galbraith,40, 1801, Argyllshire
+Betsy, Galbraith,,25, 1816, Argyllshire
+Betsy, Stout, 50, 1791, Argyllshire
+}
+
+Census 1851, Scotland, Argyllshire, Kintyre, Kilcalmonell:$ref[census1851]
+$csvtable{
+First,Last,Role,Age,Year,Place
+Neil, Galbraith,Head,50, 1801, S Knapdale
+Elizabeth, Galbraith,Wife,33,1818, Mull
+Duncan,Galbraith,Son,10,1841,Kilberry
+Elizabeth,Galbraith,Dua,5,1846,Kilberry
+Mary,Galbraith,Dau,3,1848,Kilberry
+Julia,Galbraith,Dau,0,1851,Kilberry
+Jean,Galbraith,Dau,0,1851,Kilcalmonel
+}
+
+Census 1861, Scotland, Argyllshire, Kilberry:$ref[census1861]
+listed as a married lodger
+
+Census 1871, Scotland, Argyllshire, Kilcalmonell:$ref[census1871]
+$csvtable{
+First,Last,Role,Age,Year,Place,Occupation
+Neil,Galbraith,Head,75,1796,Knapdale,Mason
+Betsy,Galbraith,Wife,53,1818,KILFINICHIN,-
+John,Galbraith,Son,17,1854,Kilcalmonell
+}
+}
+footnotes {
+    neil1794-baptism {
+        $opr-ref-link[b-1794-533-000-0010-0208 "Niell Galbreath"]
+        Near illegible.
+    }
+    neil1794-death {
+        $sp-ref-link[d-1879-516-01-0011 0004 "Neil Galbraith"]
+    }
+    census1841 {
+        https://www.findmypast.com/transcript?id=GBC%2F1841%2F0016614654
+    }
+    census1851 {
+        https://www.findmypast.com/transcript?id=GBC/1851/0019281353&expand=true
+    }
+    census1861 {
+        https://www.findmypast.com/transcript?id=GBC%2F1861%2F0022460157
+    }
+    census1871 {
+        https://www.findmypast.com/transcript?id=GBC/1871/0023464833
+    }   
+    stout-marriage {
+        $opr-ref[m-1840-516-000-0020-0394 "Niel Galbraith" "Elisabeth Stout"]
+    }
+    duncan1841-baptism {
+        $opr-ref-link[b-1841-516-000-0020-0360 "Duncan Galbraith"]{
+            Duncan Antenupital Son to Neil Galbraith Maison in South
+            Cuilgailtro and Elisabeth Stout their born
+            26th June 1841 Registered 14th September 1846
+        }
+    }
+    duncan1841-death {
+        $sp-ref-link[d-1885-564-03-0396 0132 "Duncan Galbraith"]
+        Has a correction.
+    }
+    betty1845-baptism {
+        $opr-ref-link[b-1845-516-000-0020-0360 "Betty Galbraith"]{
+            Betty Lawful daughter to Neil Galbraith Maison in South
+            Cuilgailtro and Elisabeth Stout their born
+            3 September 1845 Registered 14th September 1846
+        }
+    }
+    mary1847-baptism {
+        $opr-ref[b-1847-516-000-0020-0362 "Mary Galbraith"]
+    }
+    jean1850-baptism {
+        $opr-ref[b-1850-516-000-0020-0172 "Jean Galbraith"]
+    }
+    julia1850-baptism {
+        $opr-ref[b-1850-516-000-0020-0172 "Julia Galbraith"]
+    }
+    john1854-baptism {
+        $opr-ref[b-1854-516-000-0020-0186 "John Galbraith"]
+    }
+    john1854-death {
+        $sp-ref-link[d-1921-578-01-0115 0039 "John Galbraith"]
+    }
+}
+
---
+++ people/galbreath-john-1781-mcneil.sh
@@ -0,0 +1,73 @@
+name John Galbreath
+birth -date 'about 1781'
+external {
+
+}
+
+tags Colonsay Immigrant
+
+partner {
+    name Mary McNeil
+    birth -date 'about 1786'
+    death -date 1849
+
+    child {
+        name Flory Galbreath
+        baptism -date 2-jul-1810 -location colonsay -ref flory1810-baptism
+    }
+    child {
+        name Donald Galbreath
+        baptism -date 29-jun-1812 -location colonsay -ref donald1812-baptism
+    }
+    child {
+        name Lauchlan Galbreath
+        baptism -date 2-jul-1814 -location colonsay -ref lauchlan1814-baptism
+    }
+    child {
+        name Archibald Galbreath
+        baptism -date 22-jan-1817 -location colonsay -ref archibald1817-baptism
+    }
+    child {
+        name Jeny Galbreath
+        baptism -date 17-may-1818 -location colonsay -ref jeny1818-baptism
+    }
+    child {
+        name Jon Galbreath
+        baptism -date 31-jul-1819 -location colonsay -ref jon1819-baptism
+    }
+    child {
+        name Malcolm Galbreath
+        baptism -date 16-may-1820 -location colonsay -ref malcolm1820-baptism
+    }
+    child {
+        name Mary Galbreath
+        baptism -date 10-feb-1823 -location colonsay -ref mary1823-baptism
+    }
+}
+
+footnotes {
+    flory1810-baptism {
+        $opr-ref-link[b-1810-539-020-0010-0008 "Flory Galbreath"]
+    }
+    donald1812-baptism {
+        $opr-ref-link[b-1812-539-020-0010-0009 "Donald Galbreath"]
+    }
+    lauchlan1814-baptism {
+        $opr-ref-link[b-1814-539-020-0010-0010 "Lauchlan Galbreath"]
+    }
+    archibald1817-baptism {
+        $opr-ref-link[b-1817-539-020-0010-0012 "Archibald Galbreath"]
+    }
+    jeny1818-baptism {
+        $opr-ref-link[b-1818-539-020-0010-0013 "Jeny Galbreath"]
+    }
+    jon1819-baptism {
+        $opr-ref-link[b-1819-539-020-0010-0014 "John Galbreath"]
+    }
+    malcolm1820-baptism {
+        $opr-ref[b-1820-539-020-0020-0003 "Malcolm Galbreath"]
+    }
+    mary1823-baptism {
+        $opr-ref[b-1823-539-020-0020-0005 "Mary Galbreath"]
+    }
+}
