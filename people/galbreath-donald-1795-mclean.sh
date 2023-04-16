f22774983efd2be1efeb5ed6007cf398550711ff
nickg <nickg@client9.com>
2023-04-16 11:18:26 -0700

    updates

---
+++ people/galbreath-donald-1795-mclean.sh
@@ -0,0 +1,103 @@
+name Donald Galbreath
+birth -date 'say 1795' -location colonsay
+death -date 3-jan-1867 -location 'Elderslie Township, Bruce, Ontario, Canada'
+tags Colonsay Immigrant
+external {
+    familysearch KH1J-4Z6
+    findagrave 170628191
+}
+note {
+    This may be two different Donalds.  While there are no conflicts, there is also no
+    evidence that the Donald who married Rose McLean is the same one who married
+    Annie McLugash.
+}
+partner {
+    name Rose McLean
+    marriage -date 10-feb-1815 -location colonsay -ref mclean-marriage
+    death -date 1821
+    child {
+        name John Galbreath
+        baptism -date 26-feb-1816 -location colonsay -ref john1816-baptism
+    }
+    child galbreath-malcolm-1817-buie
+
+    child {
+        name Margat Galbreath
+        baptism -date 12-sep-1819 -location colonsay -ref margat1819-baptism
+    }
+    child {
+        name Cathrine Galbreath
+        baptism -date 24-sep-1821 -location colonsay -ref cathrine1821-baptism
+    }
+}
+partner {
+    name Annie McLugash
+    marriage -date 5-mar-1827 -location colonsay -ref mclugash-marriage
+    death -date 3-jan-1867 -location 'Bruce County, Ontario, Canada'
+    child {
+        name Angus Galbreath
+        baptism -date 7-may-1828 -location colonsay -ref angus1828-baptism
+        death -date 1880 -location 'Elderslie, Bruce, Ontario, Canada'
+        partner {
+            name Julia Ann Darrach
+            marriage -date 16-jun-1856 -location colonsay
+        }
+        body { 6 children in colonsay to 1868 }
+    }
+    child {
+        name Lachlain Galbreath
+        baptism -date 19-may-1833 -location colonsay -ref lachlain1833-baptism
+        body { Present in 1861 census }
+    }
+    child {
+        name Mary Galbreath
+        baptism -date 20-jun-1836 -location colonsay -ref may1836-baptism
+        death -date 1823 -location 'Saskatchewan, Canada'
+    }
+    child {
+        name Margaret Galbreath
+        birth -date 'about 1840'
+    }
+    child {
+        name Sarah Marion Galbreath
+        birth -date 'about 1840'
+        death -date 18-apr-1871 -location 'Arran, Bruce, Ontario, Canada'
+    }
+    child {
+        name Anne Galbreath
+        baptism -date 5-may-1844 -location colonsay -ref anne1844-baptism
+    }
+}
+footnotes {
+    mclean-marriage {
+        $opr-ref[m-1815-539-020-0010-0018 "Donald Galbreath" "Rose McLean"]
+    }
+    john1816-baptism {
+        $opr-ref[b-1816-539-020-0010-0011 "John Galbreath"]
+    }
+    margat1819-baptism {
+        $opr-ref[b-1819-539-020-0010-0014 "Margat Galbreath"]
+    }
+    cathrine1821-baptism {
+        $opr-ref[b-1821-539-020-0020-0005 "Cathrine Galbreath"]
+    }
+    mclugash-marriage {
+        $opr-ref-link[m-1827-539-020-0020-0027 "Donald Galbraith" "Annie McLugash"]{
+            March 5 | Donald Galbraith + Annie McLugash
+        }
+    }
+    angus1828-baptism {
+        $opr-ref[b-1828-539-020-0020-0007 "Angus Galbreath"]
+    }
+    lachlain1833-baptism {
+        $opr-ref[b-1833-539-020-0020-0009 "Lachlain Galbreath"]
+    }
+    mary1836-baptism {
+        $opr-ref-link[b-1836-539-020-0020-0011 "Mary Galbreath"]{
+            June 20 | Mary Daughter of Don'd and $i{blank}
+        }
+    }
+    anne1844-baptism {
+        $opr-ref[b-1844-539-020-0020-0014 "Annie Galbreath"]
+    }
+}
