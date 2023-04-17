afe2e31eeed187dda8a72a2b018b4d7c733e755c
nickg <nickg@client9.com>
2023-04-17 11:13:27 -0700

    updates

---
+++ people/galbraith-angus-1791-mcneil.sh
@@ -0,0 +1,155 @@
+name Angus Galbreath
+tags Colonsay Immigrant
+birth -date 'about 1791'
+death -date nov-1851 -location 'ontario,canada'
+external {
+    familysearch LDMP-K33
+}
+note {
+    TODO: Add Jura Census 1841
+    https://www.findmypast.com/transcript?id=GBC/1841/0016655781&expand=true
+    and also free on FamilySearch
+}
+note {
+    FamilySearch for Catherine has details of Canada Census 1861 and 1871
+    https://www.familysearch.org/tree/person/details/LD98-D4R
+}
+body {
+    Looks like the entire family moved to Ontario before 1851
+}
+partner {
+    name Catherine McNeil
+    birth -date 'about 1796'
+    marriage -date 6-jul-1811 -location colonsay -ref mcneil-marriage
+    child {
+        name Janet/Jeny Galbreath
+        baptism -date 29-jun-1812 -location colonsay -ref janet1812-baptism
+        death -date 5-oct-1885 -location 'Elderslie, Bruce, Ontario, Canada'
+        partner {
+            name Angus McFale
+            marriage -date 1-jul-1837 -location colonsay
+        }
+    }
+    child {
+        name Mary Galbreath
+        baptism -date 1-feb-1814 -location colonsay -ref mary1814-bpatism
+        death -date 24-sep-1881 -location 'ontario,canada'
+        partner {
+            name Malcolm McEachern
+            marriage -date 10-mar-1838 -location colonsay
+        }
+    }
+    child {
+        name Catherine Galbreath
+        baptism -date 20-jul-1816 -location colonsay -ref cathrine1816-baptism
+        partner {
+            name James McNeil
+            marriage -date 1-jul-1837 -location colonsay
+        }
+    }
+    child {
+        name Margrat Galbreath
+        baptism -date 10-feb-1819 -location jura -ref margrat1819-baptism
+        death -date 26-feb-1895 -location 'Elderslie, Bruce, Ontario, Canada'
+        partner {
+            name Neil McNeil
+            marriage -date 8-jul-1837 -location colonsay
+        }
+    }
+    child {
+        name Haster/Ester Galbreath
+        baptism -date 3-jun-1821 -location colonsay -ref haster1821-baptism
+        death -date 11-oct-1894 -location 'Paisley, Bruce, Ontario, Canada'
+        partner {
+            name John Archabald McPhee
+            marriage -date 6-jun-1845 -location colonsay
+        }
+    }
+    child {
+        name Donald Galbreath
+        baptism -date 3-apr-1824 -location colonsay -ref donald1824-baptism
+        death -date 14-sep-1876 -location 'Elderslie, Bruce, Ontario, Canada'
+        partner {
+            name Christina Campbell
+            marriage -date 1855 -location  'Elderslie, Bruce, Ontario, Canada'
+        }
+    }
+    child {
+        name Angus Galbreath
+        baptism -date 11-apr-1827 -location colonsay -ref angus1827-baptism
+        death -date 25-aug-1909 -location 'Victoria, British Columbia, Canada'
+        partner {
+            name Mary McNeil
+            marriage -date 1857 -location 'Bruce,Ontario,Canada'
+        }
+    }
+    child {
+        name Suesay Galbreath
+        baptism -date 26-jul-1830 -location colonsay -ref suesay-1830-baptism
+        death -date 9-apr-1876 -location 'Elderslie, Bruce, Ontario, Canada'
+        partner {
+            name Donald Brown
+            marriage -date 1859 -location 'Bruce,Ontario,Canada'
+        }
+    }
+    child {
+        name Archibald Galbreath
+        birth -date 1833 -location colonsay
+        death -date 8-nov-1904 -location 'Vancouver, British Columbia, Canada'
+        partner {
+            name Sarah Smith
+            marriage -date 26-jan-1865 -location 'Bruce,Ontario,Canada'
+        }
+    }
+    child {
+        name  Malcolm Galbreath
+        baptism -date 27-Sep-1835 -location Colonsay -ref malcom1835-baptism
+        death -date 1876 -location 'Greenwood, Kansas, USA'
+        partner {
+            name Jennet McNeil
+            marriage -date 30-jan-1868 -location 'Paisley,Bruce,Ontario,Canada'
+        }
+    }
+    child {
+        name Alexander Galbreath
+        baptism -date 12-aug-1841 -location Colonsay -ref alexander1841-baptism
+        death -date 16-jul-1869 -location 'Elderslie, Bruce, Ontario, Canada'
+    }
+}
+
+footnotes {
+    mcneil-marriage {
+
+    }
+    janet1812-baptism {
+        $opr-ref[b-1812-539-020-0010-0009 "Jeny Galbreath"]
+    }
+    mary1814-baptism {
+        $opr-ref[b-1814-539-020-0010-0010 "Mary Galbreath"]
+    }
+    cathrine1816-baptism {
+        $opr-ref[b-1816-539-020-0010-0012 "Cathrine Galbreath"]
+    }
+    margrat819-baptism {
+        $opr-ref[b-1819-539-010-0010-0117 "Margrat Galbreath"]
+    }
+    haster1821-baptism {
+        $opr-ref[b-1821-539-020-0020-0004 "Haster Galbreath"]
+    }
+    donald1824-baptism {
+        $opr-ref[b-1824-539-020-0020-0005 "Donald Galbreath"]
+    }
+    angus1827-baptism {
+        $opr-ref[b-1827-539-020-0020-0006 "Angus Galbreath"]
+    }
+    suesay1830-baptism {
+        $opr-ref[b-1830-539-020-0020-0008 "Suesay Galbreath"]
+    }
+    malcolm1835-baptism {
+        $opr-ref[b-1835-539-020-0020-0011 "Malcolm Galbreath"]
+    }
+    alexander1841-baptism {
+        $opr-ref[b-1841-539-020-0020-0013 "Alexander Galbreath"]
+    }
+}
+    
