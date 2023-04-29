1420e09342a1ebe1c91509c20eb80749beca42d6
nickg <nickg@client9.com>
2023-04-29 15:20:31 -0700

    additions

---
+++ people/galbraith-duncan-1836-scott.sh
@@ -0,0 +1,104 @@
+name Duncan Galbraith
+birth -date 'about 1840' 
+death -date 9-jan-1910 -location Kilcalmonell -ref duncan1836-death
+note {
+    Birth appears to be unrecorded or lost.
+}
+tags 'Kilcalmonell and Kilberry' 
+
+body {
+Listed as  joiner in 
+1871 Census, 9, East Blackhall Street, Greenock, East Greenock, Renfrewshire, Scotland:$ref[census1871]
+$csvtable{
+First,Last,Role,Age,Year,Place
+Duncan,Galbraith,Head,30,1841,Kilberry
+Catherine,Galbraith,Wife,25,1846,Kilmartin
+Isbella Mc...,Galbraith,Dau,1,1870,Greenock
+}
+Listed as a Wright in Census 1881, Whitehouse, Kilcalmonell, Argyllshire, Scotland:$ref[census1881]
+$csvtable{
+First,Last,Role,Age,Year,Place
+Duncan,Galbraith,Head,40,1841,Kilberry
+Catherine,Galbraith,Wife,35,1846,Kilmartin
+Daniel,Galbraith,Son,8,1873,Greenock
+William,Galbraith,Son,6,1875,Kilcalmonell
+John,Galbraith,Son,3,1878,Kilcalmonell
+}
+
+Listed as a Widower and "Wright & Miller" in Census 1891, Barneigh Cottage White House, Kilcalmonell, Argyllshire, Scotland:$ref[cenus1891]
+
+$csvtable{
+First,Last,Role,Age,Year,Place
+Duncan,Galbraith,Head,51,1840,Kilberry
+Mary,Scott,Sister-in-law,35,1856,Kilmartin
+Daniel,Galbraith,Son,18,1873,Greenock
+William,Galbraith,Son,16,1875,Kilcalmonell
+John,Galbraith,Son,13,1878,Kilcalmonell
+}
+
+Listed as a Widower and a Wright in Census 1901, (Whitehouse), Public Road, Kilcalmonell, Argyllshire, Scotland
+$csvtable{
+First,Last,Role,Age,Year,Place
+Duncan,Galbraith,Head,61,1840,South Knapdale
+Mary,Scott,Sister-in-law,50,1851,Kilmartin
+Donald,Galbraith,Son,27,1874,East Parish Greenock
+Peter,Campbell,Nephew,28,1873,Kilmichael
+John,Galbraith,Nephew,18,1884,South Knapdale
+}
+
+}
+partner {
+    name Catherine Scott
+    birth -date 'about 1846'
+    marriage -date 1-jul-1868 -location Greenock -ref scott-marriage
+    death -date 5-apr-1884 -location kilcalmonell -ref scott-death
+    child {
+        name Isabella Galbraith
+        birth -date 1869 -location greenock -ref isabella1869-birth
+        death -date 1873 -location greenock -ref isabella1869-death
+    }
+    child {
+        name Donald Galbraith
+        birth -date 1872 -location greenock -ref donald1872-birth
+    }
+    child galbraith-william-1875-thomson
+    child {
+        name John Galbraith
+        birth -date 'about 1878' -location kilcalmonell
+    }
+}
+footnotes {
+    census1871 {
+        https://www.findmypast.com/transcript?id=GBC/1871/0022934767
+    }
+    census1881 {
+        https://www.findmypast.com/transcript?id=GBC/1881/0029366085&expand=true
+    }
+    census1891 {
+        https://www.findmypast.com/transcript?id=GBC/1891/0035207994&expand=true
+    }
+    census1901 {
+        https://www.findmypast.com/transcript?id=GBC/1901/0035894118&expand=true
+    }
+    duncan1836-death  {
+        $sp-ref-link[d-1910-516-00-0001 0001 "Duncan Galbraith"]
+    }
+    scott-marriage {
+        $sp-ref-link[m-1868-564-01-0061 0031 "Duncan Galbraith" "Catherine Scott"]
+    }
+    scott-death {
+        $sp-ref-link[d-1884-516-01-0005 0002 "Catherine Galbraith"]
+        lists parents as William Scott (mason) and Isabella McIntyre, both deceased.
+        age 38
+    }
+    isabella1869-birth {
+        $sp-ref[b-1869-564-02-0706 "Isabella McInyr... Galbraith"]
+    }
+    isabella1869-death {
+        $sp-ref[b-1873-564-02-0104 "Isabella McInty... Galbraith"]
+    }
+    donald1872-birth {
+        $sp-ref[b-1872-564-02-0509 "Donald Galbraith"]
+    }
+}
+
---
+++ people/galbraith-william-1875-thomson.sh
@@ -0,0 +1,33 @@
+name William Galbraith
+birth -date 24-mar-1875 -location Kilcalmonell -ref william1875-birth
+tags 'Kilcalmonell and Kilberry' Immigrant
+external {
+
+}
+todo {
+    Census 1881, 1891, 1901
+}
+note {
+Low presence in Ancestry.com and FamilySearch.  Mostly listed as "William Scott Galbraith," and often listed with mother of Catherine Galbreath, or Catherine Brown (not Scott).  At least here parents are clearly indicated as Donald Galbraith and Catherine Scott.  There is no other William born 1875 in Kilcalmonell.
+}
+
+body {
+    Reverend?
+
+    He emmigrated to Canada in 1905 or 1907.
+}
+
+partner {
+    name Majorie MacKenzie Thomson
+    child {
+        name Sophia Frederica Nicol 
+        birth -date 1913 -location Thamesford,Ontario,Canada
+    }
+}
+
+footnotes {
+    william1876-birth {
+        $sp-ref-link[b-1875-516-01-0007 0003 "William Galbraith"]
+    }
+}
+
