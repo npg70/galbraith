f03d95139ccb0ea141e4663ff49bd4134b7f9626
nickg <nickg@client9.com>
2023-07-29 11:15:24 -0700

    updates

---
+++ people/galbraith-archibald-1831-mclean.sh
@@ -0,0 +1,49 @@
+name Archibald Galbraith
+tags 'kilcalmonell and kilberry' campbeltown
+external {
+    familysearch KZ6S-1WF
+}
+
+partner {
+    name Isabella McDiarmid
+    marriage -date 1858 -location campbeltown
+
+    child {
+        name Mary Galbraith
+        birth -date 1860
+    }
+
+    child {
+        name Jane Galbraith
+    }
+    child {
+        name Marion Galbraith
+        birth -date 1863
+    }
+    child galbraith-archibald-1865-mckay
+
+    child {
+        name Peter McDemaid Galbraith
+        birth -date 1867
+        death -date 1935
+    }
+    child {
+        name Agnes Galbraith
+        birth -date 1869
+    }
+    child {
+        name Christina Galbraith
+        birth -date 1871
+        death -date 1928
+    }
+    child {
+        name Donald Galbraith
+        birth -date 1873
+    }
+}
+
+footnotes {
+
+
+
+}
---
+++ people/galbraith-archibald-1865-mckay.sh
@@ -0,0 +1,19 @@
+name Archibald Galbraith
+external {
+    familysearch M1YS-YRV
+    wikitree Galbraith-2459
+    findagrave 179352175
+}
+birth -date 1-jul-1865 -location campbeltown
+death -date 25-mar-1928 -location campbeltown
+
+partner {
+    name Isabella McKay 
+    birth -date 1864
+    death -date 1937
+    
+
+}
+footnotes {
+}
+
