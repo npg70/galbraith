e34b2f6d3732016b2e6e35bbbdee0ce5fbd55c06
nickg <nickg@client9.com>
2023-07-29 22:58:44 -0700

    additions

--- people/galbraith-archibald-1865-mckay.sh
+++ people/galbraith-archibald-1865-mckay.sh
@@ -13,6 +13,39 @@ partner {
     death -date 8-jul-1937 -location campbeltown -ref mckay-death
     marriage -date 16-apr-1888 -location campbeltown -ref mckay-marriage 
 
+    child {
+        name Daniel Galbraith
+        birth -date 29-oct-1888 -location campbeltown -ref daniel1888-birth
+        death -date 1942
+        partner {
+            name Maggie Sinclair McMichael
+            birth -date 1898
+            death -date 1968
+        }
+    }
+    child {
+        name Mary McMillan Galbraith
+        birth -date 15-mar-1890 -location campbeltown -ref mary1890-birth
+    }
+    child {
+        name Isabella McDermid Galbraith
+        birth -date 7-oct-1891 -location campbeltown -ref isabella1891-birth
+        death -date 1967 -location campbeltown -ref isabella1891-death
+        partner {
+            name Neil McMillan
+            marriage -date 28-jun-1912 -location campbeltown
+        }
+    }
+    child {
+        name Archibald Galbraith
+        birth -date 17-may-1893 -location campbeltown -ref archibald1893-birth
+    }
+    child {
+        name  William John McKay Galbraith
+        birth -date 30-jul-1895 -location campbeltown -ref william1895-birth
+        death -location Australia 
+    }
+
     child galbraith-james-duncan-1897
 
     child {
@@ -42,7 +75,24 @@ footnotes {
     mckay-death {
         $sp-ref-link[d-1937-507-00-0073 0025 "Isabella Galbraith"]
     }
-
+    daniel1888-birth {
+        $sp-ref-link[b-1888-507-00-0327 0109 "Daniel Galbraith"]
+    }
+    mary1890-birth {
+        $sp-ref-link[b-1890-507-00-0063 0021 "Mary McMillan Galbraith"]
+    }
+    isabella1891-birth {
+        $sp-ref-link[b-1891-507-00-0269 0090 "Isabella McDermid Galbraith"]
+    }
+    isabella1891-death {
+        $sp-ref[d-1967-507-00-0091 "Isabella McDermid Galbraith"]
+    }
+    archibald1893-birth {
+        $sp-ref-link[b-1893-507-00-0156 0052 "Archibald Galbraith"]
+    }
+    william1895-birth {
+        $sp-ref-link[b-1895-507-00-0211 0071 "William John McKay Galbraith"]
+    }
     allan1899-birth {
         $sp-ref[b-1899-507-00-0147 "Allan Galbraith"]
     }
