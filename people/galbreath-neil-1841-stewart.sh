cd430a24bf73da4c17a26c0e545afab686fe74a1
nickg <nickg@client9.com>
2023-07-30 16:28:55 -0700

    add references

--- people/galbreath-neil-1841-stewart.sh
+++ people/galbreath-neil-1841-stewart.sh
@@ -6,11 +6,12 @@ external {
     familysearch K466-KG9
 }
 todo {
-    Stub - missing references.
+    Children need splitting out
 }
 partner {
     name Martha Stewart
-    marriage -date 31-dec-1861 -location glasgow
+    birth -date 'about 1841'
+    marriage -date 31-dec-1861 -location glasgow -ref stewart-marriage
 
     child {
         name Agnes Galbraith
@@ -19,6 +20,24 @@ partner {
         burial -name Kilkerran
         partner {
             name Malcolm McNeilage
+        }
+    }
+    child {
+        name John Galbraith
+        birth -date 19-nov-1864 -location Saddell -ref john1864-birth
+        death -date 10-may-1932 -location 'at sea' -ref john1864-death
+        partner {
+            name Isabella McTaggart
+            marriage -date 22-jul-1887 -location campbeltown
+        }
+    }
+    child {
+        name Martha Galbraith
+        birth -date 25-mar-1867 -location Saddell -ref martha1867-birth
+        death -date 31-jul-1939 -location Campbeltown -ref martha1867-death
+        partner {
+            name Robert McBain
+            marriage -date 31-dec-1884 -location campbeltown -ref mcbain-marriage
         }
     }
     child {
@@ -38,4 +57,36 @@ footnotes {
     neil1841-death {
         $sp-ref-link[d-1873-531-01-0006 0002 "Neil Galbraith"]
     }
+
+    stewart-marriage {
+        $sp-rf-link[m-1861-644-01-0517 0259 "Neill Galbraith" "Marta Stewart"]
+    }
+
+    agnes1863-birth {
+        $sp-ref[b-1863-531-01-0005 "Agnes Galbreath"]
+    }
+    agnes1863-death {
+        $sp-ref-link[d-1886-507-00-0138 0046 "Agnes Galbraith"]
+    }
+    john1864-birth {
+        $sp-ref-link[b-1864-531-01-0026 0009 "John Galbreath"]
+    }
+    john1864-death {
+        marine return: "1932 GALBRAITH, JOHN (Statutory registers Deaths 030/MR 260)"
+    }
+    martha1867-birth {
+        $sp-ref-link[b-1867-531-01-0005 0002 "Martha Galbraith"]
+    }
+    martha1867-death {
+        $sp-ref[d-1939-507-00-0060 "Martha Galbraith"]
+    }
+    neil1869-birth {
+        $sp-ref[b-1869-507-00-0095 "Neil Galbraith"]
+    }
+    margaret1871-birth {
+        $sp-ref[b-1871-507-00-0246 "Margaret Galbraith"]
+    }
+    margaret1871-death {
+        $sp-ref-link[d-1873-507-00-0146 0049 "Margaret Galbraith"]
+    }   
 }
