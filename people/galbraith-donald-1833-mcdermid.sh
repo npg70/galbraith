10e36c5a179246244e0bebeafa821936762e852b
nickg <nickg@client9.com>
2023-07-29 21:43:46 -0700

    fix and add references

---
+++ people/galbraith-donald-1833-mcdermid.sh
@@ -0,0 +1,109 @@
+name Donald Galbraith
+birth -date nov-1831
+baptism -date 14-mar-1832 -location 'kilcalmonell and kilberry' -ref donald1831-birth
+death -date 19-aug-1901 -location campbeltown -ref donald1831-death
+
+tags 'kilcalmonell and kilberry' campbeltown
+external {
+    familysearch KZ6S-1WF
+}
+
+partner {
+    name Isabella McDermid
+    marriage -date 7-sep-1858 -location campbeltown -ref mcdermid-marriage
+    birth -date 'about 1838'
+    death -date 5-sep-1880 -location campbeltown -ref mcdermid-death
+
+    child {
+        name Mary Galbraith
+        birth -date 16-feb-1860 -location campbeltown -ref mary1860-birth
+    }
+
+    child {
+        name Jane Galbraith
+    }
+    child {
+        name Marion Galbraith
+        birth -date 28-sep-1863
+    }
+
+    child galbraith-archibald-1865-mckay
+
+    child {
+        name Peter McDemaid Galbraith
+        birth -date 4-Jun-1867 -location campbeltown -ref peter1867-birth
+        death -date 15-jun-1935 -location govan -ref peter1867-death
+    }
+    child {
+        name Agnes Galbraith
+        birth -date 8-apr-1869 -location campbeltown -ref agnes1869-birth
+    }
+    child {
+        name Christina Galbraith
+        birth -date 3-mar-1871 -location campbeltown -ref christina1871-birth
+        death -date 10-aug-1928 -location 'East Rochester, Monroe, New York, USA'
+        partner {
+            name Michael Gilchrist
+            marriage -date 12-jul-1898 -location campbeltown
+            birth -date 1877
+            death -date 1951
+        }
+    }
+    child {
+        name Donald Galbraith
+        birth -date 22-feb-1873 -location campbeltown -ref donald1873-birth
+    }
+    child {
+        name Isabella Galbraith
+        birth -date 1880 -location campbeltown -ref isabella1880-birth
+        death -date 11-jun-1880 -location campbeltown -ref isabella1880-death
+    }
+}
+
+footnotes {
+    donald1831-baptism {
+        $opr-ref-link[b-1831-516-000-020-0328 "Donald Galbraith"]{
+            Donald lawful son to Archibald Galbraith and
+            Mary MacLean in Crothshegan born in Nov 1831
+            Registered the 14th March 1832.
+        }, nonstandard indexing in Scotlands People. Normally it's based on baptism date, but here it is indexed by birth date under 31-Oct-1831.
+    }
+    donald1831-death {
+        $sp-ref-link[d-1901-507-00-0098 0033 "Daniel Galbraith"]{
+            No 98; Name: Daniel Galbraith, Seaman, Merchant, Widower of Isabella McDermid;
+            Date: 1901 August Nineteenth;
+            Age: 40;
+            Father: Archibald Galbraith, Seaman (deceased);
+            Mother: Annie McLean (deceased);
+            Present: Peter Galbraith, Son;
+        }
+    }
+    mcdermid-marriage {
+        $sp-ref-link[m-1858-507-00-0029 0015 "Donald Galbraith" "Isabella McDermid"]
+    }
+    mcdermid-death {
+        $sp-ref-link[d-1880-507-00-0163 0055 "Isabella Galbraith"]
+    }
+    mary1860-birth {
+        $sp-ref[b-1860-507-00-0038 "Mary Galbraith"]
+    }
+
+    peter1867-birth {
+        $sp-ref-link[b-1867-507-00-0109 0037 "Peter McDermid Galbraith"]
+    }
+    agnes1869-birth {
+        $sp-ref[b-1869-507-00-0062 "Agnes Galbraith"]
+    }
+    christina1871-birth {
+        $sp-ref[b-1871-507-00-0064 "Christina McDermind"]
+    }
+    donald1873-birth {
+        $sp-ref[b-1873-507-00-0064 "Donald Galbraith"]
+    }
+    isabella1880-birth {
+        $sp-ref[b-1880-507-00-0164 "Isabella Galbraith"]
+    }
+    isabella1880-death {
+        $sp-ref-link[d-1880-507-00-0122 0055 "Isabella Galbraith"]
+    }
+}
