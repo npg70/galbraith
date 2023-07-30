3c8f16884288755d3d274255524d474d6fc51fe8
nickg <nickg@client9.com>
2023-07-29 21:04:03 -0700

    add ref

--- people/galbraith-alexander-1854-campbell.sh
+++ people/galbraith-alexander-1854-campbell.sh
@@ -11,11 +11,14 @@ todo {
 partner {
     name Mary Campbell
     marriage -date 18-oct-1877 -location saddell
-    death -date 18-nov-1950
+    death -date 18-nov-1950 -location saddell -ref campbell-death
 }
 
 footnotes {
     alexander1854-baptism {
         $opr-ref-link[b-1854-507-000-0070-0437 "Alexander Galbraith"]
+    }
+    campbell-death {
+        $sp-ref-link[d-1950-531-01-0007 0003 "Mary Galbraith"]
     }
 }
