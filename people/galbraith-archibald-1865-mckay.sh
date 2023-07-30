870bce7c5c9c27c95e680228e376b7362d9e2ca2
nickg <nickg@client9.com>
2023-07-29 23:13:49 -0700

    updates

--- people/galbraith-archibald-1865-mckay.sh
+++ people/galbraith-archibald-1865-mckay.sh
@@ -16,9 +16,10 @@ partner {
     child {
         name Daniel Galbraith
         birth -date 29-oct-1888 -location campbeltown -ref daniel1888-birth
-        death -date 1942
+        death -date 17-dec-1942 -location campbeltown -ref daniel1888-death
         partner {
             name Maggie Sinclair McMichael
+            marriage -date 1923 -location campebeltown -ref mcmichael-marriage
             birth -date 1898
             death -date 1968
         }
@@ -77,6 +78,12 @@ footnotes {
     }
     daniel1888-birth {
         $sp-ref-link[b-1888-507-00-0327 0109 "Daniel Galbraith"]
+    }
+    daniel1888-death {
+        $sp-ref-link[d-1942-507-00-0117 0039 "Daniel Galbraith"]
+    }
+    mcmichael-marriage {
+        $sp-ref[m-1923-507-00-0022 "Daniel Galbraith" "Margaret Sinclair McMichael"]
     }
     mary1890-birth {
         $sp-ref-link[b-1890-507-00-0063 0021 "Mary McMillan Galbraith"]
