0a838104c92350eb37db9a6d0daad9cd37851c3d
nickg <nickg@client9.com>
2023-07-31 21:04:04 -0700

    typos

--- people/galbraith-archibald-campbell-1883-maynard.sh
+++ people/galbraith-archibald-campbell-1883-maynard.sh
@@ -4,7 +4,7 @@ external {
     familysearch GCM9-JG8
 }
 birth -date 18-oct-1883 -location 'Govan,Plantation,Larnark' -ref archibald1883-birth
-death -date 5-feb-1950 -location 'Los Angles, California, USA'
+death -date 5-feb-1950 -location 'Los Angeles, California, USA'
 note {
     Commonly listed as "Archibald Campbell Galbraith," although I've seen no evidence of the use of the middle name.
 }
@@ -18,12 +18,12 @@ partner {
     death -date 24-oct-1971 -location 'Redwood City,San Mateo,California,USA'
     child {
         name Donald M Galbraith
-        birth -date 1917
+        birth -date 1917 -location USA
         death -date 1997
     }
     child {
         name Jane Wallace Galbraith
-        birth -date 1922
+        birth -date 1922 -location USA
         death -date 2002
     }
 }
