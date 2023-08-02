ca6a07bface92454cc6e5cc928e78be30ced33f7
nickg <nickg@client9.com>
2023-08-01 23:24:56 -0700

    updates

--- people/galbraith-alexander-1854-campbell.sh
+++ people/galbraith-alexander-1854-campbell.sh
@@ -13,12 +13,16 @@ partner {
     marriage -date 18-oct-1877 -location saddell
     death -date 18-nov-1950 -location saddell -ref campbell-death
     child {
-        name Margaret Galbraith
-        birth -date 1878 -location Saddell -ref margaret1878-birth
+        name Archibald Galbraith
+        birth -date 8-aug-1877 -location Saddell -ref archibald1879-birth
     }
     child {
-        name Archibald Galbraith
-        birth -date 8-aug-1879 -location Saddell -ref archibald1879-birth
+        name Margaret Galbraith
+        birth -date 1878 -location Saddell -ref margaret1878-birth
+        death -date 21-may-1952 -location campbeltown -ref margaret1878-death
+        partner {
+            name John Paterson
+        }
     }
     child {
         name Robert Galbraith
@@ -29,7 +33,7 @@ partner {
         birth -date 1883 -location Saddell -ref mary1883-birth
     }
     child {
-        name John Galbraith
+        name John Alexander Galbraith
         birth -date 1885 -location Saddell -ref john1885-birth
     }
     child {
@@ -37,16 +41,42 @@ partner {
         birth -date 1887 -location Saddell -ref alexander1887-birth
     }
     child {
-        name William Galbraith
+        name William Cook  Galbraith
         birth -date 1888 -location Saddell -ref william1888-birth
     }
     child {
-        name Annie Galbraith
+        name Annie Campbell Galbraith
         birth -date 1890 -location Saddell -ref annie1890-birth
     }
     child {
         name Jemima Galbraith
         birth -date 1892 -location Saddell -ref jemima1892-birth
+    }
+    child {
+        name Catherine Campbell Galbraith
+        birth -date 1895 -location Saddell -ref catherine1895-birth
+        partner {
+            name Angus Wilkieson
+        }
+    }
+    child {
+        name Ellen Elizabath Galbraith
+        birth -date 1897 -location Saddell -ref elizabeth1897-birth
+    }
+    child {
+        name Elizabeth Jessie Galbraith
+        birth -date 1899 -location Saddell -ref elizabeth1899-birth
+        partner {
+            name Duncn McIntosh
+        }
+    }
+    child {
+        name Jessie McKinnon Galbraith
+        birth -date 2-apr-1903 -location Saddell -ref jessie1903-birth
+        death -date 10-apr-1955 -location Carradale -ref jessie1955-death
+        partner {
+            name John Mitchell Sillars
+        }
     }
 }
 
@@ -84,5 +114,16 @@ footnotes {
     jemima1892-birth {
         $sp-ref[b-1892-531-01-0021 "Jemima Galbraith"]
     }
-
+    catherine1895-birth {
+        $sp-ref[b-1895-531-01-0005 "Catherine Campbell Galbraith"]
+    }
+    ellen1897-birth {
+        $sp-ref[b-1897-531-01-0002 "Ellen Elizabeth Galbraith"]
+    }
+    elizabeth1899-birth {
+        $sp-ref[b-1899-531-01-0001 "Elizabeth Jessie Galbraith"]
+    }
+    jessie1903-birth {
+        $sp-ref[b-1903-531-01-0006 "Jessie McKinnon Galbraith"]
+    }
 }
