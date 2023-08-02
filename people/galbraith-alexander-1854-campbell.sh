6df0285e3c92b14c5cc7eea6b8369a996abaa88f
nickg <nickg@client9.com>
2023-08-01 23:07:33 -0700

    update

--- people/galbraith-alexander-1854-campbell.sh
+++ people/galbraith-alexander-1854-campbell.sh
@@ -12,6 +12,42 @@ partner {
     name Mary Campbell
     marriage -date 18-oct-1877 -location saddell
     death -date 18-nov-1950 -location saddell -ref campbell-death
+    child {
+        name Margaret Galbraith
+        birth -date 1878 -location Saddell -ref margaret1878-birth
+    }
+    child {
+        name Archibald Galbraith
+        birth -date 8-aug-1879 -location Saddell -ref archibald1879-birth
+    }
+    child {
+        name Robert Galbraith
+        birth -date 1881 -location Saddell -ref robert1881-birth
+    }
+    child {
+        name Mary Galbraith
+        birth -date 1883 -location Saddell -ref mary1883-birth
+    }
+    child {
+        name John Galbraith
+        birth -date 1885 -location Saddell -ref john1885-birth
+    }
+    child {
+        name Alexander Galbraith
+        birth -date 1887 -location Saddell -ref alexander1887-birth
+    }
+    child {
+        name William Galbraith
+        birth -date 1888 -location Saddell -ref william1888-birth
+    }
+    child {
+        name Annie Galbraith
+        birth -date 1890 -location Saddell -ref annie1890-birth
+    }
+    child {
+        name Jemima Galbraith
+        birth -date 1892 -location Saddell -ref jemimam1892-birth
+    }
 }
 
 footnotes {
@@ -21,4 +57,32 @@ footnotes {
     campbell-death {
         $sp-ref-link[d-1950-531-01-0007 0003 "Mary Galbraith"]
     }
+    margaret1878-birth {
+        $sp-ref[b-1878-531-01-0002 "Margaret Galbraith"]
+    }
+    archibald1879-birth {
+        $sp-ref-link[b-1879-531-01-0016 0006 "Archibald Galbraith"]
+    }
+    robert1881-birth {
+        $sp-ref[b-1881-531-01-0025 "Robert Galbratih"]
+    }
+    mary1883-birth {
+        $sp-ref[b-1883-531-01-0020 "Mary Galbraith"]
+    }
+    john1885-birth {
+        $sp-ref[b-1885-531-01-0009 "John Alexander"]
+    }
+    alexander1887-birth {
+        $sp-ref[b-1887-531-01-0003 "Alexander Galbraith"]
+    }
+    william1888-birth {
+        $sp-ref[b-1888-531-01-0026 "William Cook Galbraith"]
+    }
+    annie1890-birth {
+        $sp-ref[b-1890-531-01-0012 "Annie Campbell Galbraith"]
+    }
+    jemima1892-birth {
+        $sp-ref[b-1892-531-01-0021 "Jemima Galbraith"]
+    }
+
 }
