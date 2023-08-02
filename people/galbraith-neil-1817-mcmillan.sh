af490cd6503a52885860b76737ed0e2a3e987cb9
nickg <nickg@client9.com>
2023-08-01 17:16:08 -0700

    children

--- people/galbraith-neil-1817-mcmillan.sh
+++ people/galbraith-neil-1817-mcmillan.sh
@@ -4,9 +4,42 @@ death -date 23-mar-1874 -location Saddell -ref neil1817-death
 partner {
     name Isabella McMillan
     marriage -date 5-feb-1856  -location saddell -ref mcmillan-marriage
+
+    child {
+        name Isabella Galbreath
+        birth -date 1858 -location saddell -ref isabella1858-birth
+        death -date 1858 -location saddell -ref isabella1858-death
+    }
+    child {
+        name Margaret Galbreath
+        birth -date 11-may-1859 -location saddell -ref margaret1859-birth
+    }
+    child {
+        name _____ Galbreath
+        death -date 1861 -location saddell -ref unnamed1861-death
+    }
+    child {
+        name Donald Galbreath
+        birth  -date 1862 -location saddell -ref donald1862-birth
+    }
+    child {
+        name Mary Galbreath
+        birth -date 1864 -location saddell -ref mary1864-birth
+    }
+    child {
+        name Isabella Galbreath
+        birth -date 1866 -location saddell -ref isabella1866-birth
+    }
+    child {
+        name John Galbreath
+        birth -date 1868 -location saddell -ref john1868-birth
+    }
+    child {
+        name Flora Galbreath
+        birth -date 1870 -location saddell -ref flora1870-birth
+    }
 }
 footnotes {
-
     neil1817-birth {
         Birth appears to be unrecorded, however confirmed in Census of 1851, and
         death record, below.
@@ -14,8 +47,37 @@ footnotes {
     neil1817-death {
         $sp-ref-link[d-1874-531-01-0001 0001 "Neil Galbraith"]
     }
-
     mcmillan-marriage {
         $sp-ref-link[m-1856-531-00-0001 0001 "Niel Galbraith" "Isabella McMillan"]
+    }
+    isabella1858-birth {
+        $sp-ref[b-1858-531-01-0003 "Isabella Galbreath"]
+    }
+    isabella1858-death {
+        $sp-ref[d-1858-531-01-0001 "Isabella Galbreath"]
+    }
+    margaret1859-birth {
+        $sp-ref-link[b-1859-531-01-0008 0003 "Margaret Galbreath"]
+    }
+    unnamed1861-birth {
+        $sp-ref[b-1861-531-01-0005 "_____ Galbreath"]
+    }
+    unnamed1861-death {
+        $sp-ref[d-1861-531-01-0003 "_____ Galbreath"]
+    }
+    donald1862-birth {
+        $sp-ref[b-1862-531-01-0014 "Donald Galbreath"]
+    }
+    mary1864-birth {
+        $sp-ref[b-1864-531-01-0010 "Mary Galbreath"]
+    }
+    isabella1866-birth {
+        $sp-ref[b-1866-531-01-0011 "Isabella Galbreath"]
+    }
+    john1868-birth {
+        $sp-ref[b-1868-531-01-0016 "John Galbreath"]
+    }
+    flora1870-birth {
+        $sp-ref[b-1870-531-01-0012 "Flora Galbreath"]
     }
 }
