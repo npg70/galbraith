78852f139f27a8dc79f20ca1d529b127d5503712
nickg <nickg@client9.com>
2023-08-03 14:50:40 -0700

    update

--- people/galbraith-peter-1826-wright.sh
+++ people/galbraith-peter-1826-wright.sh
@@ -7,9 +7,17 @@ tags dalrymple campbeltown
 baptism -date 9-apr-1826 -location dalrymple -ref peter1826-baptism
 death -date 6-apr-1868 -location Wigtown -ref peter1826-death
 partner {
-    name Jessie Hannah
+    name Lilias Wright
+    marriage -date 16-may-1853 -location dalrymple
+    birth -date 2-may-1832 -location 'St Quivox, Ayrshire, Scotland, United Kingdom'
+    death -date 3-dec-1903 -location 'Shropshire, England, United Kingdom'
 }
 footnotes {
-
+    peter1826-baptism {
+        $opr-ref[b-1826-588-000-0030-0027 "Peter Galbraith"]
+    }
+    peter1826-death {
+        $sp-ref-link[d-1868-895-00-0019 0007 "Peter Galbraith"]
+    }
 
 }
