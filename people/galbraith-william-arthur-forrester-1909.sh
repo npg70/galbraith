ece86aa34b8354dee2329515dde4d01d8268580a
nickg <nickg@client9.com>
2023-08-03 10:51:15 -0700

    additions

---
+++ people/galbraith-william-arthur-forrester-1909.sh
@@ -0,0 +1,12 @@
+name Willaim Arthur Forresster Galbraith
+birth -date 25-jun-1909 -location saddell -ref william1909-birth
+tags "Illegitimate" "Saddell and Skipness"
+body {
+    Mother was "Mary Galbraith.'  Perhaps born in Edinburgh but registered in Saddell/Carradale.
+}
+
+footnotes {
+    william1909-birth {
+        $sp-ref-link[b-1909-531-01-0005 0002 "William Arthur Forrester Galbraith"]
+    }   
+}
