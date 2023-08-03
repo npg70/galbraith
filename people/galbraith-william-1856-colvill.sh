e97fd18d4f325c95a35796f8b207dc7fe72795c6
nickg <nickg@client9.com>
2023-08-03 14:29:31 -0700

    update

---
+++ people/galbraith-william-1856-colvill.sh
@@ -0,0 +1,27 @@
+name William Galbraith
+external {
+    familysearch MTTZ-JCG
+}
+birth -date 19-jun-1856 -location campbeltown -ref william1856-birth
+death -date 29-mar-1922 -location 'rothesay,bute'
+partner {
+    name Jane Colvill
+    marriage -date 29-jun-1895 -location Edinburgh
+    birth -date 4-apr-1865 -location campbeltown
+    death -date 3-nov-1912 -location rothesay,bute
+
+    child {
+        name William Colville Galbraith
+        birth -date 1897 -location rothesay,bute
+        death -date 5-mar-1958 -location glasgow
+    }
+    child {
+        name Elizabeth Colville Galbraith
+        birth -date 10-oct-1902 -location rothesay,bute
+    }
+}
+footnotes {
+    william1856-birth {
+        $sp-ref-link[b-1856-507-02-0022 0008 'William Galbraith']
+    }
+}
