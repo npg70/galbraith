29dae6737847750bf7793f319e2176da87954e80
nickg <nickg@client9.com>
2023-08-03 14:22:44 -0700

    update

---
+++ people/galbraith-thomas-1828-richmond.sh
@@ -0,0 +1,32 @@
+name Thomas Galbraith
+external {
+    familysearch KHQ4-ZJW
+}
+baptism -date 24-jul-1828 -location dalrymple -ref thomas1828-baptism
+death -date 21-jan-1859 -location campbeltown -ref thomas1828-death
+partner {
+    name Mary Richmond
+    marriage -date 13-feb-1856 -location campbeltown -ref thomas1828-marriage
+
+    child {
+        name Hannah Orr Galbraith
+        birth 8-jan-1858 -location campbeltown -ref hannah1858-birth
+        death 9-dec-1863 -location campbeltown
+    }
+}
+
+
+footnotes {
+    thomas1828-baptism {
+        $opr-ref[b-1828-588-000-0030-0036 "Thomas Galbraith"]
+    }
+    thomas1828-death {
+        $sp-ref-link[d-1859-507-00-0011 0004 "Thomas Galbraith"]
+    }
+    thomas1828-marriage {
+        $sp-ref-link[m-1856-507-02-0002 0001 "Thomas Galbraith" "Mary Richmond"]
+    }
+    hannah1858-birth {
+        $sp-ref-link[b-1858-507-00-0003 0001 "Hannah Orr Galbraith"]
+    }
+}
