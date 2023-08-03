113da64d5678784e153f53a348ec0e512abefb11
nickg <nickg@client9.com>
2023-08-03 10:06:00 -0700

    split

---
+++ people/galbraith-archibald-1839-morton.sh
@@ -0,0 +1,50 @@
+name Alexander Galbraith
+tags paisley 'daughtered out'
+birth -date "about 1839"
+death -date 12-aug-1865 -location paisley -ref alexander-death -note "Died of typhus fever."
+partner {
+    name Barbara Morton
+    birth -date 'about 1839'
+    marriage -date 1863 -location paisley -ref morton-marriage
+    death -date 8-sep-1871 -location paisley -ref morton-death
+    child {
+        name Barbara Galbraith 
+        birth -date 1863 -location Paisley -ref barbara1863-birth
+        death -date 29-jun-1941 -location Dalry,Ayr -ref barbara1863-death
+        partner {
+            name Robert Stuart Sloan
+        }
+    }
+    child {
+        name Alexander Galbraith
+        birth -date 1865 -location Paisley -ref alexnader1865-birth
+        death -date 3-jun-1884 -location Dumbarton -ref alexander1865-death -note "tuberculois, unmarried."
+    }
+}
+body {
+    He was a shoemaker.
+}
+
+footnotes {
+    morton-marriage {
+        $sp-ref-link[m-1863-573-01-0003 0002 "Alexander Galbraith" "Barbara Morton"]
+    }
+    morton-death {
+        $sp-ref-link[d-1871-573-02-0394 0132 "Barbara Galbraith"]
+    }
+    alexander-death {
+        $sp-ref-link[d-1865-573-02-0184 0062 "Alexander Galbraith"]
+    }
+    barbara1863-birth {
+        $sp-ref[b-1863-573-01-0495 "Barbara Galbraith"]
+    }
+    barbara1863-death {
+        $sp-ref[d-587-587-00-0049 "Barbara Stuart Galbraith"]
+    }
+    alexander1865-birth {
+        $sp-ref[b-1865-573-01-0445 "Alexander Galbraith"]
+    }
+    alexander1865-death {
+        $sp-ref[d-1884-496-00-0148 0050 "Alexander Galbraith"]
+    }
+}
