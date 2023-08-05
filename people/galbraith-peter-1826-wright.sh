40dbaa4b9651638e0e603e7bbcc58104de5bf398
nickg <nickg@client9.com>
2023-08-05 10:45:47 -0700

    updates

--- people/galbraith-peter-1826-wright.sh
+++ people/galbraith-peter-1826-wright.sh
@@ -1,5 +1,5 @@
 name Peter Galbraith
-todo { stub }
+todo { missing child, etc }
 external {
     familysearch K4XV-8MX
 }
@@ -14,24 +14,48 @@ partner {
     death -date 3-dec-1903 -location 'Shropshire, England, United Kingdom'
 
     child {
+        name David Wright Galbraith
+        birth -date 18-mar-1860 -location campbeltown -ref david1860-birth
+    }
+    child {
+        name Lilias Wright Galbraith
+        birth -date 28-jan-1862 -location campbeltown -ref lilias1862-birth
+        death -date oct-1890 -location 'Newport, Shropshire, England, United Kingdom'
+    }
+    child {
+        name Jane Galbraith
+        birth -date 6-feb-1864 -location campbeltown -ref jane1864-birth
+    }
+    child {
         name John Galbraith
         birth -date 17-apr-1865 -location campbeltown -ref john1865-birth
+        death -date 14-jul-1914 -location 'Newport, Shropshire, England, United Kingdom'
     }
     child {
         name Peter Galbraith
         birth -date 18-jan-1868 -location campbeltown -ref peter1868-birth
+        death -date 10-mar-1869 -location campbeltown -ref peter1868-death
     }
     child {
         name Peter Galbraith
+        tags immigrant
         birth -date 22-jan-1871 -location campbeltown -ref peter1871-birth
-    }
-    child {
-        name William 
+        death -date 24-jul-1927 -location 'Queensland, Australia'
+        partner {
+            name Ada Gertrude Murray
+            marriage -date apr-1905 -location 'Newport, Shropshire, England, United Kingdom'
+        }
     }
 }
 footnotes {
     peter1826-baptism {
         $opr-ref[b-1826-588-000-0030-0027 "Peter Galbraith"]
+    }
+    lilias1862-birth {
+        $sp-ref[b-1862-507-00-0034 "Lilias Wright Galbraith"]
+    }
+    jane1864-birth {
+        $sp-ref[b-1864-507-00-0052 "Jane Galbraith"]
     }
     john1865-birth {
         $sp-ref-link[b-1865-507-00-0071 0024 "John Galbraith"]
