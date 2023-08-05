ee3d2acb8f20c69262b9bc59aa28fdf23024b736
nickg <nickg@client9.com>
2023-08-04 23:40:02 -0700

    cleanups

--- people/galbraith-archibald-1872-mcarthur.sh
+++ people/galbraith-archibald-1872-mcarthur.sh
@@ -1,7 +1,7 @@
 name Archibald Galbraith
 tags campbeltown glasgow
 external {
-
+    familysearch 93R3-PBC
 }
 birth -date 11-jun-1872 -location glasgow -ref archibald1872-birth
 death -date 7-jun-1943 -location govan,glasgow -ref archibald1872-death
@@ -9,15 +9,22 @@ partner {
     name Mary McArthur
     birth -date 13-aug-1870 -location campbeltown
     marriage -date 29-nov-1889 -location campbeltown -ref mcarthur-marriage
+    child {
+        name Mary McFayden Galbraith
+        birth -date 9-mar-1890 -location campbeltown -ref mary1890-birth
+    }
 }
 footnotes {
     archibald1872-birth {
-        $sp-ref[b-1872-644-07-1146 "Archibald Galbraith"]       
+        $sp-ref-link[b-1872-644-07-1146 0382 "Archibald Galbraith"]       
     }
     archibald1872-death {
         $sp-ref-link[d-1943-644-17-0576 0192 "Archibald Galbraith"]
     }
     mcarthur-marriage {
         $sp-ref-link[m-1889-507-00-0048 0024 "Archibald Galbraith" "Mary McArthur"]
+    }
+    mary1890-birth {
+        $sp-ref-link[b-1890-507-00-0055 0019 "Mary McFadyen Galbraith"]
     }
 }
