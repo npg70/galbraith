c7927c4c78a7b3815b942a0c85e8e00bb9e0dd07
nickg <nickg@client9.com>
2023-08-05 10:18:03 -0700

    add

---
+++ people/galbraith-william-cumming-1866.sh
@@ -0,0 +1,14 @@
+name William Cumming Galbraith
+tags illegitimate campbeltown
+birth -date 8-aug-1866 -location campbeltown -ref william1866-birth
+
+body {
+    mother was Margaret Galbraith, domestic servant, cross st, campbeltown.
+}
+footnotes {
+    william1866-birth {
+    $sp-ref-link[b-1866-507-00-0150 0050 "William Cumming Galbraith"]
+}
+}
+
+
