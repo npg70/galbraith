9bf6a8e7f9e55d46782ac9e928dc23f177966b95
nickg <nickg@client9.com>
2023-08-21 07:30:53 -0700

    update

---
+++ people/galbraith-william-1853-mcarthur.sh
@@ -0,0 +1,59 @@
+name William Galbraith
+
+external {
+    familysearch LB2Q-L98
+}
+baptism -date 7-nov-1853 -location campbeltown -ref william-birth
+death -date 19-feb-1920 -location campbeltown -ref william-death
+partner {
+    name Elizabeth McArthur
+
+    marriage -date 7-oct-1879 -location campbeltown -ref mcarthur-marriage
+
+    child {
+        name Janet Galbraith
+        birth -date 12-sep-1881 -location campbeltown -ref janet1881-birth
+    }
+    child {
+        name William Galbraith
+        birth -date 1884 -location campbeltown -ref william1884-birth
+    }
+    child {
+        name Elizabeth McArthur Galbraith
+        birth -date 1886 -location campbeltown -ref elizabeth1886-birth
+    }
+    child {
+        name Catherine Sillars Galbraith
+        birth -date 1889 -location campbeltown -ref catherine1889-birth
+    }
+    child {
+        name William John Galbraith
+        birth -date 3-may-1892 -location campbeltown -ref william1892-birth
+    }
+
+}
+footnotes {
+    william-birth {
+        $opr-ref-link[b-1853-507-000-0070-0420 "William Galbraith"]
+
+        Note: mother is indexed as Janet Wilson.
+    }
+    william-death {
+        
+    }
+    janet1881-birth {
+        $sp-ref[b-1881-507-00-0258 "Janet Galbraith"]
+    }
+    william1884-birth {
+        $sp-ref-link[b-1884-507-00-0225 0075 "William Galbraith"]
+    }
+    elizabeth1886-birth {
+        $sp-ref[b-1886-507-00-0311 "Elizabeth McArthur Galbraith"]
+    }
+    catherine1889-birth {
+        $sp-ref[b-1889-507-00-0195 "Catherine Sillars Galbraith"]
+    }
+    william1892-birth {
+        $sp-ref-link[b-1892-507-00-0134 0045 "William John Galbraith"]
+    }
+}
