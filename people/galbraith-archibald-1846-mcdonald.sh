1181f4ec5bf89ea67a401eff2a4cc51c92c5f51b
nickg <nickg@client9.com>
2023-08-20 20:14:09 -0700

    updates

--- people/galbraith-archibald-1846-mcdonald.sh
+++ people/galbraith-archibald-1846-mcdonald.sh
@@ -4,6 +4,7 @@ death -date 9-apr-1914 -location campbeltown -ref archibald1846-death
 tags campbeltown
 external {
     familysearch M1BQ-K7W
+    findagrave 179719680
 }
 body {
     He was a blacksmith.
@@ -18,6 +19,27 @@ partner {
     death -date 26-feb-1915 -location campbeltown -ref mcdonald-death
     body {
         Daughter of Angus McDonald and Catherine McMillan.
+    }
+    child {
+        name Catherine Galbraith
+        birth -date 1880 -location campbeltown -ref catherine1880-birth
+    }
+    child {
+        name Jane Galbraith
+        birth -date 1881 -location campbeltown -ref jane1881-birth
+    }
+    child {
+        name Mary McDonald Galbraith
+        birth -date 1883 -location campbeltown -ref mary1883-birth
+    }
+    child {
+        name Margaret Paton Galbraith
+        birth -date 1885 -location campbeltown -ref margaret1885-birth
+        death -date 15-jan-1929 -location lochgilphead -ref margaret1885-death
+    }
+    child {
+        name Jessie Galbraith
+        birth -date 1888 -location campbeltown -ref jessie1888-birth
     }
     child {
         name Duncan Galbraith
@@ -35,6 +57,12 @@ partner {
             name Jean Sinclair
         }
     }
+    child {
+        name Annie Galbraith
+        body {
+            Inferred from tombstone
+        }
+    }
 }
 
 footnotes {
@@ -50,6 +78,24 @@ footnotes {
     }
     mcdonald-death {
         $sp-ref-link[d-1915-507-00-0029 0010 "Mary Galbraith"]
+    }
+    catherine1880-birth {
+        $sp-ref[b-1880-507-00-0022 "Catherine Galbraith"]
+    }
+    jane1881-birth {
+        $sp-ref[b-1881-507-00-0328 "Jane Galbraith"]
+    }
+    mary1883-birth {
+        $sp-ref[b-1883-507-00-0347 "Mary McDonald Galbraith"]
+    }
+    margaret1885-birth {
+        $sp-ref[b-1885-507-00-0333 "Margaret Paton Galbraith"]
+    }
+    margaret1885-death {
+        $sp-ref[d-1929-526-00-0009 "Margaret Paton Galbraith"]
+    }
+    jessie1888-birth {
+        $sp-ref[b-1888-507-00-0163 "Jessie Galbraith"]
     }
     duncan1890-birth {
         $sp-ref-link[b-1890-507-00-0136 0046 "Duncan Galbraith"]
