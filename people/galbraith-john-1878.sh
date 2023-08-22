573c92a70402ced6561aaa98f379ab64e204eb1c
nickg <nickg@client9.com>
2023-08-21 22:13:29 -0700

    update

---
+++ people/galbraith-john-1878.sh
@@ -0,0 +1,14 @@
+name John Galbraith
+birth -date 26-jun-1878 -location campbeltown -ref john-birth
+tags campbeltown illegitimate
+
+body {
+    Born 5 years after husband died. Unclear if used Galbraith or Crawford after birth.
+}
+footnotes {
+
+    john-birth {
+        $sp-ref-link[b-1878-507-00-0192 0064 "John Galbraith"]
+    }
+
+}
---
+++ people/galbreath-catherine-1835-crawford.sh
@@ -0,0 +1,24 @@
+name Catherine Galbreath
+tags gigha
+baptism -date 5-jul-1835 -location gigha -ref catherine-baptism
+
+partner {
+    name Robert Crawford
+    marriage -date 3-mar-1859 -location gigha -ref crawford-marriage
+    death -date 1873
+}
+
+partner {
+    name unknown
+
+    child galbraith-john-1878
+}
+
+footnotes {
+    catherine-baptism {
+        $opr-ref[b-1835-537-000-020-0030 "Catherine Galbraith"]
+    }
+    crawford-marriage {
+        $sp-ref-link[m-1859-537-00-0001 0001 "Catherine Galbraith" "Robert Crawford"]
+    }
+}
