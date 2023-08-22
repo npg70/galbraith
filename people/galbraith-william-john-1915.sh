06445db234a4f762cbb8749bbc440f37067940b3
nickg <nickg@client9.com>
2023-08-21 20:56:54 -0700

    add

---
+++ people/galbraith-william-john-1915.sh
@@ -0,0 +1,25 @@
+name William John Galbraith
+tags campbeltown veteran 'no children'
+external {
+    familysearch GL7X-NVF
+}
+birth -date 19-mar-1915 -location campbeltown -ref william1915-birth
+death -date 11-Jul-1944 -location 'Assam, India'  -ref william1915-death
+body {
+    $p{
+    x 2n Lt. (W.S./Lt.) The Argyll and Sutherland Highlanders, Age 29. Killed due to wounds in action.
+    }
+
+    $p{
+        Featured in the Campbeltown War Memorial and The Campbeltown book.  TODO: link
+    }
+}
+
+footnotes {
+    william1915-birth {
+        $sp-ref-link[b-1915-507-00-0047 0016 "William John Galbraith"]
+    }
+    william1915-death {
+        Scotlands People, 1944 GALBRAITH, WILLIAM JOHN (Statutory registers Deaths 149/AF 35)
+    }
+}
