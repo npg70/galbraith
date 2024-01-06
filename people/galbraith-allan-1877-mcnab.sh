943b7d2f947d14c88847bb651515ea285cca7799
nickg <217179+client9@users.noreply.github.com>
2024-01-05 18:40:29 -0800

    update

--- people/galbraith-allan-1877-mcnab.sh
+++ people/galbraith-allan-1877-mcnab.sh
@@ -1,24 +1,74 @@
 name Allan Galbraith
-tags campbeltown
+tags campbeltown 'daughtered out'
 external {
     familysearch L1VB-L7S
-}
-todo {
-    likely other children between 1899-1910
 }
 
 birth -date 20-jul-1877 -location campbeltown -ref allan1877-birth
 death -date 1-sep-1936 -location glasgow -ref allan1877-death
 partner {
     name Catherine McNab
-
+    birth -date 9-dec-1877 -location Killean,Argyll
+    death -date 1953 -location glasgow
+    marriage -date 14-jan-1898 -location kilcalmonell
     child {
         name Peter McNab Galbraith
         birth -date 13-mar-1898 -location kilcalmonell -ref peter1898-birth
+        death -date 1898 -location kilcalmonell -ref peter1898-death
+    }
+    child {
+        name Daniel Galbraith
+        birth -date 1900
+        death -date 2-jul-1964 -location 'glasgow north, glasgow' -ref daniel1900-death
+        partner {
+            name Margaret Anderson McKenzie
+            birth -date 'about 1900'
+            marriage -date 17-jul-1942 -location hillhead,glasgow -ref mckenzie-marriage
+        }
+        body {
+            Both married at age 42, children unlikely
+        }
+    }
+    child {
+        name Allan Galbraith
+        birth -date 1903 -location Hutchesontown -ref allan1903-birth
+        body {
+            Maybe immigrated to Australia, death unknown.
+        }
+    }
+    child {
+        name Alexander McNab Galbraith
+        birth -date 23-dec-1904 -location hutchesontown,glasgow -ref alexander1904-birth
+        death -date 1907 -location patick,glasgow -ref alexander1904-birth
+    }
+    child {
+        name Margaret McNab Galbraith
+        birth -date 05-feb-1907 -location patrick,glasgow -ref margaret1907-birth
+        death -date 28-feb-1977 -location glasgow
+        partner {
+            name Richard Wright
+            birth -date 1905
+            death -date 1950
+        }
+    }
+    child {
+        name Isabella McDermid Galbraith
+        birth -date 1909 -location patick,glasgow -ref isabella1909-birth
+        death -date 1995
+        partner {
+            name Robert McIntosh
+            birth -date 1909
+        }
     }
     child {
         name Mary McNab Galbraith
         birth -date 9-jan-1911 -location patick,lanark -ref mary1911-birth
+        death -date 24-dec-1954 -location kelvin,glasgow
+        partner {
+            name William Deane
+            birth -date 1907
+            death -date 1940
+        }
     }
 }
 footnotes {
@@ -30,6 +80,30 @@ footnotes {
     }
     peter1898-birth {
         $sp-ref-link[b-1898-516-00-0002 0001 "Peter McNab Galbraith"]
+    }
+    peter1898-death {
+        $sp-ref[d-1898-516-00-0003 "Peter M'Nab Galbraith"]
+    }
+    daniel1900-death {
+        $sp-ref-link[d-1964-645-03-0612 0204 "Daniel Galbraith"]
+    }
+    mckenzie-marriage {
+        $sp-ref-link[m-1942-644-13-0486 0243 "Daniel Galbraith" "Margaret Anderson McKenzie"]
+    }
+    allan1903-birth {
+        $sp-ref[b-1903-644-11-1302 "Allan Galbraith"]
+    }
+    alexander1904-birth {
+        $sp-ref-link[b-1904-644-11-0103 0034 "Alexander McNab Galbraith"]
+    }
+    alexander1904-birth {
+        $sp-ref[d-1907-646-03-0073 "Alexander McNab Galbraith"]
+    }
+    margaret1907-birth {
+        $sp-ref[b-1907-646-03-0287 "Margaret McNab Galbraith"]
+    }
+    isabella1909-birth {
+        $sp-ref[b-1909-646-03-0050 "Isbella McDermid Galbraith"]
     }
     mary1911-birth {
         $sp-ref-link[b-1911-646-03-0124 0042 "Mary McNab Galbraith"]
