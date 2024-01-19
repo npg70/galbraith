b386a214637d6d440b7bf297fb3b050ec94d4da3
nickg <217179+client9@users.noreply.github.com>
2024-01-18 16:47:40 -0800

    update

---
+++ people/galbreath-john-1770-galbreath.sh
@@ -0,0 +1,86 @@
+name John Galbreath
+tags Gigha
+birth -date 'say 1770'
+
+partner {
+    name Marion Galbreath
+    marriage -date 17-jun-1795 -location gigha -ref marion-marriage
+
+    child {
+        name Archibald Galbreath
+        baptism -date 19-jul-1796 -location gigha -ref archibald1796-baptism
+    }
+
+    child {
+        name Donald Galbreath
+        baptism -date 31-mar-1798 -location gigha -ref donald1798-baptism
+    }
+
+    child galbreath-john-1799-mcquilkan
+
+    child {
+        name Margaret Galbreath
+        baptism -date 28-apr-1803 -location gigha -ref margaret1803-baptism
+    }
+
+    child {
+        name Catharine Galbreath
+        baptism -date 06-may-1805 -location gigha -ref catharine1805-baptism
+    }
+    child {
+        name Hector Galbreath
+        baptism -date 18-aug-1807 -location gigha -ref hector1808-baptism
+    }
+    child {
+        name Mary Galbreath
+        baptism -date 15-jul-1810 -location gigha -ref mary1810-baptism
+    }
+    child {
+        name Merran Galbreath
+        baptism -date 24-may-1812 -location gigha -ref merran1812-baptism
+    }
+    child {
+        name Malcolm Galbreath
+        baptism -date 25-mar-1815 -location gigha -ref malcolm1815-baptism
+    }
+    child {
+        name James Galbreath
+        baptism -date 22-oct-1817 -location gigha -ref james1817-baptism
+    }   
+
+}
+footnotes {
+    marion-marriage {
+        $opr-ref[m-1795-537-000-0010-0008 "John Galbreath" "Marion Galbreath"]
+    }
+    archibald1796-baptism {
+        $opr-ref[b-1796-537-000-0010-0010 "Archibald Galbreath"]
+    }
+    donald1798-baptism {
+        $opr-ref[b-1798-537-000-0010-0013 "Donald Galbreath"]
+    }
+    margaret1803-baptism {
+        $opr-ref[b-1803-537-000-0010-0025 "Margarate Galbreath"]
+    }
+    catharine1805-baptism {
+        $opr-ref-link[b-1805-537-000-0010-0029 "Catharine Galbreath"]
+    }
+    hector1808-baptism {
+        Indexed twice, 
+        $opr-ref[b-1807-537-000-0010-0034 "Hector Galbreath"];
+        $opr-ref[b-1808-537-000-0010-0037 "Hector Galbreath"]
+    }
+    mary1810-baptism {
+        $opr-ref[b-1810-537-000-0010-0044 "Mary Galbreath"]
+    }
+    merran1812-baptism {
+        $opr-ref-link[b-1812-537-000-0010-0048 "Marran Galbreath"]
+    }
+    malcolm1815-baptism {
+        $opr-ref[b-1815-537-000-0010-0053 "Malcom Galbreath"]
+    }
+    james1817-baptism {
+        $opr-ref[b-1817-537-000-0010-0057 "James Galbreath"]
+    }
+}
+
