1ea3ab34d0ddd646168bb12e8c824087bd5fd9dc
nickg <217179+client9@users.noreply.github.com>
2024-01-24 00:07:15 -0500

    update

---
+++ people/galbreath-george-1846-bogle.sh
@@ -0,0 +1,38 @@
+name George Galbreath
+birth -date 15-mar-1846
+baptism -date 25-may-1846 -location greenock -ref george1846-baptism
+tags 'deadend'
+
+note {
+    Family vanishes after 1891 census.  https://www.findmypast.com/transcript?id=GBC/1881/0028355565&expand=true
+}
+
+note {
+    parents are John Galbreath and Janet Hay
+}
+
+external {
+    familysearch MPJ7-G72
+}
+
+partner {
+    name Catherine Bogle
+    birth -date 'about 1846' -note 'daughter of Margaret Fyfe'
+    marriage -date 17-oct-1872 -location Inverkip,Renfrew -ref bogle-marriage
+
+    child {
+        name John Galbraith
+        birth -date 10-apr-1879 -location dunoon -ref john1879-birth
+    }
+}
+footnotes {
+    george1846-birth {
+        $opr-ref[b-1846-564-003-0060-0447 "George Galbreath"]
+    }
+    bogle-marriage {
+        $sp-ref-link[m-1872-567-02-0015 0008 "George Galbraith" "Catherine Loude Bogle"]
+    }
+    john1879-birth {
+        $sp-ref-link[b-1879-510-00-0070 0024 "John Galbraith"]
+    }
+}
