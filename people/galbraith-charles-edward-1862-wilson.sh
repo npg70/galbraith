b06f13169d39c1c45ebdbdd49ce9de0cb2a7cd34
nickg <217179+client9@users.noreply.github.com>
2024-01-26 13:04:26 -0800

    updates

---
+++ people/galbraith-charles-edward-1862-wilson.sh
@@ -0,0 +1,61 @@
+name Charles Edward Galbraith
+external {
+    familysearch 93BC-89N
+    findagrave 249312056
+}
+birth -date 18-mar-1862 -location Glasgow
+death -date 13-dec-1936 -location Kirkmichael,Dumfries -ref charles1862-death
+partner {
+   name Katherine Robina Wilson
+   marriage -date 19-nov-1885
+
+   child {
+    name James Edward Evans Galbraith
+    birth -date 1888 -location 'Lincolnshire, England'
+    death -date 10-apr-1954 -location dumfries -ref james1888-death
+    partner {
+        name ____ Murray
+    }
+   }
+   child {
+    name Janet Margaret Galbraith
+    birth -date 1890 -location 'Lincolnshire, England'
+   }
+   child {
+    name John Wilson Seton Galbraith
+    birth -date 30-jan-1893 -location Ayton,Berwick,Scotland -ref john1893-birth
+    death -date 06-jan-1970 -location dumfries -ref john1893-death
+    partner {
+        name Dorothy Priscilla Feilden
+        marriage -date 1941 -location 'Witney, Oxfordshire, England'
+    }
+   }
+   child {
+    name Charles Arthur Temple Galbraith
+    birth -date 01-oct-1896 -location Ayton,Berwick,Scotland -ref charles1896-birth
+    death -date 04-jan-1939 -location Peebles,Peebles -ref charles1896-death
+    partner {
+        name Helen Muriel Ballongyn[?]
+    }
+   }
+}
+footnotes {
+    charles1862-death {
+        $sp-ref-link[d-1936-836-00-0006 0002 "Charles Edward Galbraith"]
+    }
+    james1888-death {
+        $sp-ref-link[d-1954-821-00-0223 0075 "James Edward Evands Galbraith"]
+    }
+    john1893-birth {
+        $sp-ref-link[b-1893-727-00-0006 0002 "John Wilson Seton Galbraith"]
+    }
+    john1893-death {
+        $sp-ref[d-1970-821-00-0038 "John Wilson Seton Galbraith"]
+    }
+    charles1896-birth {
+        $sp-ref-link[b-1896-727-00-0028 0010 "Charles Arthur Temple Galbraith"]
+    }
+    charles1896-death {
+        $sp-ref-link[d-1939-768-00-0003 0001 "Charles Arthur Temple Galbraith"]
+    }
+}
--- people/galbraith-james-1818-kirkwood.sh
+++ people/galbraith-james-1818-kirkwood.sh
@@ -13,19 +13,8 @@ partner {
    birth -date 1822
    marriage -date 1859
 
-   child {
-     name Charles Edward Galbraith
-     birth -date 18-mar-1862 -location Glasgow
-     death -date 13-dec-1936 -location Kirkmichael,Dumfries -ref charles1862-death
-     partner {
-        name Katherine Robina Wilson
-        marriage -date 19-nov-1885
-    }
-   }
+   child galbraith-charles-edward-1862-wilson
 }
 footnotes {
-    charles1862-death {
-        $sp-ref-link[d-1936-836-00-0006 0002 "Charles Edward Galbraith"]{ "Landed Propritor"}
-    }
 }
 
