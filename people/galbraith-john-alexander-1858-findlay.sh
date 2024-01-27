191c9239fa9ab6b0fbc61330ff320128ddbb77f9
nickg <217179+client9@users.noreply.github.com>
2024-01-26 16:34:24 -0800

    updates

--- people/galbraith-john-alexander-1858-findlay.sh
+++ people/galbraith-john-alexander-1858-findlay.sh
@@ -9,6 +9,45 @@ death -date 25-nov-1933 -location edinburgh -ref john1858-death
 partner {
     name Mary Rogerson McKinight Findlay
     marriage 13-jun-1889 -location glasgow
+
+    child {
+        name Elsie Mary Galbraith
+        birth -date 20-May-1890 -location glasgow
+        baptism -date 22-jun-1890 -location glagow
+        death -date 1976
+        partner {
+            name James Archdale
+            birth -date 1891
+            death -date 1978
+            marriage -date 16-jun-1923 -location libberton,lanark
+        }
+    }
+    child {
+        name Donald James Findlay Galbraith
+        birth -date 13-may-1892 -location glasgow
+        death -date 25-jan-1915 -location festubert,france
+    }
+    child {
+        name Mary Helen Fraser Galbraith
+        birth -date 17-oct-1894 -location glasgow
+        baptism -date 24-nov-1894 -location glasgow
+    }
+    child {
+        name Isabelle Frances Hope Galbraith
+        birth -date 28-feb-1903 -location glasgow
+        death -date 02-may-1903 -location glasgow
+    }
+    child {
+        name Violet Hannah Goldie Galbraith
+        birth -date 07-dec-1904 -location glasgow
+        death -date 15-may-1939 -location jamaica
+        partner {
+            name Eric Nelson Harris
+            birth -date 1901
+            death -date 1965
+            marriage -date 11-aug-1927 -location 'St Marylebone, Middlesex, England'
+        }
+    }
 }
 footnotes {
     john1858-birth {
