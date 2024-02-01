e68b5ecb1c4e523d6b24bfa2bc45f1226b03aae2
nickg <217179+client9@users.noreply.github.com>
2024-02-01 13:03:02 -0800

    update

---
+++ people/galbraith-james-1843-mcliver.sh
@@ -0,0 +1,85 @@
+name James Galbraith
+external {
+    familysearch L697-YD7
+}
+tags ireland islay bowmore kilarrow
+birth -date 'about 1843' -location ireland
+death -date 17-nov-1884 -location bowmore -ref james1843-death
+
+partner {
+    name Elizabeth McLiver
+    birth -date 26-jul-1843 -location bowmore
+    death -date 21-dec-1892 -location 'kinning park, lanark' -ref mcliver-death
+    marriage -date 29-nov-1870 -location kilarrow -ref mcliver-marriage
+
+    child {
+        name Sarah Galbraith
+        birth -date 11-may-1871 -location Newington,Edinburgh -ref sarah1871-birth
+    }
+    child {
+        name Donald Galbraith
+        birth -date 11-may-1873 -location kilarrow,islay -ref donald1873-birth
+        death -date 1887 -location bowmore,islay -ref donald1873-death
+    }
+    child {
+        name Andrew Galbraith
+        birth -date 2-jul-1875 -location bowmore,islay -ref andrew1875-birth
+        death -date 1879 -location bowmore,islay -ref andrew1875-death
+    }
+    child {
+        name Robert Galbraith
+        birth -date 5-jun-1877 -location bowmore,islay -ref robert1877-birth
+    }
+    child {
+        name John Galbraith
+        birth -date 26-jul-1879 -location bowmore,islay -ref john1879-birth
+    }
+    child {
+        name Elizabeth Galbraith
+        birth -date 07-oct-1881 -location bowmore,islay -ref elizabeth1881-birth
+        death -date 1885 -location bowmore,islay -ref elizabeth1881-death
+    }
+    child {
+        name James Galbraith
+        birth -date 19-nov-1883 -location bowmore,islay -ref james1883-birth
+        death -date 1885 -location bowmore,islay -ref james1883-death
+    }
+}
+footnotes {
+    james1843-death {
+        $sp-ref-link[d-1884-536-00-0036 0012 "James Galbraith"]
+    }
+    sarah1871-birth {
+        $sp-ref-link[b-1871-685-05-0605 0202 "Sarah Galbraith"]
+    }
+    donald1873-birth {
+        $sp-ref-link[b-1873-536-00-0016 0006 "Donald Galbraith"]
+    }
+    donald1873-death {
+        $sp-ref[d-1887-536-00-0004 "Donald Galbraith"]
+    }
+    andrew1875-birth {
+        $sp-ref-link[b-1875-536-00-0022 0008 "Andrew Galbraith"]
+    }
+    andrew1875-death {
+        $sp-ref[d-1879-536-00-0029 "Andrew Galbraith"]
+    }
+    robert1877-birth {
+        $sp-ref-link[b-1877-536-00-0025 0009 "Robert Galbraith"]
+    }
+    john1879-birth {
+        $sp-ref-link[b-1879-536-00-0024 0008 "John Galbraith"]
+    }
+    elizabeth1881-birth {
+        $sp-ref-link[b-1881-536-00-0041 0014 "Elizabeth Galbraith"]
+    }
+    elizabeth1881-death {
+        $sp-ref[d-1885-536-00-0012 "Elizabeth Galbraith"]
+    }
+    james1883-birth {
+        $sp-ref-link[b-1883-536-00-0041 0014 "James Galbraith"]
+    }
+    james1883-death {
+        $sp-ref[d-1885-536-00-0014 "James Galbraith"]
+    }
+}
