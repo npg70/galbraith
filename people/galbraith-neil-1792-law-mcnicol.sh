323eaeac6782ff778cec2ffb6160aace954c590e
nickg <217179+client9@users.noreply.github.com>
2024-02-09 13:48:27 -0800

    update

---
+++ people/galbraith-neil-1792-law-mcnicol.sh
@@ -0,0 +1,153 @@
+name Neil Galbraith
+birth -date 'about 1792'
+death -date 12-dec-1869 -location Newington,Edinburgh -ref neil1792-death
+external {
+    familysearch KH5S-2CD
+}
+note {
+    Parents of Neil are listed in the death record as John Galbraith and Margaret Campbell
+}
+
+partner {
+    name Mary Law
+    birth -date 'about 1799'
+    marriage  -date 25-may-1821 -location kilmodan,argyll -ref law-marriage
+
+    child {
+        name John Galbraith
+        baptism -date 09-apr-1822 -location lochgilphead -ref john1822-baptism
+    }
+    child {
+        name Archibald Galbreath
+        baptism -date 24-feb-1824 -location lochgilphead -ref archibald1824-baptism
+    }
+    child {
+        name Peter Galbreath
+        baptism -date 11-mar-1826 -location 'killean and kilchenzie' -ref peter1826-baptism
+    }
+    child {
+        name Hector Galbraith
+        baptism -date 13-may-1828 -location 'st cuthberts' -ref hector1828-baptism
+    }
+    child {
+        name James Galbraith
+        baptism -date 21-jun-1836 -location 'st cuthberts' -ref james1836-baptism
+    }
+    child {
+        name Hector Galbraith
+        baptism -date 27-dec-1838 -location 'st cuthberts' -ref hector1838-baptism
+    }
+    child {
+        name Alexander Galbraith
+        baptism -date 25-jun-1841 -location 'St Cuthberts, Midlothian' -ref alexander1841-baptism
+    }
+    child {
+        name David Galbraith
+        baptism -date 11-oct-1843 -location 'St Cuthberts' -ref david1843-baptism
+        death -date 19-mar-1844 -location 'St Cuthberts' -ref david1843-death
+    }
+}
+partner {
+    name Julia McNicol
+    birth -date 'about 1803' -note 'the daughter of Archibald McNicol and Mary McVicer'
+    death -date 05-apr-1878 -location 'St George,Edinburgh' -ref mcnicol-death
+    marriage -date 11-nov-1845 -location 'St Cuthberts' -ref mcnicol-marriage
+}
+
+body {
+
+1827 Census of Gigha:$ref[census1827]
+$csvtable{
+First,Last,Age,Year
+Neil, Galbraith, 36, 1791
+Mary, Law, 26, 1801
+John, Galbraith, 5, 1822
+Archibald, Galbraith, 3, 1824
+Peter, Galbraith, 1, 1826
+Alexander, Law, 10, 1817
+}
+1841 Census of West Side of Earl Gray Street, St Cuthbert's, Midlothian, Scotland:$ref[census1841]
+$csvtable{
+First,Last,Age,Year,Place
+Neil,Galbraith,48,1793,Scotland
+Mary,Galbraith,42,1799,Scotland
+John,Galbraith,18,1823,Scotland
+Archibald,Galbraith,16,1825,Scotland
+Peter,Galbraith,14,1827,Scotland
+Neil,Galbraith,10,1831,Midlothian
+James,Galbraith,5,1836,Midlothian
+Hector,Galbraith,2,1839,Midlothian
+}
+
+1851 Census of Waghers House Canal Basin, St Cuthbert's, Midlothian, Scotland:$ref[census1851]
+$csvtable{
+First,Last,Role,Age,Year,Place
+Neil,Galbraith,Head,55,1796,Gigha
+Julia,Galbraith,Wife,49,1802,Kirkmichael
+James,Galbraith,Son,14,1837,St Cuthberts
+Hector,Galbraith,Son,12,1839,St Cuthberts
+Alexander,Galbraith,Son,9,1842,St Cuthberts
+}
+
+1861 Census of Port Ropetoun, St Cuthbert's, St George, Midlothian, Scotland:$ref[census1861]
+$csvtable{
+First,Last,Role,Age,Year
+Neil,Galbraith,Head,67,1794
+Julia,Galbraith,Wife,58,1803
+Hector,Galbraith,Son,22,1839
+}
+}
+footnotes {
+    census1827 {
+        https://www.gigha.org.uk/viewItem.php?id=8932&parentId=8926&sectionTitle=About+Gigha
+    }
+    census1841 {
+        https://www.findmypast.com/transcript?id=GBC/1841/0017388346&expand=true
+    }
+    census1851 {
+        https://www.findmypast.com/transcript?id=GBC/1851/0018753551&expand=true
+    }
+    census1861 {
+        https://www.findmypast.com/transcript?id=GBC/1861/0024006775&expand=true
+    }
+    neil1792-death {
+        $sp-ref-link[d-1869-685-05-0892 0298 "Neil Galbraith"]
+    }
+    law-marriage {
+        $opr-ref[m-1821-522-000-0020-0071 "Neil Galbreath" "Mary Law"]
+    }
+    john1822-baptism {
+        $opr-ref[b-1822-527-001-0030-0010 "John Galbraith"]
+    }
+    archibald1824-baptism {
+        $opr-ref[b-1824-527-001-0030-0016 "Archibald Galbreath"]
+    }
+    peter1826-baptism {
+        $opr-ref[b-1826-519-000-0020-0053 "Peter Galbreath"]. Only the father "Niel Galbreath" is listed as parents.
+    }
+
+    hector1828-baptism {
+        $opr-ref[b-1828-685-002-0340-0203 "Hector Galbraith"]
+    }
+    james1836-baptism {
+        $opr-ref[b-1836-685-002-0350-0277 "James Galbraith"]
+    }
+    hector1838-baptism {
+        $opr-ref[b-1838-685-002-0350-0465 "Hector Galbraith"]
+    }
+    alexander1841-baptism {
+        $opr-ref[b-1841-685-002-0350-0631 "Alexander Galbraith"]
+    }
+    david1843-baptism { 
+        $opr-ref[b-1843-685-002-0360-0138 "David Galbraith"]
+    }
+    david1843-death {
+        $opr-ref[d-1844-685-002-0510-0187 "David Galbraith"]
+    }
+    mcnicol-marriage {
+        $opr-ref[m-1845-685-002-0460-0077 "Neil Galbraith" "Julia McNicol"]
+    }
+    mcnicol-death {
+        $sp-ref-link[d-1878-685-01-0440 0147 "Julia Galbraith"]
+    }
+}
---
+++ people/galbreath-john-1760-gilchrist.sh
@@ -0,0 +1,82 @@
+name John Galbreath
+tags gigha 'daughtered out'
+birth -date 'about 1760'
+external {
+    familysearch KZZS-S74
+}
+body {
+    He was a schoolmaster.
+}
+body {
+1827 Census of Gigha at Ardmeanish:$ref[census1827]
+$csvtable{
+First,Last,Age,Year
+John,Galbraith,67,1769
+Mary,Gilchrist,59,1768
+Lachlan,Galbraith,30,1797
+Mary,Galbraith,24,1803
+}
+}
+
+partner {
+    name Mary Gilchrist
+    birth -date 'about 1768'
+    marriage -date 15-feb-1785 -location 'killean and kilchenzie' -ref gilchrist-marriage
+
+    child {
+        name Anny Galbreath
+        birth -date 'about 1792'
+        body {
+            ???
+        }
+    }
+    child {
+        name Lachlan Galbreath
+        baptism -date 15-jun-1797 -location gigha -ref lachlan1797-baptism
+        death -date 15-may-1867 -location 'killen and kilchenzie' -ref lachlan1797-death -note unmarried
+    }
+    child {
+        name _____ Galbbreath
+        baptism -date 08-dec-1799 -location gigha -ref unnamed1799-baptism
+        death -date 16-dec-1867 -location gigha -ref unnamed1799-death
+    }
+    child {
+        name Charles Galbreath
+        baptism -date 03-may-1802 -location gigha -ref charles1802-baptism
+        death -date 02-jun-1802 -location gigha -ref charles1802-death
+    }
+    child {
+        name Mary Galbreath
+        baptism -date 04-sep-1803 -location gigha -ref mary1803-baptism
+    }
+}
+footnotes {
+    census1827 {
+        https://www.gigha.org.uk/viewItem.php?id=8932
+    }
+    gilchrist-marriage {
+        $opr-ref-link[m-1785-519-000-0010-0311 "John Galbreath"]
+    }
+    lachlan1797-baptism {
+        $opr-ref[b-1797-537-000-0010-0011 "Lachlan Galbreath"]
+    }
+    lachlan1797-death {
+        $sp-ref-link[b-1867-519-00-0016 0006 "Lachlan Galbraith"]
+    }
+    unnamed1799-baptism {
+        Indexed three times:
+        $opr-ref[b-1799-537-000-0010-0011 "_____ Galbreath"];
+        $opr-ref[b-1799-537-000-0010-0017 "_____ Galbreath"];
+        $opr-ref[b-1799-537-000-0010-0018 "_____ Galbreath"]
+    }
+    charles1802-baptism {
+        Indexed twice but with same identifier.
+        $opr-ref[b-1802-537-000-0010-0023 "Charles Galbreath"]
+    }
+    charles1802-death {
+        $opr-ref[d-1802-537-000-0010-0024 "Charles Galbreath"]
+    }
+    mary1803-baptism {
+        $opr-ref[b-1803-537-000-0010-0026 "Mary Galbreath"]
+    }
+}
