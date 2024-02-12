f00122e65d2b0726af38f148d198a1222f5a5208
nickg <217179+client9@users.noreply.github.com>
2024-02-11 21:55:51 -0800

    update

---
+++ people/galbreath-john-1767-macdonald.sh
@@ -0,0 +1,90 @@
+name John Galbreath
+birth -date 'about 1767'
+tags gigha 'dead end'
+todo {
+    No records after 1827.  FamilySearch is a mess.
+}
+note {
+    Sarah was a common nickname of Merran
+}
+note {
+    There is also a John Galbreath who married a Merran Galbreath in Gigha at the same time.  They are different.
+}
+partner {
+    name Merran Sarah MacDonald
+    marriage -date 23-feb-1808 -location gigha -ref macdonald-marriage
+    birth -date 'about 1787'
+
+    child {
+        name Elenora Nelly Galbreath
+        baptism -date 02-mar-1809 -location gigha -ref elenora1809-baptism
+    }
+    child {
+        name Neil Galbreath
+        baptism -date 18-jan-1811 -location gigha -ref neil1811-baptism
+    }
+    child {
+        name Archibald Galbreath
+        baptism -date 17-nov-1812 -location gigha -ref archibald1812-baptism
+    }
+    child {
+        name Catharine Galbreath
+        baptism -date 21-feb-1815 -location gigha -ref catharine1815-baptism
+    }
+    child {
+        name Donald Galbreath
+        baptism -date 15-jun-1817 -location gigha -ref donald1817-baptism
+    }
+    child {
+        name Catharine Galbreath
+        baptism -date 22-jul-1820 -location gigha -ref catharine1820-baptism
+    }
+    child {
+        name Lachlan Galbreath
+        baptism -date 08-jun-1824 -location gigha -ref lachlan1824-baptism
+    }
+}
+body {
+Isle of Gigha Census 1827:$ref[census1827]
+$csvtable{
+First,Last,Age,Year
+John, Galbraith, 60, 1767
+Sarah, Macdonald, 40, 1787
+Nelly, Galbraith, 19, 1808
+Neill, Galbraith, 17, 1810
+Archibald, Galbraith, 14, 1813
+Donald, Galbraith, 9, 1818
+Lachlan, Galbraith, 4, 1823
+Janet, Macneill, 75, 1752
+}
+}
+
+footnotes {
+    census1827 {
+        https://www.gigha.org.uk/viewItem.php?id=8932
+    }
+    macdonald-marriage {
+       $opr-ref[m-1808-537-000-0010-0039 "John Galbreath" "Merran MacDonald"]
+   } 
+    elenora1809-baptism {
+        $opr-ref[b-1809-537-000-0010-0040 "Elenora Galbreath"]
+    }
+    neil1811-baptism {
+        $opr-ref[b-1811-537-000-0010-0046 "Neil Galbreath"]
+    }
+    archibald1812-baptism {
+        $opr-ref[b-1812-537-000-0010-0049 "Archibald Galbreath"]
+    }
+    catharine1815-baptism {
+        $opr-ref[b-1815-537-000-0010-0053 "Catharine Galbreath"]
+    }
+    donald1817-baptism {
+        $opr-ref[b-1817-537-000-0010-0057 "Donald Galbreath"]
+    }
+    catharine1820-baptism {
+        $opr-ref[b-1820-537-000-0020-0003 "Catharine Galbreath"]
+    }
+    lachlan1824-baptism {
+        $opr-ref[b-1824-537-000-0020-0010 "Lachlan Galbreath"]
+    }
+}
