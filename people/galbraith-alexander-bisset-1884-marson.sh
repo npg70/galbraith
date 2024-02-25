4aa352631a474ba50757fe180a8b1166e0a9868e
nickg <217179+client9@users.noreply.github.com>
2024-02-25 08:42:01 -0800

    update

---
+++ people/galbraith-alexander-bisset-1884-marson.sh
@@ -0,0 +1,27 @@
+name Alexander Bisset Galbraith
+external {
+    familysearch GVXD-NC2
+}
+todo {
+    definitely other children, and parents are known.
+}
+birth -date 03-mar-1884 -location Gorbal -ref alexander1884-birth
+body {
+    He was a fine-art dealer.
+}
+partner {
+    name Margaret Munn Marson
+    birth -date 'about 1886' -note 'daughter of William Marson and Annie Munn'
+    marriage -date 26-sep-1912 -location gorbals -ref marson-marriage
+
+    child galbraith-roderick-maclean-1922
+}
+
+footnotes {
+    alexander1884-birth {
+        $sp-ref-link[b-1884-644-12-0415 0139 "Alexander Bisset Galbraith"]
+    }
+    marson-marriage {
+        $sp-ref-link[m-1912-644-17-0422 0211 "Alexander Bisset Galbraith" "Margaret Munn Marson"]
+    }
+}
---
+++ people/galbraith-roderick-maclean-1922.sh
@@ -0,0 +1,20 @@
+name Roderick Maclean Galbraith
+tags veteran glasgow
+birth -date 1922 -location govanhill -ref roderick1922-birth
+death -date 1941 -ref roderick1922-death
+external {
+    familysearch GPGR-WM1
+}
+
+body {
+    Roderick was the last Galbraith in South Knapdale. He was a Sergant of the Royal Airforce and died "Due to War Operations, Drowned".  It's unclear why he was in South Knapdale at all, as he was from Glasgow.
+}
+footnotes {
+    roderick1922-birth {
+        $sp-ref[b-1922-644-16-0412 "Roderick MacLean Galbraith"]
+    }
+    roderick1922-death {
+        $sp-ref-link[d-1941-533-01-0001 0001 "Roderick MacLean Galbraith"]
+    }
+}
+
