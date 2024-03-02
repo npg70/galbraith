a9c07b428f36b554a72748225506f6d50d5de36a
nickg <217179+client9@users.noreply.github.com>
2024-03-02 11:46:50 -0800

    update

--- people/galbraith-james-1894-barnie.sh
+++ people/galbraith-james-1894-barnie.sh
@@ -2,6 +2,7 @@ name James Galbraith
 tags campbeltown immigrant
 external {
     familysearch GH2X-J97
+    findagrave 236356385
 }
 birth -date 1-nov-1894 -location campbeltown -ref james1894-birth
 death -date 1988 -location 'ontario, canada'
@@ -9,8 +10,56 @@ partner {
     name Mary Isabelle Barnie
     birth -date 1897
     marriage -date 16-aug-1920 -location 'brantford,brant,ontario,canada'
+
+    child {
+        name Jean Isabel Galbraith
+        birth -date 11-aug-1921 -location ontario,canada
+        death -date 10-feb-2005 -location lanark,ontario,canada
+        partner {
+            name Braden Wallace Austin
+            birth -date 25-sep-1922 -location ontario,canada
+            death -date 24-may-2014 -location ontario,canada
+        }
+    }
     child {
         name Elizabeth Catherine Elsie Galbraith
+        birth -date 16-jun-1922
+        death -date 08-feb-2018 -location elgin,ontario,canada
+        partner {
+            name James Ernest Rice
+            birth -date 08-may-1933 -location ontario,canada
+            death -date 23-aug-2016 -location ontario,canada
+        }
+    }
+    child {
+        name Donald James Galbraith
+        birth -date 06-sep-1924
+        death -date 17-feb-1951 -location ontario,canada
+        partner {
+            name Beverly Jane Richards
+            birth -date 23-oct-1927 -location toronto,ontario,canada
+            death -date 29-may-1994 -location ontario,canada
+        }
+    }
+    child {
+        name George Douglas Galbraith
+        birth -date 06-jan-1928 -location elgin,ontario,canada
+        death -date 12-feb-2001 -location ontario,canada
+        partner {
+            name Betty Browne
+            birth -date 1929
+            death -date 04-feb-1999
+        }
+    }
+    child {
+        name Mary Janet Galbraith
+        birth -date 01-jan-1932 -location elgin,ontario,canada
+        death -date 05-aug-2020 -location renfrew,ontario,canada
+        partner {
+            name Robert A Murat
+            birth -date 13-jan-1932
+            death -date 26-may-1974
+        }
     }
 }
 footnotes {
--- people/galbreath-donald-1862-mclarty.sh
+++ people/galbreath-donald-1862-mclarty.sh
@@ -2,9 +2,29 @@ name Donald Galbreath
 tags 'Saddell and Skipness' immigrant
 external {
     familysearch M1RX-3KL
+    ancestry tree/71855511/person/232218862057/facts
+    findagrave 148338512
+    billiongraves 1203483
 }
 birth -date 28-may-1862 -location saddell -ref donald1862-birth
 death -date 13-aug-1947 -location london,middlesex,ontario,canada
+
+body {
+
+Immigration to St. Catharines, Ontario, Canada  June 1911
+
+
+From [Ancestry.com](https://www.ancestry.com/family-tree/person/tree/71855511/person/232218862057/facts), and [Bagpipe.news](https://bagpipe.news/2021/02/15/donald-galbraith-from-argyll-to-ontario-via-kilmarnock/)
+
+$blockquote{
+At the turn of the 20th century, Donald was founder Pipe Major of the Argyll and Bute Artillery Pipe Band in Campbeltown and taught the Campbeltown Boy's Brigade pipe band. In 1907, he and his family moved to Kilmarnock. Donald's father was also a piper.
+}
+
+$blockquote{
+When the family came to Canada Donald continued to play. He was pipe major of the Ingersoll Pipe Band. His son, James played in the same band. His great grandson, James played Donald's 130 year old pipes at family occasions.
+}
+
+}
 partner {
     name Catherine McLarty
     birth -date 7-may-1859 -location campbeltown
