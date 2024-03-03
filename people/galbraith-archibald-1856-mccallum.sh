b19a361b590aa6931dd454df41a37d9eb131770d
nickg <217179+client9@users.noreply.github.com>
2024-03-02 17:18:34 -0800

    update

--- people/galbraith-archibald-1856-mccallum.sh
+++ people/galbraith-archibald-1856-mccallum.sh
@@ -19,17 +19,7 @@ partner {
 
     child galbraith-archibald-1890
 
-    child {
-        name Neil Galbraith
-        birth -date 13-feb-1894  -location 'killean' -ref neil1894-birth
-        death -date 29-mar-1974 -location 'killean' -ref neil1894-death
-        partner {
-            name Catherine McSporran
-            birth -date 1901 -location killean
-            death -date 3-feb-1991 -location campbeltown
-            marriage -date 12-may-1923 -location campbeltown
-        }
-    }
+    child galbraith-neil-1894-mcsporran
 }
 footnotes {
     archibald1856-birth {
@@ -47,9 +37,6 @@ footnotes {
     }
     jane1888-death {
         $sp-ref-link[d-1963-507-00-0055 0019 "Jane Galbraith"]
-    }
-    neil1894-birth {
-        $sp-ref-link[b-1894-519-00-0002 0001 "Neil Galbraith"]
     }
 }
 
---
+++ people/galbraith-neil-1894-mcsporran.sh
@@ -0,0 +1,36 @@
+name Neil Galbraith
+external {
+    familysearch GHDN-CJ8
+}
+birth -date 13-feb-1894  -location 'killean' -ref neil1894-birth
+death -date 29-mar-1974 -location 'tayinloan' -ref neil1894-death
+body {
+    He was a game-keeper.
+}
+partner {
+   name Catherine McSporran
+   birth -date 1901 -location killean -note 'Daughter of Peter McSporran and Margaret McArthur'
+   death -date 3-feb-1991 -location campbeltown -ref mcsporran-death
+   marriage -date 12-may-1926 -location campbeltown -ref mcsporran-marriage
+
+   child {
+    name Margaret McCallum McArthur Galbraith
+    birth -date 1926
+    death -date 2002
+   }
+}
+
+footnotes {
+    neil1894-birth {
+        $sp-ref-link[b-1894-519-00-0002 0001 "Neil Galbraith"]
+    }
+    neil1894-death {
+        $sp-ref[d-1974-535-00-0002 "Neil Galbraith"]
+    }
+    mcsporran-marriage {
+        $sp-ref-link[m-1926-507-00-0020 0010 "Neil Galbraith" "Catherine McSporran"]
+    }
+    mcsporran-death {
+        $sp-ref[d-1991-536-00-0016 "Catherine Galbraith"]
+    }
+}
