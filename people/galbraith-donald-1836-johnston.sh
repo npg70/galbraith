fa996c92ae4d57c60fdd413e468034883863aa0b
nickg <217179+client9@users.noreply.github.com>
2024-03-04 20:19:06 -0800

    update

---
+++ people/galbraith-donald-1836-johnston.sh
@@ -0,0 +1,20 @@
+name Donald Galbraith
+tags 'Argyll:South Knapdale' Renfrew:Greenock
+birth -date 3-may-1836
+baptism -date 24-may-1836 -location 'South knapdale' -ref donald1836-baptism
+death -date 26-mar-1888 -location greenock -ref donald1836-death
+partner {
+   name Ann Jane Johnston
+   marriage -date 10-feb-1865 -location greenock -ref johnston-marriage
+}
+footnotes {
+    donald1836-baptism {    
+        $opr-ref[b-1836-533-000-0020-0094 "Donald Galbreath"]
+    }
+    donald1836-death {
+        $sp-ref-link[d-1888-564-01-0068 0023 "Donald Galbraith"]
+    }
+    johnston-marriage {
+        $sp-ref[m-1865-564-03-0069 "Donald Galbreath" "Ann Jane Johnston"]
+    }
+}
