4b2624757ef5cdf7a7626bb87010ff54ad5cb357
nickg <217179+client9@users.noreply.github.com>
2024-03-23 10:08:56 -0700

    split out mary

---
+++ people/galbraith-mary-1810-templeton.sh
@@ -0,0 +1,18 @@
+name Mary Galbreath
+baptism -date 15-jul-1810 -location gigha -ref mary1810-baptism
+death -date 10-dec-1891 -location Chicago,Cook,Illinois,USA
+tags Argyll:Gigha Immigrant:USA:Illinois
+todo {
+    add marriage reference, children
+}
+partner {
+    name Thomas Templeton
+    birth -date 11-jan-1801
+    death -date 30-nov-1881 -location Chicago,Cook,Illinois,USA
+    marriage -date 21-jul-1834 -location glasgow
+}
+footnotes {
+    mary1810-baptism {
+        $opr-ref[b-1810-537-000-0010-0044 "Mary Galbreath"]
+    }
+}
