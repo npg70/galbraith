fa0a207b8b2bc2d2393d7af41e918f5a21bd3e5d
nickg <217179+client9@users.noreply.github.com>
2024-03-24 20:51:53 -0700

    add

---
+++ people/galbreath-archibald-1780-templeton.sh
@@ -0,0 +1,39 @@
+name Archibald Galbreath
+tags Argyll:Campbeltown 'dead end'
+external {
+    familysearch KCKB-P89
+}
+body {
+    He was a baker.
+}
+partner {
+    name Jackie Templeton
+    marriage -date 26-jun-1805 -location campbeltown -ref templeton-marriage
+    birth  -date 08-oct-1783
+    baptism -date 12-oct-1783 -location campbeltown
+    body {
+        She was the daughter of Thomas Templeton and Anges/Ann Colville
+    }
+
+    child {
+        name Janet Galbreath
+        birth -date 14-sep-1806
+        baptism -date 17-sep-1806 -location Campbeltown -ref janet1806-baptism
+    }
+}
+footnotes {
+    templeton-marriage {
+        $opr-ref-link[m-1805-507-000-0050-0070 "Archibald Galbreath" "Jackie Templeton"]{
+            Archibald Galbreath Baker in Campbeltown + Jackie Templeton
+            Daughter to Thomas Templeton Farmer in Drungrave[?]. Both
+            of this parish. Contracted 22 + Married 26th June 1805
+        }
+    }
+    janet1806-baptism {
+        $opr-ref-link[b-1806-507-000-0040-0293 "Janet Galbreath"]{
+            Janet | Lawful Daughter of Archibald Galbreath baker in Campbeltown
+            and Jackie Templeton born 14 bapt 17 September 1806. Janet. 
+        }
+    }
+}
+
