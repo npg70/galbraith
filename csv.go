1e716bf087d418672f5be83434c5a912c9bd1888
nickg <217179+client9@users.noreply.github.com>
2024-03-28 09:40:26 -0700

    fix up csv tables a bit

--- csv.go
+++ csv.go
@@ -9,17 +9,22 @@ import (
 func CsvTableHTML(args []string, body string) string {
 	out := strings.Builder{}
 	out.WriteString("<div class=table-responsive>")
-	out.WriteString("<table class='table table-borderless table-sm font-monospace small'>")
+	out.WriteString("<table class='table table-borderless table-sm small ms-3'>")
 	r := csv.NewReader(strings.NewReader(body))
 
+	/* optional caption that probably needs work */
+	caption := strings.Join(args[1:], " ")
+	if caption != "" {
+		out.WriteString("<caption>" + caption + "</caption>")
+	}
 	// read header row
 	row, _ := r.Read()
-	out.WriteString("<tr>")
+	out.WriteString("<thead><tr>")
 	for _, col := range row {
 		// note: bootstrap tables don't inherit colors
 		out.WriteString("<th class='text-body'>" + strings.TrimSpace(col) + "</th>")
 	}
-	out.WriteString("</tr>\n")
+	out.WriteString("</tr></thead>\n")
 
 	for {
 		row, err := r.Read()
