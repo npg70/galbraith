8269a218f5ace67a6039a960541a93a72810a51d
nickg <217179+client9@users.noreply.github.com>
2024-04-03 20:47:10 -0700

    split out another son

---
+++ people/galbraith-hector-mcnair-1923-balloch.sh
@@ -0,0 +1,30 @@
+name Hector McNair Galbraith
+birth -date 1923 -location southend -ref hector1923-birth
+death -date 20-jul-2001 -location campbeltown -ref hector1923-death
+
+external {
+    familysearch GZXY-V45
+    findagrave 120960333
+}
+
+partner {
+    name Mary Balloch
+    marriage -date 1960 -location campbeltown -ref balloch-marriage
+    birth -date 'about 1926'
+    death -date 20-nov-1995 -ref balloch-death
+}
+footnotes {
+    hector1923-birth {
+        $sp-ref[b-1923-532-00-0006 "Hector McNair Galbraith"]
+    }
+    hector1923-death {
+        $sp-ref[d-2001-536-00-0048 "Hector McNair Galbraith"]
+    }
+    balloch-marriage {
+        $sp-ref[m-1960-507-00-0071 "Hector McNair Galbraith" "Mary Balloch"]
+    }
+    balloch-death {
+        $sp-ref[d-1995-536-00-0087 "Mary Galbraith"], references mother's maiden name is "Taylor"
+    }
+
+}
--- people/galbraith-john-mccorkindale-1886-young.sh
+++ people/galbraith-john-mccorkindale-1886-young.sh
@@ -20,11 +20,8 @@ partner {
         birth -date 21-apr-1921 -location southend -ref margaret1921-birth
         death -date 1994 -location campbeltown -ref margaret1921-death
     }
-    child {
-        name Hector McNair Galbraith
-        birth -date 1923 -location southend -ref hector1923-birth
-        death -date 2001 -location campbeltown -ref hector1923-death
-    }
+    child galbraith-hector-mcnair-1923-balloch
+
     child galbraith-andrew-young-1925-mckinnon
 
     child {
@@ -51,12 +48,6 @@ footnotes {
     }
     margaret1921-death {
         $sp-ref[d-1994-536-00-0038 "Margaret McCorkindale Galbraith"]
-    }
-    hector1923-birth {
-        $sp-ref[b-1923-532-00-0006 "Hector McNair Galbraith"]
-    }
-    hector1923-death {
-        $sp-ref[d-2001-536-00-0048 "Hector McNair Galbraith"]
     }
     mary1928-birth {
         $sp-ref[b-1928-532-00-0013 "Mary McCorkindale Galbraith"]
