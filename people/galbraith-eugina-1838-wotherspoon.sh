7d31717a7c390f8ceb52fa908b4e22cecdbc2596
nickg <217179+client9@users.noreply.github.com>
2024-04-22 15:41:12 -0700

    fill out stub

--- people/galbraith-eugina-1838-wotherspoon.sh
+++ people/galbraith-eugina-1838-wotherspoon.sh
@@ -1,17 +1,49 @@
 name Eugina Galbraith
 baptism -date 25-jan-1838 -location campbeltown -ref eugina1838-baptism
 death -date 5-oct-1920 -location clachan -ref eugina1838-death
-tags Argyll:Campbeltown
-todo {
-    stub, marriage, james' parents
-}
+tags Argyll:Gigha Argyll:Campbeltown
 external {
-  familysearch KZLL-XNS
+  familysearch KD34-8ZY
   findagrave 115118723
+}
+todo {
+    Add Census data showing her in Gigha.
 }
 partner {
   name James Wotherspoon
+  baptism -date 30-sep-1833 -location gigha
+  death -date 07-jan-1913 -location gigha
   marriage -date 24-apr-1866 -ref wotherspoon-marriage
+  body {
+    He was the son of James Wotherspoon and Mary Campbell.
+}
+  child {
+        name Archibald Wotherspoon
+        birth -date 02-jul-1866 -location gigha
+  }
+  child {
+        name John Wotherspoon
+        birth -date 10-dec-1868 -location gigha
+  }
+  child {
+        name Eugenia Wotherspoon
+        birth -date 05-nov-1870 -location gigha
+  }
+  child {
+        name Mary Wotherspoon
+        birth -date 21-dec-1871 -location gigha
+  }
+  child {
+        name James Wotherspoon
+        birth -date 30-apr-1874 -location gigha
+   }
+  child {
+        name Helen Wotherspoon
+        birth -date 08-aug-1875 -location gigha
+        body {
+            m. $child-link[galbraith-archibald-1861-wotherspoon]{Archibald Galbraith}
+        }
+    }
 }
 
 footnotes {
@@ -22,6 +54,6 @@ footnotes {
     $sp-ref-link[d-1920-516-00-0006 0002 "Eugenia Galbraith"]  
   }
   wotherspoon-marriage {
-  
+    $sp-ref-link[m-1866-537-00-0001 0001 "Euginia Galbraith" "James Wotherspoon"] 
   }
 }
