42d2c9d87829fe7e964d41b60d0830af149cf716
nickg <217179+client9@users.noreply.github.com>
2024-04-21 20:54:52 -0700

    split out mary, immigrant

--- people/galbreath-james-1726-anderson.sh
+++ people/galbreath-james-1726-anderson.sh
@@ -7,6 +7,7 @@ external {
 partner {
     name Margaret Anderson
     birth -date "about 1725"
+    marriage -date 'about 1747'
     child {
         name James Galbreath
         baptism -date 30-oct-1748 -location campbeltown -ref james1748-baptism
@@ -27,22 +28,8 @@ partner {
         name Ann Galbreath
         baptism -date 12-mar-1756 -location campbeltown -ref ann1756-baptism
     }
-    child {
-        name Mary Galbreath
-        baptism -date 21-jan-1759 -location campbeltown -ref mary1759-baptism
-        death -date 13-apr-1856 -location "virgina,usa"
-        burial -location "Glade Spring,Washington,Virginia,USA" -name "Old Glade Spring Presbyterian Church Cemetery"
-        external {
-            familysearch 9M98-PTF
-            findagrave 41551747
-        }
-        partner {
-            name Peter Clark
-            marriage -date 11-june-1784 -location campbeltown
-            baptism -date 17-mar-1752 -location campbeltown
-            death -date 25-sep-1821 -location "Glade Spring,Washington,Virginia,USA"
-        }
-    }
+    child galbreath-mary-1759-clark
+
     child {
         name James Galbreath
         baptism -date 3-may-1761 -location campbeltown -ref james1761-baptism
@@ -63,7 +50,6 @@ footnotes {
     james1726-baptism {
         $opr-ref-link[b-1726-507-000-0011-0168 "James Galbreath"]
     }
-
     james1748-baptism {
         $opr-ref[b-1748-507-000-0011-0266 "James Galbreath"]
     }
@@ -81,15 +67,6 @@ footnotes {
     ann1756-baptism {
         $opr-ref-link[b-1756-507-000-0011-0299 "Ann Galbreath"]
         $opr-ref-link[b-1756-507-000-0010-0226 "Ann Galbraith"]
-    }
-    mary1759-baptism {
-        $opr-ref[b-1759-507-000-0011-0313 "Mary Galbreath"]{
-        Mary | James Galbreath and Mary Anderson had
-        a daught baptized 23st January named Mary
-    }
-        $opr-ref[b-1759-507-000-0010-0241 "Mary Galbraith"]{
-        $i{1759} Mary | James Galbraith + Mary Anderon bapt Mary $i{no date}
-    } Unclear why Scotlands People has this indexed as 31 Dec 1758.
     }
     james1761-baptism {
         $opr-ref[b-1761-507-000-0011-0325 "James Galbreath"]
---
+++ people/galbreath-mary-1759-clark.sh
@@ -0,0 +1,27 @@
+name Mary Galbreath
+tags Argyll:Campbeltown immigrant:usa:virginia
+baptism -date 21-jan-1759 -location campbeltown -ref mary1759-baptism
+death -date 13-apr-1856 -location "virgina,usa"
+burial -location "Glade Spring,Washington,Virginia,USA" -name "Old Glade Spring Presbyterian Church Cemetery"
+external {
+   familysearch 9M98-PTF
+   findagrave 41551747
+}
+partner {
+   name Peter Clark
+   marriage -date 11-june-1784 -location campbeltown
+   baptism -date 17-mar-1752 -location campbeltown
+   death -date 25-sep-1821 -location "Glade Spring,Washington,Virginia,USA"
+}
+footnotes {
+    mary1759-baptism {
+        $opr-ref[b-1759-507-000-0011-0313 "Mary Galbreath"]{
+        Mary | James Galbreath and Mary Anderson had
+        a daught baptized 23st January named Mary
+        }
+        $opr-ref[b-1759-507-000-0010-0241 "Mary Galbraith"]{
+            $i{1759} Mary | James Galbraith + Mary Anderon bapt Mary $i{no date}
+        }
+        Unclear why Scotlands People has this indexed as 31 Dec 1758.
+    }
+}
