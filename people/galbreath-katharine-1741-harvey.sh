25a99580419a7eabfe0811d58f8df3a81d9dcd1b
nickg <217179+client9@users.noreply.github.com>
2024-04-25 07:10:26 -0700

    cleanup and link to marthas spouse robert clark

--- people/galbreath-katharine-1741-harvey.sh
+++ people/galbreath-katharine-1741-harvey.sh
@@ -2,78 +2,81 @@ name Katharine Galbreath
 tags Argyll:Campbeltown
 baptism -date 10-may-1741 -location campbeltown -ref katharine1741-baptism
 birth -date 3-may-1741
-external () {
+external {
     familysearch 9M9D-F9R
 }
-partner () {
+partner {
     name James Harvie
     birth -date 1735
     marriage -date 1-dec-1761 -location campbeltown
     body {
         He was the son of James Harvey and Janet Mitchell.
     }
-    child () {
+    child {
         name James Harvie
         birth -date 27-oct-1762     
         baptism -date 31-oct-1762 -location campbeltown
         death -date 1847 -location washington,ohio,usa
         burial -location belpre,washington,ohio,usa
     }
-    child () {
+    child  {
         name Martha Harvie
         birth -date 15-nov-1764
         baptism -date 18-nov-1764 -location campbeltown
+        body {
+            Married $child-link[clark-robert-1756-harvie]{Robert Clark}
+        }
         death -date 1821 -location washington,virgina,usa
         burial -location washington,virgina,usa -name 'Old Glade Spring Cemetery'
-        partner () {
+        partner {
             name Robert Clark
             birth -date 1757
             death -date 1821
         }
         body 'Immigrated in 1814 with sons'
     }
-    child () {
+    child  {
         name Janet Harvie
         birth -date 20-jul-1767
         baptism -date 21-jul-1767 -location campbeltown
-        partner () {
+        partner {
             name Alexander Paterson
             marriage -date 17-feb-1792 -location campbeltown
         }
     }
-    child () {
+    child {
         name Archibald Harvey
         birth -date 26-jun-1770
         baptism -date 1-jul-1770 -location campbeltown
     }
-    child () {
+    child {
         name John Harvie
         birth -date 5-dec-1772
         baptism -date 7-dec-1772 -location campbeltown
         death -date 15-jun-1843 -location washington,ohio,usa
         burial -location washington,ohio,usa -name 'Fleming Old Cemetery'
-        partner () {
+        partner {
             name Ann Johnston
             marriage -date 1-apr-1798 -location "isle of man"
         }
     }
-    child () {
+    child {
         name Samuel Harvey
         birth -date 24-may-1775
         baptism -date 25-may-1775 -location campbeltown
     }
-    child () {
+    child {
         name Robert Harvey
         birth -date 11-nov-1777
         baptism -date 16-nov-1777 -location campbeltown    
     }
 
-    child () {
+    child {
         name Andrew Harvey
         birth -date 23-feb-1783
         baptism -date 26-feb-1783 -location campbeltown
     }
-    child () {
+    child {
         name Agnes Harvey
         birth -date 5-jun-1785
         baptism -date 5-jun-1785 -location campbeltown    
