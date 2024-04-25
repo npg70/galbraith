95442d08266a90d8dbc888c4339566aa6111a784
nickg <217179+client9@users.noreply.github.com>
2024-04-25 07:03:03 -0700

    add secondary peoples

---
+++ people/clark-robert-1756-harvie.sh
@@ -0,0 +1,15 @@
+name Robert Clark
+tags Argyll:Campbeltown
+external {
+    familysearch LHVF-TMX
+}
+baptism -date 24-jul-1756 -location campbeltown
+death -date may-1821 -location 'glade spring,washington,virginia,usa'
+partner {
+    name Martha Harvie
+    marriage -date 27-aug-1790 -location campbeltown
+    body {
+        The daughter of $child-link[galbreath-katharine-1741-harvey]{Katherine Galbreath} and James Harvey.
+    }
+}
+
---
+++ people/fleeming-john-1713-langwill.sh
@@ -0,0 +1,36 @@
+name John Fleeming
+tags Argyll:Campbeltown
+
+partner {
+    name Agnes Langwill
+    marriage -date 02-oct-1756 -location campbeltown
+
+    child {
+        name John Fleeming
+        baptism -date 23-jan-1758 -location campbeltown
+        death -date 05-feb-1854 -location 'barlow,washington,ohio,usa'
+        partner {
+            name Jean Colville
+            marriage -date 20-dec-1790
+            death -date 1852
+        }
+    }
+
+    child {
+        name Isobell Fleeming
+        baptism -date 24-feb-1760 -location campbeltown
+    }
+    child {
+        name Agnes Fleeming
+        birth -date 'about 1761' -location campbeltown
+        body {
+            Married $child-link[galbreath-alexander-1753-flemming]{Alexander Galbreath}
+        }
+    }
+    child {
+        name Elizabeth Fleeming
+        birth -date 07-jun-1763
+        baptism -date 08-jun-1763 -location campbeltown
+    }
+}
+
