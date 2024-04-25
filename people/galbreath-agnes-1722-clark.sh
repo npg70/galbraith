6a59a9117e4e04e5d759548de8d13b4906f9b84c
nickg <217179+client9@users.noreply.github.com>
2024-04-25 07:11:41 -0700

    unstub children

--- people/galbreath-agnes-1722-clark.sh
+++ people/galbreath-agnes-1722-clark.sh
@@ -2,6 +2,7 @@ name Agnes Galbreath
 tags Argyll:Campbeltown
 birth -location Kilkeddan
 baptism -date 18-feb-1722 -location campbeltown -ref agnes1722-baptism
+death -date 1792
 external {
     familysearch LZDN-7CY
 }
@@ -9,6 +10,85 @@ partner {
     name James Clark
     marriage -date 7-nov-1739 -location campbeltown
     death -date 1782
+
+    child {
+        name Mary Clark
+        baptism -date 12-apr-1741 -location campbeltown
+        death -date 15-dec-1795 -location 'Amity,Washington,Pennsylvania,USA'
+        partner {
+            name Aaron French
+        }
+    }
+    child {
+        name Margaret Clark
+        baptism -date 4-jun-1743 -location campbeltown
+        death -date 18-jul-1809 -location 'Martins Ferry, Belmont, Ohio, USA'
+        partner {
+            name James Ross
+        }
+    }
+    child {
+        name Janet Clark
+        baptism -date 30-jun-1747 -location campbeltown
+        death -date 1827 -location campbeltown
+        partner {
+            name William Daniel McMurchy
+        }
+    }
+    child {
+        name John Clark
+        baptism -date 17-dec-1749 -location campbeltown
+        death -date 1819 -location 'york,pennslyvania,usa'
+        partner {
+            name Margaret Breckenridge
+        }
+    }
+    child {
+        name Peter Patrick Clark
+        baptism -date 22-mar-1752 -location campbeltown
+        death -date 25-sep-1821 -location 'glade spring,washington,virginia,usa'
+        body {
+            Married $child-link[galbreath-mary-1759-clark]{Mary Galbraith}
+        }
+    }
+    child {
+        name James Clark
+        baptism -date 05-july-1754 -location campbeltown
+        death -date 21-dec-1818 -location 'glade spring,washington,virginia,usa'
+        partner {
+            name Isabella Mary Breckenridge
+        }
+    }
+    child {
+        name William Clark
+    }
+    child {
+        name Helen Clark
+    }
+
+    child clark-robert-1756-harvie
+
+    child {
+        name Katherine Clark
+    }
+    child {
+        name Alexander Clark
+    }
+    child {
+        name Agnes Clark
+    }
+    child {
+        name Jean Clark
+    }
+    child {
+        name James Clark
+    }
+    child {
+        name Gabriel Clark
+    }
+    child {
+        name William Clark
+    }
 }
 footnotes {
     agnes1722-baptism {
