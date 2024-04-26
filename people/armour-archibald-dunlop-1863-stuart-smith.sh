4d7d15c22ac95217fc52999442587bccdf5e3a68
nickg <217179+client9@users.noreply.github.com>
2024-04-26 13:22:17 -0700

    additional armour

---
+++ people/armour-archibald-dunlop-1863-stuart-smith.sh
@@ -0,0 +1,58 @@
+name Archibald Dunlop Armour
+tags Argyll:Campbeltown
+external {
+    familysearch L1C9-4BS
+    findagrave 176479457
+}
+note {
+    Nice bio on $elink[https://www.findagrave.com/memorial/176479457/archibald_dunlop_armour]{Find a Grave}.
+}
+body {
+$blockquote{
+Archibald Dunlop Armour was an original shareholder and Director of the Picture House. He was born into a family of plumbers who ran their business for several generations in Longrow, Campbeltown. Archibald's Father, Robert Armour, was a coppersmith and tinsmith as well as a plumber. Archibald's Grandfather (another Robert Armour) was also a coppersmith, tinsmith and plumber. However, Grandfather Armour was actually an illicit still maker, supplying Kintyre and beyond with whisky stills, and using his plumbing business as a front. He set up his business in 1811 and kept detailed records in his Still Books until 1817.
+}
+$blockquote{
+In 1885, work was carried out in Robert's original shop premises and according to the Campbeltown Courier, "Workmen found a still vat buried pretty far in the ground." and a secret vent leading up to the main chimney. " It is supposed that at one time the ground beneath the floor of the shop has been a vault where secret distilling operations were carried on."
+}
+}
+
+birth -date 8-sep-1863 -location campbeltown
+death -date 22-oct-1938 -location stirling,scotland
+partner {
+    name Esther Stuart
+    birth -date 11-jan-1870 -location Glasgow
+    death -date 25-sep-1903 -location Campbeltown
+    marriage -date 14-oct-1896 -location glasgow
+
+    child {
+        name Mary Forbes Armour
+        birth -date 1898
+        death -date 1922
+    }
+    child {
+        name Robert Armour
+        birth -date 1900
+        death -date 1938
+    }
+    child {
+        name Peter Stuart Armour
+        birth -date 1901
+        death -date 1988
+    }
+    child {
+        name John Armour
+        birth -date 1903
+        death -date 1906
+    }
+}
+partner {
+    name Maggie Wilson Smith
+    birth -date 15-sep-1861 -location Cupar,Fife,Scotland
+    marriage -date 12-apr-1905 -location edinburgh
+
+    child {
+        name Margaret Armour
+        birth -date 1906
+    }
+}
+
