008ac79370b90c1a12afc13d6049281cd1683aa4
nickg <217179+client9@users.noreply.github.com>
2024-05-13 22:18:49 -0700

    update

---
+++ people/galbraith-sarah-1798-clark.sh
@@ -0,0 +1,46 @@
+name Sarah Marion Galbraith
+tags "Argyll:Killean and Kilchenzie" Argyll:Gigha
+external {
+    familysearch LZZL-8FT
+}
+baptism -date 16-nov-1798 -location 'killean and kilchenzie' -ref sarah1798-baptism
+death -date 17-mar-1873 -location gigha -ref sarah1798-death
+
+body {
+$p{
+    She was the daughter of Duncan Galbreath and Mary Kelly.
+}
+$p{
+    She is listed a "Marion Clerk" in the 1841 at Gigha, with her husband Donald.$ref[census1841]
+}
+$p{
+    She is listed as "Marrion Clark" in the 1851 Census at Gigha, with her hisnamd Donald and a step-daughter Barbara McKenzie age 19.$ref[census1851]
+}
+}
+note {
+    "Sarah" is the ancelization of "Marion", "Moore", etc.
+}
+partner {
+    name Donald Clark
+    birth -date 'about 1774'
+    marriage -date 26-jul-1836 -location 'killean and kilchenzie' -ref clark-marriage
+}
+
+footnotes {
+    sarah1798-baptism {
+        $opr-ref[b-1798-519-000-0010-0131 "Sarah Galbreath"]
+    }
+    sarah1798-death {
+        $sp-ref-link[d-1873-537-00-0002 0001 "Marion Galbraith"]
+    }
+    clark-marriage {
+        $opr-ref[m-1836-519-000-0020-0234 "Sarah Galbreath" "Donald Clark"]
+    }
+    census1841 {
+        https://www.findmypast.com/transcript?id=GBC/1841/0016657015
+    }
+    census1851 {
+        https://www.findmypast.com/transcript?id=GBC%2F1851%2F0019321719
+    }
+}
+
