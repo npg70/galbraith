920b529742ae259993ea3792405ceffb4f7f550d
nickg <217179+client9@users.noreply.github.com>
2024-05-18 10:53:38 -0700

    consolidate

--- english.go
+++ english.go
@@ -111,3 +111,10 @@ func AllMostSome(s []string) (string, string) {
 	}
 	return "", ""
 }
+
+// number to roman numeral. Doesn't go beyond 20, and doesn't handle errors.
+// fine for intended purpose.
+
+func toRoman(num int) string {
+	return []string{"", "i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix",
+		"x", "xi", "xii", "xiii", "xiv", "xv", "xvi", "xvii", "xviii", "xix", "xx"}[num]
--- roman.go
+++
@@ -1,9 +0,0 @@
-package main
-
-// number to roman numeral. Doesn't go beyond 20, and doesn't handle errors.
-// fine for intended purpose.
-
-func toRoman(num int) string {
-	return []string{"", "i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix",
-		"x", "xi", "xii", "xiii", "xiv", "xv", "xvi", "xvii", "xviii", "xix", "xx"}[num]
-}
