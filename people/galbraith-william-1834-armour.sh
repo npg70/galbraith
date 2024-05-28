179b542049591b609c7ecee6833a2dc36fb72b43
nickg <217179+client9@users.noreply.github.com>
2024-05-27 22:50:41 -0700

    add kilbarchan galbraiths

---
+++ people/galbraith-william-1834-armour.sh
@@ -0,0 +1,89 @@
+name William Galbraith
+external {
+    familysearch K46X-469
+}
+tags Renfrew:kilbarchan
+
+baptism -date 21-jul-1834 -location kilbarchan,renfrew -ref william1834-baptism
+death -date 23-mar-1891 -location linwood,renfrew -ref william1834-death
+
+partner {
+    name Elizabeth Armour
+    birth -date 'about 1833'
+    marriage -date 03-jun-1856 -location kilbarchan -ref armour-marriage
+    death -date 31-july-1873 -location kilbarchan -ref armour-death
+    body {
+        She was the daughter of William Armour and Janet Anderson.
+    }
+    child {
+        name John Galbraith
+        birth -date 17-mar-1857 -location kilbarchan
+        death -date 26-jan-1938 -location Lochwinnoch,Renfrew -ref john1857-death
+        partner {
+            name Mary Barr Gibson
+            marriage -date 1887 -location Paisley
+        }
+
+    }
+    child {
+        name Janet Carswell Galbraith
+        birth -date 27-jul-1859 -location kilbarchan
+        death -date 16-jun-1862 -location kilbarchan
+    }
+    child {
+        name William Galbraith
+        birth -date 02-jul-1862 -location kilbarchan
+        death -date 29-jul-1945 -location Elderslie,Renfrew
+        partner {
+            name Devina Murray
+            marriage -date 24-jun-1884
+        }
+    }
+    child {
+        name Elizabeth Galbraith
+        birth -date 22-aug-1864 -location kilbarchan
+        death -date 16-jul-1873 -location kilbarchan -ref elizabeth1864-death
+    }
+    child {
+        name Robert Galbraith
+        birth -date 18-dec-1866 -location kilbarchan
+        death -date 23-oct-1937 -location Aberdeen
+        partner {
+            name Eliza  Jane Jack Alison
+            marriage -date 13-dec-1892
+        }
+    }
+    child {
+        name Allan Galbraith
+        birth -date 23-jan-1870 -location kilbarchan
+    }
+    child {
+        name James Galbraith
+        birth -date 09-nov-1872 -location kilbarchan
+    }
+}
+partner {
+    name Jean Melvin
+    marriage -date 03-jun-1884 -location linwood,renfrew
+}
+
+footnotes {
+    william1834-baptism {
+        $opr-ref[b-1834-568-000-0030-0050 "William Galbraith"]
+    }
+    william1834-death {
+        $sp-ref-link[d-1891-568-02-00011 0004 "William Galbraith"]
+    }
+    armour-marriage {
+        $sp-ref-link[m-1856-568-00-0010 0005 "William Galbraith" "Elizabeth Armour"]
+    }
+    armour-death {
+        $sp-ref-link[d-1873-568-00-0092 0031 "Elizabeth Galbraith"]
+    }
+    john1857-death {
+        $sp-ref-link[d-1938-570-00-0002 0001 "John Galbraith"]
+    }
+    elizabeth1864-death {
+        $sp-ref-link[d-1873-568-00-0086 0029 "Elizabeth Galbraith"]
+    }
+}
