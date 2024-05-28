d9d59f68d61267fcdaa5b9a265bc69c2e5ad3fe9
nickg <217179+client9@users.noreply.github.com>
2024-05-27 22:51:00 -0700

    add kilbarchan galbraiths

---
+++ people/galbraith-john-1797-caldwell.sh
@@ -0,0 +1,46 @@
+name John Galbraith
+birth -date 'about 1797'
+death -date 27-feb-1861 -location kilbarchan -ref john1797-death
+external {
+    familysearch K8F5-JV4
+}
+
+partner {
+    name Janet Caldwell
+    birth -date 1799
+    marriage -date 03-july-1819 -location kilbarchan
+    death -date 03-aug-1883 -location linwood,renfrew -ref caldwell-death
+
+    child {
+        name Robert Galbraith
+        baptism -date 17-apr-1820 -location kilbarchan -ref robert1820-baptism
+    }
+    child {
+        name Janet Galbraith
+        baptism -date 13-mar-1822 -location kilbarchan -ref janet1822-baptism
+    }
+    child {
+        name John Galbraith
+        baptism -date 20-may-1825 -location kilbarchan -ref john1825-baptism
+    }
+    child galbraith-william-1834-armour
+
+}
+
+footnotes {
+    john1797-death {
+        $sp-ref-link[d-1861-568-00-0033 0011 "John Galbraith"]
+    }
+    caldwell-death {
+        $sp-ref-link[d-1883-568-02-0045 0015 "Janet Galbraith"]
+    }
+    robert1820-baptism {
+        $opr-ref[b-1820-568-000-0030-0005 "Robert Galbraith"]
+    }
+    janet1822-baptism {
+        $opr-ref[b-1822-568-000-0030-0009 "Janet Galbraith"]
+    }
+    john1825-baptism {
+        $opr-ref[b-1825-568-000-0030-0020 "John Galbraith"]
+    }
+}
