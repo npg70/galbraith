06e8da7b276e8f195c1ea09b98cd6323c4d6bde9
nickg <217179+client9@users.noreply.github.com>
2024-05-27 22:49:44 -0700

    add more spouse info

--- people/galbraith-edward-1863-mcdonald.sh
+++ people/galbraith-edward-1863-mcdonald.sh
@@ -5,7 +5,12 @@
  death -date 29-jun-1921 -location 'killean and kilchenzie' -ref edward1863-death
  partner {
     name Elizabeth McDonald
+    birth -date 'about 1866'
     marriage -date 6-jun-1907 -location 'killean and kilchenzie' -ref edward1863-marriage
+    death -date 20-jan-1947 -location tarbert -ref mcdonald-death
+    body {
+        She was the daughter of Donald McDonald and Annie Aird.
+    }
  }
 
  footnotes {
@@ -18,6 +23,8 @@
     edward1863-marriage {
         $sp-ref-link[m-1907-519-00-0003 0002 "Edward Galbraith" "Elizabeth MacDonald"]
     }
-
+    mcdonald-death {
+        $sp-ref-link[d-1947-535-00-0003 0001 "Elizabeth Galbraith"]
+    }
 }
 
