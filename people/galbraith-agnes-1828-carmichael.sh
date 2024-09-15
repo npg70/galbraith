a40beeb1fb21d2f1dd23e751b156c701ad10961f
nickg <217179+client9@users.noreply.github.com>
2024-09-15 07:35:26 -0700

    various updates

--- people/galbraith-agnes-1828-carmichael.sh
+++ people/galbraith-agnes-1828-carmichael.sh
@@ -1,10 +1,16 @@
 name Agnes Galbreath
 baptism -date 3-aug-1828 -location campbeltown -ref agnes1828-baptism
 death -date 16-apr-1901 -location campbeltown -ref agnes1828-death
+burial -name 'Kilchousland Graveyard'
+external {
+    wikitree Galbraith-3176
+    findagrave 114485206
+}
 partner {
    name John Carmichael
-   death -date 1865
-   marriage -date 24-apr-1849
+   birth -date 1825
+   death -date 15-apr-1865 -location dalintober
+   marriage -date 24-apr-1849 -location campbeltown
 }
 footnotes {
     agnes1828-baptism {
@@ -12,5 +18,15 @@ footnotes {
     }
     agnes1828-death {
         $sp-ref-link[d-1901-507-00-0046 0016 "Agnes Galbraith"]
+    }
+}
+footnotes {
+    agnes1828-baptism {
+    }
+    agnes1828-death {
+        
+    }
+    john-marriage {
+
     }
 }
