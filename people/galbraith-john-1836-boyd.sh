146818a9c31c97568b0fb0a7b7fa9c78e5ad0a1e
nickg <217179+client9@users.noreply.github.com>
2024-09-25 14:50:31 -0700

    census

--- people/galbraith-john-1836-boyd.sh
+++ people/galbraith-john-1836-boyd.sh
@@ -3,13 +3,14 @@ tags 'Argyll:Kilcalmonell and Kilberry' Argyll:Campbeltown Renfrew:Greenock
 
 birth -date 25-mar-1836
 baptism -date 21-jul-1836 -location kilcalmonell -ref john1836-baptism
+death -date 1925 -location nottingham -note speculative
 external {
     familysearch M1YK-YP2
 }
 
 partner {
     name Margaret Boyd
-    birth -date 'about 1838' -note 'daughter of Daniel Boyd and Elizabeth Rissick'
+    birth -date 'about 1838' -location Ireland -note 'daughter of Daniel Boyd and Elizabeth Rissick'
     marriage -date 6-aug-1863 -location greenock -ref boyd-marriage
     death -date 23-apr-1901 -location greenock -ref boyd-death
 
@@ -21,6 +22,7 @@ partner {
         name Elizabeth Galbraith
         birth -date 03-jan-1866 -location greenock -ref elizabeth1866-birth
     }
+
     child galbraith-jane-boyd-1867-henderson
 
     child {
@@ -38,6 +40,58 @@ partner {
 }
 body {
     He was an insurance agent.
+
+Census 1871 - 15, Nelson Street, Greenock, West Parish, Renfrewshire, Scotland$ref[census-1871]
+
+$csvtable{
+First,Last,Role,Age,Year,Place,Occupation
+John,Galbraith,Head,35,1836,Kilcalmonel,Hatter & Hosier
+Margaret B,Galbraith,Wife,35,1836,Ireland,-
+Mary,Galbraith,Dau,6,1865,Greenock,-
+Elizabeth,Galbraith,Dau,5,1866,Greenock,-
+Jane B,Galbraith,Dau,3,1868,Campbeltown,-
+John,Galbraith,Son,1,1870,Greenock,-
+}
+
+Census 1881 - 17, Patrick Street, Greenock West, West District, Renfrewshire, Scotland$ref[census-1881]
+
+$csvtable{
+First,Last,Role,Age,Year,Place,Occupation
+John,Galbraith,Head,45,1836,Kilcalmonel,Hatter & Hosier
+Margaret B,Galbraith,Wife,44,1837,Ireland,-
+Mary,Galbraith,Dau,16,1865,Greenock,Assisting at home
+Elizabeth,Galbraith,Dau,15,1866,Greenock,Scholar
+Jane B,Galbraith,Dau,13,1868,Greenock,Scholar
+John,Galbraith,Son,11,1870,Greenock,Scholar
+}
+
+    Census 1891 - 9, Ardgowan Street (W), Greenock, West Greenock, Renfrewshire, Scotland$ref[census-1891]
+
+$csvtable{
+First,Last,Role,Age,Year,Place,Occupation
+John,Galbraith,Head,55,1836,Kilcalmonell,Insurance agent
+Margaret,Galbraith,Wife,53,1838,Glasgow,-
+Elizabeth,Galbraith,Dau,25,1866,Greenock,-
+Jane B,Galbraith,Dau,23,1868,Greenock,Teacher for the deaf mistress
+}
+
+
+    Census 1901 - 24, Patrick Street, Greenock, West District, Renfrewshire, Scotland$ref[census-1901]
+
+$csvtable{
+First,Last,Role,Age,Year,Place,Occupation
+John,Galbraith,Head,65,1836,Kilcalmonel,Insurance agent (fire life)
+Marg,Galbraith,Wife,63,1838,Ireland,-
+Elizabeth,Galbraith,Dau,35,1866,Greenock,-
+}
+
+Census 1911 - Cairnorrie Hilton Road, Carlton, Nottinghamshire, England$ref[census-1911]
+
+$csvtable{
+First,Last,Role,Age,Year,Place,Occupation
+John,Galbraith,Head,75,1836,Kilcalmonel,Retired hatter and hosier dealer
+Elizabeth,Galbraith,Dau,45,1866,Greenock,-
+}
 }
 
 footnotes {
@@ -70,5 +124,17 @@ footnotes {
     }
     angus1872-death {
         $sp-ref[d-1872-563-03-0623 "Angus Cunningham Galbraith"]
+    }
+    census-1881 {
+        https://www.findmypast.com/transcript?id=GBC%2F1881%2F0029543662&expand=true&tab=this
+    }
+    census-1891 {
+        https://www.findmypast.com/transcript?id=GBC%2F1891%2F0035381933&expand=true&tab=this
+    }
+    census-1901 {
+        https://www.findmypast.com/transcript?id=GBC%2F1901%2F0036077202&expand=true&tab=this
+    }
+    census-1911 {
+        https://www.findmypast.com/transcript?id=GBC%2F1911%2FRG14%2F20468%2F0373%2F1&tab=this
     }
 }
