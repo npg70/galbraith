37449ed88e75620f6fb47507fab19507f0ccd8c2
nickg <217179+client9@users.noreply.github.com>
2024-09-27 11:31:00 -0700

    source

--- sources/kintyre-magazine-55.sh
+++ sources/kintyre-magazine-55.sh
@@ -8,4 +8,4 @@ $blockquote{
 This stone is situated about 300 yards up from the junction at Polliwilline. The road that now runs from Macharioch to Polliwilline is still called the 'new' road by some of the older farmers in the district, namely, $b{Andrew Galbraith}, late of Langholm, and my father.
 }
 
-Possibly $child-link[galbraith-andrew-young-1925-mckinnon]{Andrew Young Galbraith}, born Southend in 1925.
+$child-link[galbraith-andrew-young-1925-mckinnon]{Andrew Young Galbraith}, born Southend in 1925.  Identity confirmed by author via email 26-Sep-2024.
