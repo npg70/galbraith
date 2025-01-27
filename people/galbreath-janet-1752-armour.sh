f99b4c0cbdcf0910077e2806102c593174d3f535
nickg <217179+client9@users.noreply.github.com>
2025-01-26 22:35:22 -0800

    add references for children

--- people/galbreath-janet-1752-armour.sh
+++ people/galbreath-janet-1752-armour.sh
@@ -8,17 +8,18 @@ external {
 partner {
     name James Armour
     baptism -date 28-apr-1751
-    marriage -date 15-dec-1780 -location campbeltown -ref armour-marriage
+    marriage -date 'about 1773' -location campbeltown -ref armour-marriage
     body {
         He was the son of William Armour and $child-link[galbreath-mary-1729-armour]{Mary Galbreath}.
     }
     child {
         name Andrew Armour
-        baptism -date 15-mar-1774
+        birth -date 13-mar-1774
+        baptism -date 15-mar-1774 -location campbeltown -ref andrew1774-baptism
     }
     child {
         name Martha Armour
-        baptism -date 10-sep-1775
+        baptism -date 10-sep-1775 -location campbeltown
     }
     child {
         name William Armour
@@ -33,7 +34,8 @@ partner {
     }
     child {
         name Mary Armour
-        baptism -date 27-jun-1781 -location campbeltown
+        birth -date 25-jun-1781
+        baptism -date 27-jun-1781 -location campbeltown -ref mary1781-baptism
         death -date 30-mar-1858 -location glasgow
         partner {
             name John McCallum
@@ -41,7 +43,8 @@ partner {
     }
     child {
         name Archibald Armour
-        baptism -date 13-jun-1783
+        birth -date 10-jun-1783
+        baptism -date 13-jun-1783 -location campbeltown -ref archibald1783-baptism
         death -date 1841
         partner {
             name Isabella Greenlees
@@ -52,12 +55,13 @@ partner {
     }
     child {
         name Janet Armour
-        baptism -date 25-oct-1785
+        birth -date 23-oct-1785
+        baptism -date 25-oct-1785 -location campbeltown -ref janet1785-baptism
         death -date 20-nov-1826
     }
     child {
         name Ann Armour
-        baptism -date 6-jun-1792
+        baptism -date 6-jun-1792 -location campbeltown
     }
 }
 footnotes {
@@ -65,6 +69,31 @@ footnotes {
         $opr-ref-link[b-1752-507-000-0011-0284 "Janet Galbreath"]
     }
     armour-marriage {
-        unconfirmed.  Date is from un-sourced ancestry.com trees.
+        OPR marriage record has not been found.
+    }
+    andrew1774-baptism {
+        $opr-ref-link[b-1774-507-000-0020-0011 "Andrew Armour"]{
+            Andrew | Lawful son to James Armour and Jenat
+            Galbreath Born 13th March Bapt 15th
+        }
+    }
+    mary1781-baptism {
+        $opr-ref-link[b-1781-507-000-0020-0054 "Mary Armour"]{
+            May | Lawful Dau to James Armour &
+            Janet Galbreath born 25th June was Bapt 27th 1781
+        }
+    }
+    archibald1783-baptism {
+        $opr-ref-link[b-1783-507-000-0020-0068 "Archibald Armour"]{
+            Archibald | Lawfull son to James Armour in Trodigal &
+            Jenat Galbraith was born 10th & Bapt 13th July 
+
+        }
+    }
+    janet1785-baptism {
+        $opr-ref-link[b-1785-507-000-0020-0085 "Jenat Armour"]{
+            Jenat | Lawfl dau to James Armour Farmer in Trodigal &
+            Jenat Galbraith was born 23rd & Bapt 25th Oct
+        }
     }
 }
