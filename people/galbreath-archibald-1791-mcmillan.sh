6e2f2f6962528231a01bca383524550321010885
nickg <217179+client9@users.noreply.github.com>
2025-01-31 09:29:42 -0800

    add parents, transcriptions, gender, occupation

--- people/galbreath-archibald-1791-mcmillan.sh
+++ people/galbreath-archibald-1791-mcmillan.sh
@@ -8,17 +8,22 @@ note {
     Latest is that he died in Dungannon, County Tyrone, Ireland in 1869 age 78.  The official Irish website seems to imply that's all information available.  BNA search turned up no newpaper notice of his death in Ireland.
 }
 
+body {
+    He was a sailor or fisherman in Campbeltown and Dalintober.
+}
+
 external {
     familysearch GXM5-KT5
     wikitree Galbraith-1866
 }
 partner {
     name Margaret McMillan
+    gender female
+    parent {
+        name Duncan McMillan
+    }
     marriage -date 16-feb-1813 -location campbeltown -ref marriage
     baptism -date 25-Oct-1791 -location campbeltown 
-    body {
-        Daughter of Neil McMillan, b. 1750 and Margaret McEachine
-    }
     child galbreath-anne-1814-cook
     child {
         name Niel Galbreath
@@ -30,9 +35,16 @@ partner {
 
 footnotes {
     marriage {
-        $opr-ref-link[m-1813-507-000-0050-0103 "Archibald Galbreath" "Margaret MacMillan"]
+        $opr-ref-link[m-1813-507-000-0050-0103 "Archibald Galbreath" "Margaret MacMillan"]{
+            Archibald Galbreath Sailor Campbeltown and Margaret MacMillan Daughter
+            to the deceased Duncan McMillan late weaver in Campbeltown both of this
+            Parish was married the 16th day of February 1813.
+        }
     }
     niel1816-baptism {
-        $opr-ref-link[b-1816-507-000-0040-0449 "Niel Galbreath"]
+        $opr-ref-link[b-1816-507-000-0040-0449 "Niel Galbreath"]{
+            Niel | Lawful Son to Archibald Galbreath Fisher Dalintober and Margaret 
+            MacMillan born 17th baptized 18th August 1816 Named Niel
+        }
     }
 }
