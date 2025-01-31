dfd8444655d9b70e39535760bbf4a8ee99686f0f
nickg <217179+client9@users.noreply.github.com>
2025-01-30 16:28:09 -0800

    solving david galbreath x greenlees

---
+++ people/greenlees-thomas-1754-greenlees.sh
@@ -0,0 +1,261 @@
+name Thomas Greenlees
+gender male
+baptism -date 04-apr-1754 -location campbeltown 
+death -date 21-mar-1830 -location campbeltown
+external {
+    familysearch 9MMT-FMV
+    findagrave 167547983
+}
+note {
+    The baptisms from the Old Parish Records contain two errors.  Helen is referred to as "Elisabeth Greenlees" and "Helen Ferguson." Possible remarried?
+}
+
+body {
+    He was a malster, although also listed as a "Dyster"
+}
+note {
+    Not in the Census of 1792.
+}
+body {
+The grave marker says four children died in infancy.  Of the 11 known children:
+$ul{
+$li{7 are accounted for by death or marriage}
+$li{2 are assumed died in infancy}
+$li{Which leaves Robert and Thomas}
+}
+}
+
+body {
+Census of 1841, Long Row, Campbeltown, Argyllshire, Scotland:$ref[census1841]
+$csvtable{
+First,Last,Age,Year
+Widow, Greenlees, 75, 1766
+Margaret, Greenlees, 48, 1793
+Helen, Greenlees, 45, 1796
+John, Greenlees, 35, 1806
+Jean, Greenlees, 30, 1811
+}
+}
+
+body {
+Census of 1851, Long Row, Campbeltown, Cantyre, Argyllshire, Scotland:$ref[census1851]
+$csvtable{
+First,Last,Role,Status,Age,Year
+Helen, Greenlees, Head, Window, 86, 1765
+Margaret, Greenlees, Dau, Unm, 50, 1801
+John, Greenlees, Son, Umn, 43, 1808
+Jean, Greenlees, Dau,-, 40, 1811
+}
+}
+body {
+Census of 1861, Longrow, Campbeltown, Argyllshire, Scotland:$ref[census1861]
+$csvtable{
+First,Last,Role,Status,Age,Year
+Jane [Jean],Greenlees,Aunt,Umn,50,1811
+Margaret,Greenlees,Aunt,Umn,70,1791
+John,Greenlees,Uncle,Umn,57,1804
+}
+}
+
+body {
+Census of 1871, Burnside Street, Campbeltown, Argyllshire, Scotland:$ref[census1871]
+$csvtable{
+First,Last,Role,Status,Age,Year
+Jean,Greenlees,Head,Unm,64,1807
+John,Greenlees,Brother,Unm,66,1805
+}
+}
+
+partner {
+    name Helen Greenlees
+    gender female
+    birth -date 22-mar-1765
+    baptism -date 24-mar-1765 -location campbeltown
+
+    marriage -date 'about 1788'
+
+    child {
+        name Mary Greenlees
+        gender female
+        baptism -date 13-jan-1790 -location campebeltown -ref mary1790-baptism
+        partner galbreath-david-1793-greenlees
+
+    }
+    child {
+        name Margaret Greenlees
+        gender female
+        baptism -date 25-sep-1791 -location campebeltown -ref mary1791-baptism
+        death -date 15-feb-1864 -location campbeltown -ref mary1791-death
+    }
+    child {
+        name John Greenlees
+        gender male
+        baptism -date 01-jul-1793 -location campbeltown -ref john1793-baptism
+        death -note dy
+    }
+    child {
+        name Helen Greenlees
+        gender female
+        baptism -date 30-aug-1794 -location campbeltown -ref helen1794-baptism
+        death -date 'before 1851'
+    }
+    child {
+        name Hugh Greenlees
+        gender male
+        baptism -date 21-jul-1796 -location campbeltown -ref hugh1796-baptism
+        death -note dy
+    }
+    child {
+        name Robert Greenlees
+        gender male
+        baptism -date 28-aug-1798 -location campbeltown -ref robert1798-baptism
+        death -note dy
+    }
+    child {
+        name Hugh Greenlees
+        gender male
+        birth -date 6-nov-1799
+        baptism -date 8-dec-1799 -location campbeltown -ref hugh1799-baptism
+        death -date 14-feb-1855 -location campbeltown -ref hugh1799-death
+        burial -name 'kilkerran cemetery'
+        partner {
+            name Jean Colville
+            gender female
+            birth -date 24-dec-1799
+            baptism -date 25-dec-1799 -location campbeltown
+            marriage -date 17-jun-1822 -location campbeltown
+        }
+    }
+    child {
+        name Thomas Greenlees
+        gender male
+        baptism -date 10-dec-1800 -location campbeltown -ref thomas1800-baptism
+        death -note dy
+    }
+    child {
+        name John Greenlees
+        gender male
+        baptism -date 08-dec-1801 -location campbeltown -ref john1801-baptism
+        death -date 17-feb-1873 -location campbeltown -ref john1801-death
+    }
+    child {
+        name Jean Greenlees
+        gender female
+        birth -date 30-may-1804
+        baptism -date 31-may-1804 -location campbeltown -ref jean1804-baptism
+        death -date 15-nov-1879 -location campbeltown -ref jean1804-death -note unmarried
+    }
+    child {
+        name Isobell Greenlees
+        gender female
+        birth -date 14-aug-1806
+        baptism -date 15-aug-1806 -location campbeltown -ref isobell1806-baptism
+        death -date 05-mar-1861 -location campbeltown -ref isobell1806-death
+        partner {
+            name Alexander McPherson
+        }
+    }
+}
+
+footnotes {
+    mary1790-baptism {
+        $opr-ref-link[b-1790-507-000-0020-0117 "Mary Greenlees"]{
+            Mary Lawful dau to Thomas Greenlees [?] mill Strawmiln &
+            Helen Greenlees was born 12th + Bapt. 13th Janry
+        }
+    }
+    mary1791-baptism {
+        This record contains two errors. "Margaret" is the correct first name,
+        and the mother was Hellen not Elisabeth Greenlees.
+
+        $opr-ref-link[b-1791-507-000-0020-0135 "Mary Greenlees"]{
+            Mary | Lawful dau. to Thomas Greenlees Dyster at Lintmiler[?]
+            + Elisabeth Greenlies was born 24th & Bapt 25th Sept
+        }
+    }
+    mary1791-death {
+        $sp-ref-link[d-1864-507-00-0031 0011 "Margaret Greenlees"]{
+            Name: Margaret Greenlees (single);
+            Death: 1864 February Fifthtenth, Longrow Campbeltown;
+            Age: 73 years (~1791)
+            Father: Thomas Greenlees, Malster (deceased);
+            Mother: Helen Greenlees, M.S Greenlees (deceased);
+        }
+    }
+    john1793-baptism {
+        $opr-ref-link[b-1793-507-000-0040-0108 "John Greenlees"]{
+            John Lawful son to Thomas Greenlees & Helen Greenlees in Strathmiller ...
+        }
+    }
+    helen1794-baptism {
+        $opr-ref-link[b-1794-507-000-0040-0118 "Hellen Greenlees"]{
+            Hellen Lawful dau to Thomas Greenlees & Helen Greenlees in Strath 
+            born 28th bapt 31
+        }
+    }
+    hugh1796-baptism {
+        $opr-ref-link[b-1796-507-000-0040-0141 "Hugh Greenlees"]
+    }
+    robert1798-baptism {
+        Note mispelling of name "Grenlees" and incorrect mothers name of "Ferguson".
+        $opr-ref-link[b-1798-507-000-0040-0171 "Robert Grenlees"]{
+            Robert | Lawful son to Thomas Grenlees & Helen Ferguson Strath
+            was born 25th & Baptized 28th the child name Robert.
+        }
+    }
+    hugh1799-baptism {
+        $opr-ref-link[b-1799-507-000-0040-0192 "Hugh Greenlees"]
+    }   
+    hugh1799-death {
+        $sp-ref-link[d-1855-507-01-0017 0006 "Hugh Greenlees"]{
+            Name: Greenlees, Thomas, Distiller;
+            Age: 57 years (~1798);
+            Born: Campbeltown;
+            Father: Thomas Greenlees, Maltster (deceased);
+            Mother: Helen Greenlees, Maiden Name Greenlees;
+        }
+    }
+    thomas1800-baptism {
+        $opr-ref-link[b-1800-507-000-0040-0209 "Thomas Greenlees"]{
+            Thomas | Thomas Greenlees + Hellen Greenlees in Strath
+            had a lawful son born 7th + bapt 10th named Thomas
+        }
+    }
+    john1801-baptism {
+        $opr-ref-link[b-1801-507-000-0040-0222 "John Greenlees"]
+    }
+    john1801-death {
+        $sp-ref-link[d-1873-507-00-0047 0016 "John Greenlees"]
+    }   
+    jean1804-baptism {
+        $opr-ref-link[b-1804-507-000-0040-0263 "Jean Greenlees"]{
+            Jean | Lawful Daughter to Thomas Greenlees Dyer at Strat Miller and
+            Helen Greenlees. Born 30th Baptized 31st May 1804 Named Jean
+       }
+    }
+    jean1804-death {
+        $sp-ref[d-1879-507-00-0169 "Jean Greenlees"]
+    }
+    isobell1806-baptism {
+        $opr-ref-link[b-1806-507-000-0040-0292 "Isobell Greenlees"]{
+            Isobell | Lawful Daughter to Thomas Greenlees Dyster Strath Milar and
+            Helen Greenlees born 14 baptized 15 August 1806 named Isobell
+        }
+    }
+    isobell1806-death {
+        $sp-ref-link[d-1861-507-00-0031 0011 "Isabella Greenlees"]
+    }
+
+    census1841 {
+        https://www.findmypast.com/transcript?id=GBC%2F1841%2F0016597110&expand=true&tab=this
+    }
+    census1851 {
+        https://www.findmypast.com/transcript?id=GBC%2F1851%2F0019256843&expand=true&tab=this
+    }
+    census1861 {
+        https://www.findmypast.com/transcript?id=GBC%2F1861%2F0022163832&expand=true&tab=this
+    }
+    census1871 {
+        https://www.findmypast.com/transcript?id=GBC%2F1871%2F0023442843&expand=true&tab=this
+    }
+}
