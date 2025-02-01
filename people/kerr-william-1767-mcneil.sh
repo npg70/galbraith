4a65d0dd8fd611f09d9ae63cdfaddcd32456e3a3
nickg <217179+client9@users.noreply.github.com>
2025-02-01 10:46:24 -0800

    add children, etc

--- people/kerr-william-1767-mcneil.sh
+++ people/kerr-william-1767-mcneil.sh
@@ -1,16 +1,15 @@
 name William Kerr
+gender male
 external {
     familysearch 9M9Z-KQY
 }
 body {
     Overseer or manager of coal mine.
 }
-todo {
-    There are more children.
-}
 partner {
     name Margaret McNeil
-    marriage -date 25-jan-1788 -location campbeltown
+    gender female
+    marriage -date 25-jan-1788 -location campbeltown -ref mcneil-marriage
 
     child {
         name John Kerr
@@ -23,12 +22,48 @@ partner {
         baptism -date 13-jul-1790 -location campbeltown -ref charles1790-baptism
     }
     child {
+        name Mary Kerr
+        baptism -date 15-jun-1793 -location campbeltown
+    }
+    child {
+        name Edward Kerr
+        baptism -date 01-apr-1795 -location campbeltown
+    }
+    child {
+        name Margaret Kerr
+        baptism -date 21-mar-1797 -location campbeltown
+    }
+    child {
+        name Robert Kerr
+        birth -date 17-apr-1799
+        baptism -date 21-apr-1799  -location campbeltown
+    }
+    child {
+        name William Kerr
+        baptism -date 12-aug-1801 -location campbeltown
+    }
+    child {
         name David Kerr
         birth -date 10-dec-1803
         baptism -date 27-dec-1803 -location campbeltown -ref david1803-baptism
+        death -date 1841 -location Melbourne,Victoria,Australia
     }
+    child {
+        name Elizabeth Wilson Kerr
+        baptism -date 29-aug-1805 -location campbeltown
+    }
+
 }
 footnotes {
+    mcneil-marriage {
+        $opr-ref-link[m-1788-507-000-0050-0023 "William Kerr" "Margaret McNeil"]{
+            William Kerr of this Parish & Margaret McNeil [illegible] 25th
+        }
+        $opr-ref-link[m-1788-519-000-0010-0316 "William Ker" "Peggy McNiell"]{
+            Wiliam Ker from the parishes of Campbeltown and Peggy
+            McNiell of this parish Contracted Janry 18th Married
+        }
+    }   
     john1788-baptism {
         $opr-ref-link[b-1788-507-000-0020-0108 "John Kerr"]{
             John Lawful son to Wm Kerr Knockinoch & Margt. MacNeill
