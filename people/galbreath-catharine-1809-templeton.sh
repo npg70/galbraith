f5e80405a3bcdab2f07719f0f62a9ab6ff67011c
nickg <217179+client9@users.noreply.github.com>
2025-02-02 20:52:48 -0800

    split out daughters

---
+++ people/galbreath-catharine-1809-templeton.sh
@@ -0,0 +1,73 @@
+name Catharine Galbreath
+gender female
+tags Argyll:Campbeltown 'Dead End'
+birth -date 01-apr-1809
+baptism -date 25-apr-1809 -location campbeltown -ref catharine1809-baptism
+
+note {
+    Family vanishes.  No records found after last child. 
+}
+note {
+    Hugh Templeton, malster, is seen in Midlothian, with a different wife Janet.$ref[census1851]$ref[census1861].  Strong maybe.
+}
+
+partner {
+    name Hugh Templeton
+    gender male
+    marriage -date 23-feb-1837 -location campbeltown -ref marriage
+    body {
+   	    maltster
+    }
+    child {
+        name Martha Templeton
+        birth -date 26-jan-1838
+        baptism -date 27-jan-1838 -location campbeltown
+    }
+    child {
+        name Ann Templeton
+        birth -date 26-mar-1840
+        baptism -date 01-apr-1840 -location campbeltown
+   }
+    child {
+        name Janet Templeton
+        birth -date 23-jun-1842
+        baptism -date 31-jul-1842 -location campbeltown
+    }
+    child {
+        name Margaret Templeton
+        birth -date 25-dec-1844
+        baptism -date 02-feb-1845 -location campbeltown
+    }
+    child {
+        name Catharine Templeton
+        birth -date 18-apr-1847
+        baptism -date 02-may-1847 -location campbeltown
+    }
+}
+
+footnotes {
+  catharine1809-baptism {
+  Last entry on page:
+  	$opr-ref-link[b-1809-507-000-0040-0333 "Catharine Galbreath"]{
+        Catharine | Lawful Daughter to James Galbreath Farmer Skerbolin
+        & Martha Galbreath born 1st baptized 25th Apr 1809 Cathatine
+    }
+  }
+  marriage {
+    $opr-ref-link[m-1837-507-000-0060-0335 "Catharine Galbreath" "Hugh Templeton"]{
+        Hugh Templeton Malster and Catharine Galbreath daughter 
+        of the late James Galbreath Skerbolin both of this Parish
+        were married twenty-third Febry 1837
+    }
+    }
+   census1841 {
+    Recorded as "Tempelton"
+    https://www.findmypast.com/transcript?id=GBC%2F1841%2F0016602774&expand=true&tab=this
+    }
+    census1851 {
+        https://www.findmypast.com/transcript?id=GBC%2F1851%2F0018777401&expand=true&tab=this
+    }
+    census1861 {
+        https://www.findmypast.com/transcript?id=GBC%2F1861%2F0024006792&expand=true&tab=this
+    }
+}
---
+++ people/galbreath-jean-1817-pickan-kelly.sh
@@ -0,0 +1,56 @@
+name Jean Galbreath
+external {
+    findagrave 114485211
+    ancestry tree/12079504/person/282649352363
+}
+gender female
+birth -date 25-nov-1817
+baptism -date 3-dec-1817 -location campbeltown -ref jean1817-baptism
+death -date 12-mar-1900 -location campbeltown -ref jean1817-death
+partner {
+    name Robert Pickan
+    gender male
+    death -date 'before 1849'
+    marriage -date 01-jan-1839 -location campbeltown -ref pickan-marriage
+    body {
+        He was farmer in Smerby.
+    }
+}
+partner {
+  	name Robert Kelly
+    gender male
+    birth -date 06-nov-1814 -location campbeltown
+    marriage -date 14-mar-1849 -location campbeltown -ref kelly-marriage
+    death -date 28-Nov-1859 -location 'sea'
+    body {
+        blacksmith, shipmaster
+    }
+}
+
+footnotes {
+  jean1817-baptism {
+  	$opr-ref-link[b-1817-507-000-0040-0469 "Jean Galbreath"]{
+        Jean | Lawful Daughter to James Galbreath Farmer Skerbolin
+        and Martha Galbreath born 25th Nov baptized 3 Dec 1817 Named Jean
+    }
+  }
+  jean1817-death {
+    $sp-ref-link[d-1900-507-00-0063 0021 "Jean Galbraith"]
+    }
+    pickan-marriage {
+        $opr-ref-link[m-1839-507-000-0060-0349 "Jean Galbreath" "Robert Pickan"]{
+            Robert Pickan farmer Smerby and Jean Galbreath
+            daughter of the late _____ Galbreath Skerbolin 
+            both of this Parish were married first January 1839
+         }
+     }
+    kelly-marriage {
+        $opr-ref-link[m-1849-507-000-0060-0431 "Jean Galbreath" "Robert Kelly"]{
+            Robert Kelly Blacksmith in Campbeltown & Jean Galbreath
+            Daughter of _____ Galbreath Farmer in _____ and Relict of Robert
+            Pickan late Farmer Smerby both of this Parish were married 
+            fourthteenth March 1849 
+
+        }
+    }
+}
