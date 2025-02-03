77408641c5baabc6e0c6a5679d31b97e27e97962
nickg <217179+client9@users.noreply.github.com>
2025-02-02 20:52:16 -0800

    add census 1881

--- people/galbreath-donald-1819-mclean.sh
+++ people/galbreath-donald-1819-mclean.sh
@@ -1,4 +1,5 @@
 name Donald Galbraith
+gender male
 tags 'Argyll:Killean and Kilchenzie' Argyll:Campbeltown
 external {
     familysearch LZ68-L2C
@@ -9,25 +10,40 @@ death -date 20-nov-1897 -location campbeltown -ref death
 note {
     Deaths, marriages, and decendants not investigated. 
 }
+body {
+Census of 1881: 6, High Street, Campbeltown, Argyllshire, Scotland:$ref[census1881]
+$csvtable{
+First,Last,Role,Status,Age,Year,Place,Job
+Donald,Galbraith,Head,Married,60,1821,BALACHANTYNE K PARISH GAELIC,Cartwright
+Isabella,Galbraith,Wife,Married,63,1818,CAMPBELTOWN GAELIC,Cartwrights wife
+Donald,Galbraith,Son,Unmarried,23,1858,CAMPBELTOWN GAELIC,Laborer
+Alexander,Galbraith,Son,Unmarried,21,1860,CAMPBELTOWN GAELIC,Mason
+}
+}
 partner {
     name Isabella McLean
+    gender female
     birth -date 10-mar-1816 -location campbeltown  -note 'daughter of Donald McLean and Agnes Stewart'
     marriage -date 13-dec-1847 -location campbeltown -ref mclean-marriage
     death -date 30-dec-1887 -location campbeltown -ref mclean-death
     child {
         name Agnes Galbreath
+        gender male
         baptism -date 25-jan-1848 -location campbeltown -ref agnes1848-baptism
     }
     child {
         name Isabella Galbraith
+        gender female
         baptism -date 22-aug-1850 -location campbeltown -ref isabella1850-baptism
         death -date 16-feb-1940 -location campbeltown -ref isabella1850-death
         partner {
             name Peter McGregor
+            gender male
         }
     }
     child {
         name Margaret Galbraith
+        gender female
         baptism -date 29-aug-1853 -location campbeltown -ref margaret1853-baptism
         death -date 31-mar-1857 -location campletown -ref margaret1853-death
     }
@@ -35,6 +51,7 @@ partner {
 
     child {
         name Alexander Galbreath
+        gender male
         birth -date 13-may-1859 -location campbeltown -ref alexander1859-birth
     }
 }
@@ -80,6 +97,9 @@ footnotes {
     }
     alexander1859-birth {
         $sp-ref-link[b-1859-507-00-0082 0028 "Alexander Galbreath"]
+    }
+    census1881 {
+       https://www.findmypast.com/transcript?id=GBC%2F1881%2F0029341604&expand=true&tab=this
     }
 }
 
