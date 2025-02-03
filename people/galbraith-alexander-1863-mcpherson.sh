93acf0f4714b6c66a96c0ed196a4c19951ab0b52
nickg <217179+client9@users.noreply.github.com>
2025-02-02 20:23:51 -0800

    remove todo, add gender, add census, add spouse parents

--- people/galbraith-alexander-1863-mcpherson.sh
+++ people/galbraith-alexander-1863-mcpherson.sh
@@ -1,15 +1,31 @@
 name Alexander Galbraith
+gender male
 tags Glasgow Argyll:Dunoon
 external {
     familysearch 9Q3N-7FL
 }
-todo {
-    Census 1891: https://www.findmypast.com/transcript?id=GBC/1891/0035194337&expand=true
+body {
+Census of 1891: Bellmont Place, Dunoon & Kilmun, Dunoon, Argyllshire, Scotland:$ref[census1891]
+$csvtable{
+First,Last,Role,Status,Age,Year,Place,Job
+Duncan,McPherson,Head,Widower,74,1817,Argyll,Retired Teacher
+Alexander,Galbraith,Son in Law,Married,27,1864,Glasgow,Coachman
+Elizabeth,Galbraith,Daughter,Married,27,1864,-,Greenock
+James,McPherson,Son,Unmarried,24,1867,Greenock,Seaman
 }
+}
+
 birth -date 13-aug-1863 -location glasgow -ref alexander1863-birth
 partner {
     name Elizabeth Davie MacPherson
-    birth -date 30-jul-1860 -location greenock -note 'daughter of Duncan McPherson and Elizabeth Wilson Davie'
+    gender female
+    parent {
+        name Duncan McPherson
+        partner {
+            name Elizabeth Wilson Davie
+        }
+    }
+    birth -date 30-jul-1860 -location greenock
     death -date 1917 -location paisley -ref macpherson-death
     marriage -date 24-sep-1890  -location Dunoon -ref macpherson-marriage
 
@@ -44,5 +60,9 @@ footnotes {
     }
     malcolm1894-death {
         $sp-ref-link[d-1955-564-00-0249 0083 "Malcolm Henry Galbraith"]
+    }
+
+    census1891 {
+        https://www.findmypast.com/transcript?id=GBC/1891/0035194337&expand=true
     }
 }
