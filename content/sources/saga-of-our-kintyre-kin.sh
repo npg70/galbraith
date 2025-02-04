16cb0b3bfc0bbbdcf5076fe315a30fd7349baaeb
nickg <217179+client9@users.noreply.github.com>
2025-02-04 14:57:33 -0800

    add missing

---
+++ content/sources/saga-of-our-kintyre-kin.sh
@@ -0,0 +1,24 @@
+---
+title: Saga of our Kintyre Kin, 2nd Ed
+---
+
+$h1{Saga of our Kintyre Kin, 2nd}
+
+Only one Galbraith, but very useful for extended relations.  Excellent background data on Kintyre as well.
+
+$hr{}
+$section{
+$child-link[ralston-andrew-1758-fleeming]{Andrew Ralson} / Margaret Fleming - Page 83
+
+Unfortunately no clues on Margaret, but extensive notes on the southend farm.
+}
+
+$hr{}
+$section{
+$child-link[galbreath-catharine-1788-greenlees]{Catharine Galbreath} and James Greenlees.
+
+Has some information on the Peninver farms.
+}
+$hr{}
+
+
