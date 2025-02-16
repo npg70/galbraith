ac95a405f542247b6ae62f8a6832d182e0798855
nickg <217179+client9@users.noreply.github.com>
2025-02-16 07:35:09 -0800

    opr gaps add

---
+++ content/journal/opr-gaps.sh
@@ -0,0 +1,63 @@
+---
+title: OPR Gaps
+---
+
+$h2{Campbeltown births, 1686-1686}
+
+Page $opr-page[b-1680-507-000-0011-0048] lists a 2.5 year gap:
+
+$blockquote{
+Here we meet with a want in the Register from 1st 
+Jan 1684 until 18th June 1686. Being immediately it appears
+after the Decrease of Mr Edward Keith, and the said Register was
+kept during the interval between Mr Keith's Death and Mr.
+??? ordination by John Logan Grammar Schoolmaster
+at that time.
+}
+
+The minister's death is recorded in $opr-page[b-1682-507-000-0011-0044]
+
+$h2{Campbeltown Births: A 5 month gap 1708-1709}
+
+On page $opr-page[b-1708-507-000-0011-0104], there is gap From 21-Aug-1708 to 30-Jan-1709
+
+$blockquote{
+There occurs some vacant places in the original.
+}
+
+
+$h2{Campbeltown Births: A 3 month gap 1748-1749:}
+
+Page $opr-page[b-1748-507-000-0011-0267] lists a 4 month gap:
+
+$blockquote{
+Here occurs a blank from the 23rd November 1748 
+until the 2nd April 1749
+}
+
+$h2{Campbeltown Marriages, gap 1705-1706}
+
+Page $opr-page[m-1705-507-000-0011-0414] lists a 13 month gap:
+
+$blockquote{
+Here occurs a blank in the register from the above date ($i{19 Feb 1705})
+, till 24th March 1706
+}
+
+$h2{Campbeltown Marriages, gap 1773-1778}
+
+$ul{
+$li{$opr-page[m-1773-507-000-0020-0157] - Last entry is 11-Oct-1773}
+$li{$opr-page[m-1773-507-000-0020-0158] - Blank except for beautiful caligraphy that just says "Marriages"}
+$li{$opr-page[m-1773-507-000-0020-0159] - Undated "page 79" has 5 entries, no year listed. One of which the marriage of $child-link[/galbreath-william-1736-dunlop]{William Galbreath} and Mary Campbell.}
+$li{$opr-page[m-1773-507-000-0020-0160] - First entry says 04-feb-1776, but date was written in later.  Second entry is 27-dec-1777, and then continues normally.}
+}
+
+This likely explains missing marriage records for:
+$ul{
+$li{$child-link[galbreath-samuel-1736-mcnair]{Samuel Galbreath} and Janet McNair}
+$li{$child-link[galbreath-mary-1751-park]{Mary Galbreath} and John Park (second marriage)}
+$li{$child-link[galbreath-janet-1752-armour]{Janet Galbreath} and James Armour}
+$li{$child-link[harvey-andrew-1749-orr]{Andrew Harvey} and Margaret Orr}
+$li{$child-link[harvey-james-1751-mcnair]{James Harvey} and Jean McNair}
+}
