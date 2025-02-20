fd3706c14700f42f93b44912a3a3017b5450bfd4
nickg <217179+client9@users.noreply.github.com>
2025-02-20 06:21:27 -0800

    gender, references

--- people/fleeming-daniel-1721-johnstone.sh
+++ people/fleeming-daniel-1721-johnstone.sh
@@ -1,68 +1,134 @@
 name Daniel Fleeming
+gender male
+tags Argyll:Campbeltown
+
 external {
     familysearch LW6B-59G
 }
 
 baptism -date 29-jan-1721 -location campbeltown
 
+note {
+    Not in the Census of 1792.
+}
+
 partner {
     name Isabel Johnstone
+    gender female
     marriage -date 10-feb-1751 -location campbeltown
 
     child {
         name Robert Fleeming
+        gender male
         baptism -date 05-jun-1752 -location campbeltown
     }
     child {
         name Archibald Fleeming
+        gender male
         baptism -date 16-aug-1754 -location campbeltown
     }
     child {
         name Henry Fleeming
-        baptism -date 04-apr-1756 -location campbeltown
+        gender male
+        baptism -date 04-apr-1756 -location campbeltown -ref henry1756-baptism
     }
     child {
         name James Fleeming
-        baptism -date 20-mar-1758 -location campbeltown
+        gender male
+        baptism -date 20-mar-1758 -location campbeltown -ref james1758-baptism
     }
     child {
         name Agnes Fleeming
+        gender female
         baptism -date 05-jan-1760 -location campbeltown -ref agnes1760-baptism
         body {
-            Perhaps married $child-link[albreath-alexander-1753-flemming]{Alexander Galbreath}
+            Perhaps married $child-link[galbreath-alexander-1753-flemming]{Alexander Galbreath}, immigrant at Peach Bottom, York, Pennsylvania.
         }
     }
     child {
         name Rachel Fleeming
-        baptism -date 25-jan-1762 -location campbeltown
+        gender female
+        baptism -date 25-jan-1762 -location campbeltown -ref rachel1762-baptism
     }
     child {
         name Elizabeth Fleeming
-        baptism -date 19-jan-1764 -location campbeltown
+        gender female
+        birth -date 18-jan-1764
+        baptism -date 19-jan-1764 -location campbeltown -ref eliz1764-baptism
         partner {
-            name Achibald McQuilkan
+            name Archibald McQuilkan
+            gender male
+            marriage -date 10-sep-1784 -location campbeltown -ref mcquilkan-marriage
+            body { also known as Achibald Wilkinson }
+        }
+    }
+    child {
+        name Daniel Fleeming
+        gender male
+        birth -date 21-oct-1765
+        baptism -date 25-oct-1765 -location campbeltown -ref daniel1765-baptism
+        body {
+            Twin.
         }
     }
     child {
         name Adam Fleeming
+        gender male
         birth -date 21-oct-1765
-        baptism -date 25-oct-1765 -location campbeltown
-    }
-    child {
-        name Daniel Fleeming
-        birth -date 21-oct-1765
-        baptism -date 25-oct-1765 -location campbeltown
+        baptism -date 25-oct-1765 -location campbeltown -ref adam1765-baptism
+        body {
+            Twin.
+        }
     }
 }
 footnotes {
+    henry1756-baptism {
+        $opr-ref-link[b-1756-507-000-0011-0299 "Henry Fleeming"]{
+            Henry | Daniel Fleeming and Isobel Johnston
+            had a son baptized 4th April named Henry
+        }
+    }
+    james1758-baptism {
+        $opr-ref-link[b-1756-507-000-0011-0309 "James Fleeming"]{
+            James | Daniel Fleeming and Isobel Johnston
+            had a son born + baptized 20th March named James
+        }
+    }
     agnes1760-baptism {
-
         $opr-ref-link[b-1760-507-000-0011-0318 "Agnes Fleeming"]{
             Agnes | Daniel Fleeming merchant and Ann Johnstone
             had a Daughter baptized 5th Janry named Agnes
         }
-        Unclear why mother used "Ann" and not "Isobel"
+        Unclear why mother is recorded as "Ann" and not "Isobel"
+    }
+    rachel1762-baptism {
+        $opr-ref-link[b-1762-507-000-0011-0328 "Rachel Fleeming"]{
+            Rachel | Daniel Fleeming merchant and Ann Johnstone
+            had a Daughter born + baptized 25th Janry N Rachel
+        }
+    }
+    eliz1764-baptism {
+        $opr-ref-link[b-1764-507-000-0011-0348 "Elizabeth Fleeming"]{
+            Elizabeth | Daniel Fleeming merchant and Ann Johnstone
+            had a Daughter born 18th baptized 19th Janry named Elizabeth
+        }
+    }
+    mcquilkan-marriage {
+        $opr-ref-link[m-1784-507-000-0020-0168 "Betty Fleeming" "Archibald McQuilkan"]{
+            Archd McQuilkan of Niels[?] Betty Fleeming both of this Parish 
+            contracted 10th Sept & Married
+        }
+    }
+    daniel1765-baptism {
+        $opr-ref-link[b-1765-507-000-0011-0348 "Adam Fleeming"]{
+            Daniel + Adam | Danl Fleeming + Isobel Johnston had twins
+            born the 21st and baptized the 25th Oct named Daniel + Adam
+        }
+    }
+    adam1765-baptism {
+        $opr-ref-link[b-1765-507-000-0011-0348 "Adam Fleeming"]{
+            Daniel + Adam | Danl Fleeming + Isobel Johnston had twins
+            born the 21st and baptized the 25th Oct named Daniel + Adam
+        }
     }
 }
-
-
