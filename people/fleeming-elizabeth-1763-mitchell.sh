6385db77b5d085b926795e5776eef5081503e323
nickg <217179+client9@users.noreply.github.com>
2025-02-20 06:20:56 -0800

    add kilkerran inscription, findgrave, marriage transcriptions

--- people/fleeming-elizabeth-1763-mitchell.sh
+++ people/fleeming-elizabeth-1763-mitchell.sh
@@ -2,14 +2,25 @@ name Elizabeth Fleeming
 gender female
 external {
     familysearch KZ4K-N9S
+    findagrave 204250053
 }
 birth -date 07-jun-1763
 baptism -date 08-jun-1763 -location campbeltown -ref elizabeth1763-baptism
 death -date 11-nov-1805 -location campbeltown
+body {
+    Not in the Census of 1792.
+}
+body {
+Kilkerran Cemetery inscription (coincidentally? next to $child-link[galbreath-archibald-1760-langwill-corbet]{Archibald Galbreath}):
+$blockquote{
+971	Erected by ROBERT MITCHELL in memory of ELIZABETH FLEMING his spouse who died 11 November 1805 aged 42 years.  The above ROBERT MITCHELL died June 1824 aged 74 years, their daughter ELIZABETH wife of PETER PATERSON died 29 April 1837 aged 37 years, PETER PATERSON died at Glasgow 11 November 1878 aged 88 years, CATHERINE PATERSON daughter of the said PETER PATERSON and ELIZABETH MITCHELL died 20 November 1907 aged 79 years and interred here.
+}
+}
+
 partner {
     name Robert Mitchell
     gender male
-    marriage -date 01-apr-1785 -location campbeltown
+    marriage -date 01-apr-1785 -location campbeltown -ref mitchell-marriage
     birth -date 'about 1750'
     death -date jun-1824
 }
@@ -19,6 +30,11 @@ footnotes {
         $blockquote{
             Eliz | John Fleeming and Ann Langwill had a Daughter
             born 7th and baptized 8th June named Elizabeth
+        }
+    }
+    mitchell-marriage {
+        $opr-ref-link[m-1785-507-000-0020-0169 "Elizabeth Fleeming" "Robert Mitchell"]{
+            Robert Mitchell & Eliza Fleeming both of this Parish contracted 1st April & married
         }
     }
 }
