e8411a8cb6192972f67b3cf9a18414bbcf9db31d
nickg <217179+client9@users.noreply.github.com>
2025-02-23 19:08:10 -0800

    initial langwill research

---
+++ people/langwill-james-1725-orr.sh
@@ -0,0 +1,107 @@
+name James Langwill
+gender male
+parent {
+    name John Langwill
+    gender male
+    partner {
+        name Marion Fleeming
+        gender female
+    }
+}
+baptism -date 07-mar-1725 -location campbeltown
+external {
+    familysearch KC3P-4MB
+}
+
+partner {
+    name Janet Orr
+    gender female
+    parent {
+        name John Orr
+        gender male
+        partner {
+            name Agnes Paterson
+            gender female
+        }
+    }
+    baptism -date 11-nov-1728 -location campbeltown
+    marriage -date 20-oct-1746 -location campbeltown
+
+    child {
+        name John Langwill
+        gender male
+        baptism -date 27-sep-1747 -location campbeltown -ref john1747-baptism
+    }
+    child {
+        name Peter Langwill
+        gender male
+        baptism -date 05-apr-1749 -location campbeltown -ref peter1749-baptism
+        death -date 22-jul-1749 -location campbeltown
+        burial -name 'kilkerran cemetery'
+    }
+    child {
+        name James Langwill
+        gender male
+        baptism -date 12-apr-1752 -location campbeltown -ref james1752-baptism
+        partner {
+            name Jane Montgomery
+            gender female
+        }
+    }
+    child {
+        name Agnes Langwill
+        gender female
+        baptism -date 10-dec-1759 -location campbeltown -ref agnes1759-baptism
+        partner {
+            name James Mitchell
+            gender male
+        }
+    }
+    child {
+        name Mathew Langwill
+        gender male
+        birth -date 27-feb-1762
+        baptism -date 28-feb-1762 -location campbeltown -ref matt1762-baptism
+    }
+    child {
+        name Janet Langwill
+        gender female
+        birth -date 02-feb-1766 -location campbeltown
+        baptism -date 04-feb-1766 -location campbeltown -ref janet1766-baptism
+        body {
+            There a 3 Janet Langwills born around this time.  Family Search is confused.
+        }
+    }
+}
+footnotes {
+    john1747-baptism {
+        $opr-ref[b-1747-507-000-0011-0261 "John Langwill"]
+    }
+    peter1749-baptism {
+        $opr-ref-link[b-1749-507-000-0011-0267 "Peter Langwill"]{
+            Peter | James Langwill and Janet Orr had a son
+            baptized 5th April named Peter
+        }
+    }
+    james1752-baptism {
+        $opr-ref[b-1752-507-000-0011-0281 "James Langwill"]
+    }
+    agnes1759-baptism {
+        $opr-ref[b-1759-507-000-0011-0317 "Agnes Langwill"]
+        $opr-ref[b-1759-507-000-0010-0246 "Agnes Langwill"]
+    }
+    matt1762-baptism {
+        $opr-ref-link[b-1762-507-000-0011-0328 "Mathew Langwill"]{
+            Mathew | James Langwill and Janet Orr had a son born
+            the 27th and baptized the 28th Febry named Mathew
+        }
+        $opr-ref[b-1762-507-000-0010-0263 "Matthew Langwill"]
+    }
+    janet1766-baptism {
+        $opr-ref-link[b-1766-507-000-0011-0350 "Janet Langwill"]{
+            Janet | James Langwill and Janet Orr had a Daughter
+            born on 2nd & baptized 4th February named Janet
+        }
+        $opr-ref[b-1766-507-000-0010-0302 "Janet Langwill"]
+    }
+}
