e9f3b08975031dec9f56c87f5b85a58b3800eda7
nickg <217179+client9@users.noreply.github.com>
2025-02-24 11:08:29 -0800

    add gender, transcribe baptisms

--- people/galbraith-john-1730-clark.sh
+++ people/galbraith-john-1730-clark.sh
@@ -14,33 +14,48 @@ partner {
     marriage -date 30-sep-1758 -location campbeltown -ref clark-marriage
     child {
         name Peter Galbraith
+        gender male
+        birth -date 28-jan-1766
         baptism -date 29-jan-1766 -location campbeltown -ref peter1766-baptism
     }
     child {
         name Matthew Galbraith
+        gender male
+        birth -date 22-feb-1768
         baptism -date 22-feb-1768 -location campbeltown -ref matthew1768-baptism
     }
     child {
         name Archibald Galbreath
+        gender male
         baptism -date 22-nov-1774 -location campbeltown -ref archibald1774-baptism
     }
 }
 footnotes {
     clark-marriage {
-        $opr-ref-link[m-1758-507-000-0010-0237 "John Galbraith" "Jean Clark"]
-        $opr-ref-link[m-1758-507-000-0011-0459 "John Galbraith" "Jean Clark"]
+        $opr-ref-link[m-1758-507-000-0011-0459 "John Galbraith" "Jean Clark"]{
+            John Galbraith + Jean Clark | Oct 
+        }
+        $opr-ref-link[m-1758-507-000-0010-0237 "John Galbraith" "Jean Clark"]{
+            Oct | John Galbraith + Jean Clark 
+        }
     }
-
     peter1766-baptism {
-        $opr-ref-link[b-1766-507-000-0011-0350 "Peter Galbreath"]
+        $opr-ref-link[b-1766-507-000-0011-0350 "Peter Galbreath"]{
+            Peter | John Galbreath and Jean Clark had a son born
+            the 28th Baptized 29th of January named Peter
+        }
         $opr-ref[b-1766-507-000-0010-0301 "Peter Galbraith"], note mother indexed as "Jean Clerk"
     }
     matthew1768-baptism {
-        $opr-ref-link[b-1768-507-000-0011-0363 "Mathew Galbreath"]
+        $opr-ref-link[b-1768-507-000-0011-0363 "Mathew Galbreath"]{
+            Mathew | John Galbreath and Jean Clark had a son born
+            and baptized 22 Febry named Mathew
+        }
         $opr-ref[b-1768-507-000-0010-0324 "Matthew Galbraith"]
     }
     archibald1774-baptism {
-        $opr-ref-link[b-1774-507-000-0020-0015 "Archibald Galbreath"]
+        $opr-ref-link[b-1774-507-000-0020-0015 "Archibald Galbreath"]{
+            Arch | Son to John Galbreath and Jean Clark Born 2[___] Bapt 22nd.
+        }
     }
-
 }
