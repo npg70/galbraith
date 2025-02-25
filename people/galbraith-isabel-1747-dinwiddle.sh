612cbd20769586617bf3989e3f8b1034c8fc3872
nickg <217179+client9@users.noreply.github.com>
2025-02-24 21:16:29 -0800

    add wikitree, references

--- people/galbraith-isabel-1747-dinwiddle.sh
+++ people/galbraith-isabel-1747-dinwiddle.sh
@@ -2,17 +2,31 @@ name Isabel Galbraith
 gender female
 external {
    familysearch KLH1-6L9
+   wikitree Galbreath-155
 }
 note {
     Confused with $child-link[galbreath-isobell-parks]{Isobell Galbreath} who married John Parks.
 }
 birth -date 1747 -location Bucks,Pennsylvania,USA
-death -date 28-sep-1831 -location Greene,Tennessee,USA
+death -date 28-sep-1831 -location Greene,Tennessee,USA -ref death
 partner {
    name James Dinwiddle
+   birth -date 1740
+   death -date 1807
+   body {
+    Last will on 19 Apr 1806.$ref[last-will]  It may reference other children.  Probate TBD.
+   }
 }
 partner {
    name _____ Gaston
+}
+footnotes {
+    death {
+        http://files.usgwarchives.net/tn/greene/bibles/sevier01.txt 
+    }
+    last-will {
+        https://www.ancestry.com/mediaui-viewer/collection/1030/tree/28478523/person/12056937312/media/461350f1-3ecd-493d-b47e-aefe08d2b2f7?queryId=e65134cc-39af-4af2-9351-afeac2537832&searchContextTreeId=&searchContextPersonId=&_phsrc=nGB544&_phstart=successSource
+    }
 }
 
 
