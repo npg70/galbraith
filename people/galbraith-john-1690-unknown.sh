964ab1adb4317df13235b9198314aa6ef9063881
nickg <217179+client9@users.noreply.github.com>
2025-02-27 13:00:22 -0800

    add parent stub

---
+++ people/galbraith-john-1690-unknown.sh
@@ -0,0 +1,14 @@
+name John Galbraith
+gender male
+birth -date 23-dec-1690 -location 'Northern Ireland'
+tags Ireland USA:Pennsylvania
+external {
+    cgs 270129707975
+}
+partner {
+    name Janet _____
+    gender female
+    child galbraith-john-jr-1714-smith
+    child galbraith-william-1731-lowry-kerr
+}
+
