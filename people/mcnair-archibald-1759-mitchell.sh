c144c4c4af625c09a807f8feb026851ba15ff1ae
nickg <217179+client9@users.noreply.github.com>
2025-02-28 10:16:14 -0800

    add child

--- people/mcnair-archibald-1759-mitchell.sh
+++ people/mcnair-archibald-1759-mitchell.sh
@@ -75,13 +75,27 @@ partner {
         gender male
         baptism -date 26-mar-1792 -location campbeltown
     }
+
     child mcnair-john-1793-mckenzie
+
+    child {
+        name Jean Mitchell
+        gender female
+        baptism -date 01-dec-1796 -location ayr -ref jean1796-baptism
+    }
 }
 footnotes {
     mitchell-marriage {
         $opr-ref-link[m-1787-507-000-0020-0170 "Archibald McNair" "Margaret Mitchell"]{
             Archd McNair & Marg Mitchell both of this parish contracted
             16th Aug & were married.
+        }
+    }
+    jean1796-baptism {
+        $opr-ref-link[b-1796-578-000-0050-0065 "Jean McNair"]{
+            Jean McNair Dau Law of Archibald McNair Farmer in Cock-h???
+            and Margt Mitchell his spouse was born 27th Nov 1796
+            Bapt 1st Dec by Dn McGill
         }
     }
 }
