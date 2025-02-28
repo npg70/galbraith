8214e689d729c644937c1d81155ba97df7a76b36
nickg <217179+client9@users.noreply.github.com>
2025-02-27 21:22:56 -0800

    add other kids

--- people/galbraith-james-1666-chambers.sh
+++ people/galbraith-james-1666-chambers.sh
@@ -3,6 +3,7 @@ gender male
 tags Ireland USA:Pennsylvania Immigrant:USA:Pennsylvania
 ydna yes
 birth -date 1666 -location ireland
+death -date 23-aug-1744 -location 'Donegal,Lancaster,Pennsylvania,USA'
 external {
    cgs 270120723219 
 }
@@ -13,5 +14,49 @@ partner {
 
     child galbraith-john-1690-unknown
 
+    child {
+        name Andrew Galbraith
+        gender male
+        birth -date 'about 1692' -location 'Northern Ireland'
+        partner {
+            name Mary Kyle
+            gender female
+        }
+    }
+    child {
+        name Robert Galbraith
+        gender male
+        birth -date 'about 1700' -location 'Northern Ireland'
+    }
+    child {
+        name Rebecca Galbraith
+        gender female
+        birth -date 1703 -location 'Newtown Cunningham, Donegal, Ireland'
+        death -date jan-1748 -location 'Donegal, Lancaster, Pennsylvania, USA'
+        partner {
+            name Alexander Stewart Baronet
+            gender male
+        }
+    }
     child galbraith-james-1703-bertram
+
+    child {
+        name Isabel Galbraith
+        gender female
+        birth -date 1705 -location Ireland
+        partner {
+            name Alexander McMillan
+            gender male
+        }
+    }
+    child {
+        name Eleanor Galbraith
+        gender female
+        birth -date 1717 -location Ireland
+        death -date 1758 -location 'Cumberland,Pennsylvania,USA'
+        partner {
+            name Patrick McKinley
+            gender male
+        }
+    }
 }
