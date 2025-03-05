5afe76512ae87c21c5317bb3006db5744a976548
nickg <217179+client9@users.noreply.github.com>
2025-03-05 10:45:45 -0800

    fill out children

--- people/galbreath-margaret-1771-mcnair.sh
+++ people/galbreath-margaret-1771-mcnair.sh
@@ -14,7 +14,8 @@ note {
     There is extensive biographical information on her $elink[https://www.findagrave.com/memorial/42220658/margaret-mcnair]{Find-A-Grave} memorial.
 }
 body {
-    Immigration in 1821.
+    He was a farmer on Kildonan where all children where born.
+    The family immigrated in 1821.
 }
 
 partner {
@@ -24,6 +25,125 @@ partner {
    baptism -date 06-jan-1763 -location campbeltown
    death -date 13-sep-1857 -location 'New Mills,New Brunswick,Canada'
    marriage -date 24-apr-1802 -location campbeltown -ref mcnair-marriage
+
+   child {
+        name Archibald McNair
+        gender male
+        birth -date 12-mar-1803
+        baptism -date 16-mar-1803 -location campbeltown
+        death -date 07-may-1870 -location 'New Mills, New Brunswick, Canada'
+        partner {
+            name Isabellan MacMillan
+            birth -date 1808
+            death -date 1888
+            marriage -date 29-jan-1832 -location 'New Mills, New Brunswick, Canada'
+        }
+    }
+    child {
+        name David McNair
+        gender male
+        birth -date 11-jan-1805
+        baptism -date 13-jan-1805 -location campbeltown
+        death -date 02-may-1883 -location 'New Mills, New Brunswick, Canada'
+        partner {
+            name Janet Ferguson
+            gender female
+            birth -date 1812
+            death -date 1887
+            marriage -date 18-apr-1837 -location 'Addington,New Brunswick,Canada'
+        }
+    }
+    child {
+        name Margaret McNair
+        birth -date 01-oct-1807
+        baptism -date 04-oct-1807 -location campbeltown
+        death -date 1879 -location 'Noble,Indiana,USA'
+        partner {
+            name Nathaniel McNair
+            gender male
+            birth -date 1800 -location Ayr
+            death -date 1865 -location 'Noble,Indiana,USA'
+            marriage -date 10-aug-1826 -location Beresford,New Brunswick,Canada
+        }
+        body {
+            Marriage to cousin needs investigating.
+        }
+    }
+    child {
+        name John McNair
+        gender male
+        birth -date 13-oct-1809 
+        baptism -date 15-oct-1809
+        death -date 26-feb-1889 -location 'New Mills, New Brunswick, Canada'
+    }
+    child {
+        name Mary McNair
+        gender female
+        birth -date 16-may-1811
+        baptism -date 17-may-1811 -location campbeltown
+        death -date 26-feb-1889 -location 'New Mills, New Brunswick, Canada'
+        partner {
+            name Elizabeth Kelso
+            gender female
+            birth -date 1811
+            death -date 1872
+            marriage -date 06-apr-1836 -location 'Addington,New Brunswick,Canada'
+        }
+    }
+    child {
+        name James McNair
+        gender male
+        birth -date 04-sep-1818
+        baptism -date 08-sep-1813 -location campbeltown
+        death -date 1890 -location 'Arrow Rock, Saline, Missouri, USA'
+        partner {
+            name Elizabeth Black
+            gender female
+            marriage -date 28-feb-1837 -location 'Addington,New Brunswick,Canada'
+        }
+    }
+    child {
+        name Nathaniel McNair
+        gender male
+        birth -date 25-jan-1815 
+        baptism -date 29-jan-1815 -location campbeltown
+        death -date 09-jun-1880 -location 'New Mills, New Brunswick, Canada'
+        body {
+            Spouse needs investigating
+        }
+    }    
+    child {
+        name Alexander McNair
+        gender male
+        birth -date 05-may-1817
+        baptism -date 07-may-1817 -location campbeltown
+        death -date 25-dec-1893 -location 'New Mills, New Brunswick, Canada'
+        partner {
+            name Catherine Kelso
+            gender female
+            birth -date 1818
+            death -date 1890
+            marriage -date 27-jul-1841 -location 'New Mills, New Brunswick, Canada'
+        }
+    }
+    child {
+        name Jean McNair
+        gender female
+        birth -date 01-jul-1819
+        baptism -date 04-jul-1819 -location campbeltown
+        death -date 28-sep-1896 -location 'New Mills, New Brunswick, Canada'
+        partner {
+            name John McMillan
+            birth -date 1814
+            death -date 1895
+            marriage -date 03-aug-1840 -location 'New Mills, New Brunswick, Canada'
+        }
+    }
+    child {
+        name Janet McNair
+        gender female
+        birth -date 1824 -location 'New Mills, New Brunswick, Canada'
+    }
 }
 footnotes {
     marg-bapt {
@@ -34,14 +154,14 @@ footnotes {
         $opr-ref[b-1771-507-000-0030-0198 "Margaret Galbreath"]
     }
     mcnair-marriage {
-        Indexed twice one in Campbeltown, one in Saddell: $opr-ref-link[m-1802-507-000-0050-0056 "Margaret Galbreath" "Nathanel McNair"]{
+        $opr-ref-link[m-1802-507-000-0050-0056 "Margaret Galbreath" "Nathanel McNair"]{
         April 1802 | Nathaniel McNair in the Parish &
         Margaret Galbreath in Saddell Parish | 24 [day]     
-    }
-    $opr-ref-link[m-1802-531-000-0010-0065 "Margaret Galbreath" "Nathaniel McNair"]{
+        }
+        $opr-ref-link[m-1802-531-000-0010-0065 "Margaret Galbreath" "Nathaniel McNair"]{
         26 April | This day are booked with consent of parties
         in order for marriage. Nathaniel McNair of the Parish
         of [???] and Margaret Galbreath of this Parish.
-    }
+        }
     }
 }
