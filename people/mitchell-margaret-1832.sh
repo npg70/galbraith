4e7c8e23c46b92663e26b5c3d8f6e6ab860a7189
nickg <217179+client9@users.noreply.github.com>
2025-03-05 12:45:36 -0800

    add links and death

--- people/mitchell-margaret-1832.sh
+++ people/mitchell-margaret-1832.sh
@@ -2,10 +2,13 @@ name Margaret Mitchell
 gender female
 external {
     findagrave 115909878
+    familysearch 9M9S-RMN
+    wikitree Mitchell-52230
+    ancestry tree/12079504/person/282645035707
 }
 birth -date 07-feb-1832
 baptism -date 14-feb-1832 -location campbeltown -ref margaret1832-baptism
-death -date 17-sep-1868 -location campbeltown -note umn.
+death -date 17-sep-1868 -location campbeltown -note umn. -ref margaret1832-death
 note {
     Sometimes confused with $child-link[mitchell-margaret-1822-morrison]{Margaret Mitchell} who married John Morrison.
 }
@@ -13,5 +16,8 @@ note {
 footnotes {
     margaret1832-baptism {
         $opr-ref[b-1832-507-000-0060-0234 "Margaret Mitchell"]
+    }
+    margaret1832-death {
+        $sp-ref-link[d-1868-507-00-0123 0041 "Margaret Mitchell"]
     }
 }
