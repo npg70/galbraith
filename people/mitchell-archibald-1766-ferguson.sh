19086f65a0e35de19566cfcd0f3628f8689b6527
nickg <217179+client9@users.noreply.github.com>
2025-03-05 12:44:08 -0800

    add mary mitchell unconfusion

---
+++ people/mitchell-archibald-1766-ferguson.sh
@@ -0,0 +1,23 @@
+name Archibald Mitchell
+gender male
+tags Argyll:Campbeltown
+birth -date 13-nov-1766
+baptism -date 16-nov-1766 -location campbeltown
+death -date 1849 -location campbeltown
+external {
+    findagrave 171586640
+    familysearch LC7Q-DPX
+    wikitree Mitchell-21491
+}
+body { 
+    Malster and founder of Springbank Distillery.
+}
+partner {
+    name Isabella Ferguson
+    gender female
+    birth -date 22-aug-1774 
+    baptism -date 24-aug-1774 -location campbeltown
+    death -date 24-aug-1849 -location campbeltown
+
+    child mitchell-mary-1799-sheddan
+}
---
+++ people/mitchell-mary-1799-sheddan.sh
@@ -0,0 +1,45 @@
+name Mary Mitchell
+gender female
+tags Argyll:Campbeltown 'end of line'
+external {
+    findagrave 168819519
+    ancestry tree/12079504/person/282660850481
+}
+birth -date 20-sep-1799
+baptism -date 23-sep-1799 -location campbeltown -ref mary1799-baptism
+death -date 8-jun-1868 -location campbeltown -ref mary1799-death
+body {
+    Mary was the daughter of Archibald Mitchell, founder of Springbank Distillery.
+    James was a "hosier," maker of socks and things. No evidence of children.
+}
+note {
+    Historically confused with $child-link[mitchell-mary-1803-greenlees]{Mary Mitchell}, b. 1803, m. Robert Greenlees.
+}
+note {
+    Historically confused with $child-link[mcnair-archibald-1796-mitchell]{Mary Mitchell}, b. 1799, m. Archibald McNair.
+}
+partner {
+    name James Sheddan
+    gender male
+    birth -date 1797
+    death -date 4-jul-1838 -location campbeltown
+    marriage -date 16-nov-1830 -location campbeltown -ref sheddan-marriage
+}
+footnotes {
+    mary1799-baptism {
+        $opr-ref-link[b-1799-507-000-0040-0189 "Mary Mitchell"]{
+            $i{September 1799}
+            Mary | Archibald Mitchell & Isobell Ferguson in Town had a
+            Lawful daughter born 28th Baptized 23rd the childs name is Mary
+        }
+    }
+    mary1799-death {
+        $sp-ref-link[d-1868-507-00-0082 0041 "Mary Mitchell"]
+    }
+    sheddan-marriage {
+        $opr-ref-link[m-1830-507-000-0060-0297 "Mary Mitchell" "James Sheddan"]{
+            James Sheddan Stocking Weaver + Mary Mitchell both
+            of this Parish were married 16th Nov 1830
+        }
+    }   
+}
