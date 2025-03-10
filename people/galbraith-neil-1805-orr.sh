c72f1ab3e551ba4c96c096b8ac984182ccedf176
nickg <217179+client9@users.noreply.github.com>
2025-03-09 22:06:50 -0700

    fix tags, add gender

--- people/galbraith-neil-1805-orr.sh
+++ people/galbraith-neil-1805-orr.sh
@@ -1,5 +1,6 @@
 name Neil Galbraith
-tags 'South Knapdale' immigrant
+gender male
+tags 'Argyll:South Knapdale' immigrant:canada:ontario
 baptism -date 08-apr-1805 -location 'South Knapdale' -ref neil1805-baptism
 death -location 'ontario,canada'
 
@@ -10,22 +11,26 @@ external {
 
 partner {
     name Janet Orr
+    gender female
     birth -date 'about 1819' -location Scotland
     death -date 06-oct-1897 -location Carman,Manitoba,Canada
     marriage -date 'about 1805' -location Canada
     child {
         name John Galbraith
+        gender male
         birth -date nov-1856 -location grey,ontario,canada
         death -date 21-jul-1927 -location 'Duluth,St. Louis,Minnesota,USA'
         partner {
             name Emma Orr
-            birth -date apr-1862 -location ontario,cadana
+            gender female
+            birth -date apr-1862 -location ontario,Canada
             death -date 12-aug-1932 -location 'Duluth,St. Louis,Minnesota,USA'
             marriage -date 24-mar-1885 -location 'Crediton, Huron, Ontario, Canada'
         }
     }
     child {
         name Annie Galbraith
+        gender female
         birth -date 01-oct-1858 -location grey,ontario,canada
         death -date 21-jan-1944 -location manitoba,canada
         partner {
@@ -35,28 +40,34 @@ partner {
     }
     child {
         name Duncan Galbraith
+        gender male 
         birth -date 23-jun-1861 -location grey,ontario,canada
         death -date 05-aug-1935 -location manitoba,canada
         partner {
             name Mary Ellen McCannell
+            gender female
             marriage -date 29-dec-1886 -location grey,ontario,canada
         }
     }
     child {
         name Elisabeth Galbraith
+        gender female
         birth -date 23-aug-1863 -location grey,ontario,canada
         death -date 29-dec-1948 -location manitoba,canada
         partner {
             name William Herbert Waddell
+            gender male
             marriage -date 28-mar-1883 -location grey,ontario,canada
         }
     }
     child {
         name Janet Galbraith
+        gender female
         birth -date 18-may-1865 -location grey,ontario,canada
         death -date 04-may-1903 -location manitoba,canada
         partner {
             name Charles Edward Peckover
+            gender male
             marriage -date 04-jan-1888 -location grey,ontario,canada
         }
     }
