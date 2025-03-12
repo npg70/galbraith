a41bff00a86d08ec4a809b92e8662215791f6904
nickg <217179+client9@users.noreply.github.com>
2025-03-11 23:38:46 -0700

    updates

---
+++ people/harvie-john-1727-harvie.sh
@@ -0,0 +1,69 @@
+name John Harvey
+gender male
+external {
+    familysearch M1TP-B74
+}
+baptism -date 07-feb-1727 -location campbeltown -ref john1727-baptism
+partner {
+  name Janet Harvey
+  gender female
+  birth -date 'about 1730'
+  marriage -date 04-jun-1749 -location campbeltown
+
+  child {
+    name Andrew Harvey
+    gender male
+    baptism -date 26-mar-1750 -location campbeltown
+    partner {
+        name Margaret Brown
+        birth -date 1756
+        marriage -date 05-dec-1778 -location campbeltown
+    }
+    body {
+        8 children.
+     }
+   }
+   child {
+    name Mary Harvey
+    gender female
+    baptism -date jan-1752 -location campbeltown
+    death -note dy
+    }
+    child {
+        name James Harvey
+        gender male
+        baptism -date feb-1753 -location campbeltown
+        partner {
+            name Mary Orr
+            gender female
+            baptism -date 12-dec-1756 -location campbeltown
+            parent orr-edward-1720-langwill
+        }
+        body {
+            5 children
+        }
+    }
+    child {
+       name Mary Harvey
+       gender female
+       baptism -date 23-mar-1755 -location campbeltown
+    }
+    child {
+        name Archibald Harvey
+        gender male
+        baptism -date may-1757 -location campbeltown
+    }
+    child {
+        name William Harvey
+        gender male
+        baptism -date 06-sep-1764 -location campbeltown
+    }
+}
+footnotes {
+    john1727-baptism {
+        $opr-ref-link[b-1727-507-000-0011-0170 "John Harvie"]{
+            John | James Harvie & Janet Mitchell had a son
+            baptized 8th Febry named John
+        }
+    }
+}
