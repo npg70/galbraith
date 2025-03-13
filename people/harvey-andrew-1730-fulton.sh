43b2aec8cf3d7a504d9b36ad6a92d3c8a50b1130
nickg <217179+client9@users.noreply.github.com>
2025-03-12 17:53:56 -0700

    updates

---
+++ people/harvey-andrew-1730-fulton.sh
@@ -0,0 +1,101 @@
+name Andrew Harvey
+gender male
+tags argyll:campbeltown
+external {
+    familysearch KZLT-4WG
+    ancestry tree/12079504/person/282642876192
+}
+baptism -date 15-nov-1730 -location campbeltown
+
+note {
+    Not in the 1792 Census.
+}
+partner {
+    name Jean Fulton
+    gender female
+    baptism -date 01-sep-1734 -location campbeltown
+    partner {
+        name John Fulton
+        gender male
+        partner {
+            name Margaret Templeton
+            gender female
+        }
+    }
+    baptism -date 21-sep-1734 -location campbeltown
+    marriage -date 11-jun-1758 -location campbeltown
+
+    child {
+        name John Harvie
+        gender male
+        baptism -date 16-aug-1759 -location campbeltown -ref john1759-baptism
+        death -note dy
+    }
+    child {
+        name James Harvie
+        gender male
+        birth -date 05-sep-1761
+        baptism -date 06-sep-1761 -location campbeltown -ref james1761-baptism
+    }
+    child {
+        name Jean Harvie
+        gender female
+        birth -date 01-dec-1763
+        baptism -date 03-dec-1763 -location campbeltown -ref jean1763-baptism
+    }
+    child {
+        name William Harvie
+        gender male
+        baptism -date 07-feb-1765 -location campbeltown -ref william1765-baptism
+        partner {
+            name Jean Fulton
+            gender female
+            marriage -date 23-sep-1784 -location campbeltown
+        }
+    }
+    child {
+        name John Harvie
+        gender male
+        baptism -date 12-jul-1767 -location campbeltown -ref john1767-baptism
+    }
+    child {
+        name Robert Harvie
+        gender male
+        baptism -date 25-oct-1769 -location campbeltown -ref robert1769-baptism
+    }
+    child {
+        name _____ Harvie
+        gender male
+        baptism -date 17-mar-1772 -location campbeltown -ref unknown1772-baptism
+    }
+}
+footnotes {
+    john1759-baptism {
+        The two records list the baptism on the 16th and 23rd of August. Needs invesigation.
+        $opr-ref[b-1759-507-000-0011-0316 "John Harvie"]
+        $opr-ref[b-1759-507-000-0010-0244 "John Harvie"]
+    }
+    james1761-baptism {
+        $opr-ref[b-1761-507-000-0011-0326 "James Harvie"]
+        $opr-ref[b-1761-507-000-0010-0261 "James Harvey"]
+    }
+    jean1763-baptism {
+        $opr-ref[b-1763-507-000-0011-0337 "Jean Harvie"]
+        $opr-ref[b-1763-507-000-0010-0279 "Jean Harvie"]
+    }
+    william1765-baptism {
+        $opr-ref[b-1765-507-000-0011-0344 "William Harvie"]
+        $opr-ref[b-1765-507-000-0010-0290 "William Harvie"]
+    }
+    john1767-baptism {
+        $opr-ref-link[b-1767-507-000-0011-0359 "John Harvie"]
+        $opr-ref[b-1767-507-000-0010-0317 "John Harvie"]
+    }
+    robert1769-baptism {
+        $opr-ref[b-1769-507-000-0011-0372 "Robert Harvie"]
+        $opr-ref[b-1769-507-000-0010-0336 "Robert Harvie"]
+    }
+    unknown1772-baptism {
+        $opr-ref[b-1772-507-000-0011-0382 "_____ Harvie"]
+    }
+}
--- people/orr-edward-1720-langwill.sh
+++ people/orr-edward-1720-langwill.sh
@@ -26,7 +26,7 @@ partner {
     child {
         name Agnes Orr
         gender female
-        baptism -date 20-nov-1745 -location campbeltown
+        baptism -date 20-nov-1745 -location campbeltown -ref agnes1745-baptism
         partner harvey-william-1738-orr
         body {
             maybe in correct Agnes who married William
@@ -34,40 +34,45 @@ partner {
     }
     child {
         name Janet Orr
-        baptism -date 23-aug-1747 -location campbeltown
+        baptism -date 23-aug-1747 -location campbeltown -ref janet1747-baptism
+    }
+    child {
+        name Isobell Orr
+        gender female
+        baptism -date 08-dec-1749 -location campbeltown -ref isobell1749-baptism
     }
     child {
         name John Orr
-        baptism -date 25-mar-1752 -location campbeltown
+        baptism -date 25-mar-1752 -location campbeltown -ref john1752-baptism
     }
     child {
         name Margaret Orr
         gender female
-        baptism -date 31-mar-1754 -location campbeltown
+        baptism -date 31-mar-1754 -location campbeltown -ref margaret1754-baptism
         partner harvey-andrew-1749-orr
     }
     child {
         name Mary Orr
         gender female
-        baptism -date 12-dec-1756  -location campbeltown
+        baptism -date 12-dec-1756  -location campbeltown -ref mary1756-baptism
         partner harvey-james-1753-orr
     }
     child {
         name Peter Orr
         gender male
-        baptism -date 07-apr-1759 -location campbeltown
+        baptism -date 07-apr-1759 -location campbeltown -ref peter1759-baptism
     }
     child {
         name Martha Orr
         gender female
         birth -date 15-jul-1761
-        baptism -date 19-jul-1761 -location campbeltown
+        baptism -date 19-jul-1761 -location campbeltown -ref martha1761-baptism
     }
     child {
         name Ann Orr
         gender female
         birth -date 21-mar-1764 
-        baptism -date 22-mar-1764 -location campbeltown
+        baptism -date 22-mar-1764 -location campbeltown -ref ann1764-baptism
         death -date 20-aug-1842 -location campbeltown
         partner {
             name Andrew Ralston
@@ -81,21 +86,55 @@ partner {
         }
     }
     child {
-        name Isabell Orr
-        gender female
-        baptism -date 08-dec-1749 -location campbeltown
-    }
-    child {
         name Edward Orr
         gender male
         birth -date 10-mar-1767
-        baptism -date 12-mar-1767 -location campbeltown
+        baptism -date 12-mar-1767 -location campbeltown -ref edward1767-baptism
         body {
             maybe married Isabella Johnston, 3 kids 1807-1809
         }
     }
 }
 footnotes {
-
-
+    agnes1745-baptism {
+        $opr-ref-link[b-1745-507-000-0011-0253 "Agnes Orr"]
+        $opr-ref[b-1745-507-000-0010-0182 "Agnes Orr"]
+    }
+    janet1747-baptism {
+        Mother indexed under "Isobel Languill"
+        $opr-ref[b-1747-507-000-0010-0188 "Janet Orr"]
+    }
+    isobell1749-baptism {
+        $opr-ref[b-1749-507-000-0011-0270 "Isobell Orr"]
+        $opr-ref[b-1749-507-000-0010-0197 "Isobell Orr"]
+    }
+    john1752-baptism {
+        $opr-ref[b-1752-507-000-0011-0281 "John Orr"]
+        $opr-ref[b-1752-507-000-0010-0208 "John Orr"]
+    }
+    margaret1754-baptism {
+        $opr-ref-link[b-1754-507-000-0011-0290 "Margaret Orr"]
+        $opr-ref[b-1754-507-000-0010-216 "Margaret Orr"]
+    }
+    mary1756-baptism {
+        $opr-ref-link[b-1756-507-000-0011-0303 "Mary Orr"]
+        $opr-ref[b-1756-507-000-0010-0231 "Mary Orr"]
+    }
+    peter1759-baptism {
+        $opr-ref[b-1759-507-000-0011-0314 "Peter Orr"]
+        $opr-ref[b-1759-507-000-0010-0243 "Peter Orr"]
+    }
+    martha1761-baptism {
+        Mother indexed as "Isobel Langville"
+        $opr-ref[b-1761-507-000-0011-0326 "Martha Orr"]
+        $opr-ref[b-1761-507-000-0010-0260 "Martha Orr"]
+    }
+    ann1764-baptism {
+        $opr-ref[b-1764-507-000-0011-0339 "Ann Orr"]
+        $opr-ref[b-1764-507-000-0010-0283 "Ann Orr"]
+    }
+    edward1767-baptism {
+        $opr-ref[b-1767-507-000-0011-0357 "Edward Orr"]
+        $opr-ref[b-1767-507-000-0010-0314 "Edward Orr"]
+    }
 }
