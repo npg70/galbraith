97156aec2b0d8fca16ade8201fd00158d94e22de
nickg <217179+client9@users.noreply.github.com>
2025-03-13 23:33:26 -0700

    more harvey

--- people/harvey-william-1769-alexander.sh
+++ people/harvey-william-1769-alexander.sh
@@ -1,27 +1,31 @@
 name William Harvey
 gender male
-tags Argyll:Campbeltown
+tags Argyll:Campbeltown tags Argyll:Dalintober
 
 baptism -date 05-nov-1769 -location campbeltown -ref william1769-baptism
 death -date 1845
 external {
     familysearch K4BK-36S
 }
-
+body {
+    He was a weaver in Dalintober.
+}
 partner {
    name Elizabeth Alexander
    gender female
-   marriage -date 08-nov-1793 -location campbeltown
-    body {
+   marriage -date 08-nov-1793 -location campbeltown -ref alexander-marriage
+   body {
         Parents unknown.
-    }
+   }
    child {
-    name Margaret Harvey
-    gender female
+        name Margaret Harvey
+        gender female
+        baptism -date 22-may-1795 -location campbeltown -ref margaret1795-baptism
    }
    child {
         name Elizabeth Harvey
         gender female
+        baptism -date 05-mar-1797 -location campbeltown -ref elizabeth1797-baptism
         death -note dy
    }
 
@@ -30,10 +34,14 @@ partner {
     child {
         name Elizabeth Harvey
         gender female
+        birth -date 15-dec-1800
+        baptism -date 17-dec-1800 -location campbeltown -ref elizabeth1800-baptism
     }
     child {
         name Agnes Harvey
         gender female
+        birth -date 20-jan-1803
+        baptism -date 26-jan-1803 -location campbeltown -ref agnes1803-baptism
     }
     child {
         name Thomas Harvey
@@ -53,6 +61,8 @@ partner {
     child {
         name Jean Harvey
         gender female
+        birth -date 04-jun-1807
+        baptism -date 05-jun-1807 -location campbeltown -ref jean1807-baptism
         death -note dy
     }
    child harvey-james-1808-clark
@@ -60,18 +70,23 @@ partner {
    child {
         name Janet Harvey
         gender female
+        baptism -date 17-aug-1810 -location campbeltown -ref jean1810-baptism
    }
    child {
         name Jean Harvey
         gender female
+        baptism -date 17-aug-1810 -location campbeltown -ref jean1810-baptism
     }
     child {
         name Agnes Harvey
         gender female
+        baptism -date 04-sep-1814 -location campbeltoen -ref agnes1814-baptism
     }
     child {
         name Susan Harvey
         gender female
+        birth -date 31-oct-1818
+        baptism -date 04-nov-1818 -location campbeltown -ref susan1818-baptism
     }
 }
 footnotes {
@@ -79,7 +94,53 @@ footnotes {
         $opr-ref[b-1769-507-000-0011-0372 "William Harvie"]
         $opr-ref[b-1769-507-000-0010-0336 "William Harvey"]
     }
+    alexander-marriage {
+        $opr-ref-link[m-1793-507-000-0050-0034 "William Harvy" "Elizabeth Alexander"]
+    }
+    margaret1795-baptism {  
+        $opr-ref[b-1795-507-000-0040-0127 "Margaret Harvy"]
+    }
+    elizabeth1797-baptism {
+        $opr-ref[b-1797-507-000-0040-0147 "Elizabeth Harviy"]
+    }
+    elizabeth1800-baptism {
+        $opr-ref-link[b-1800-507-000-0040-0209 "Elizabeth Harvey"]{
+            Elizabeth | William Harvey & Eliz. Alexander in Dalintober had
+            a lawful Daur born 15th baptized 17th named Elizabeth
+        }
+    }
+    agnes1803-baptism {
+        $opr-ref-link[b-1803-507-000-0040-0243 "Agnes Harvie"]{
+            Agnes | Lawfull Daughter to William Harvie Weaver Dalintober
+            and Elizabeth Alexander. Born 20th Baptized 26 January 1803.
+            Named Agnes
+        }
+    }
+    jean1807-baptism {
+        $opr-ref-link[b-1807-507-000-0040-0304 "Jean Harvie"]{
+            Jean | Lawful Daughter to William Harvie Weaver Dalintober
+            and Elizabeth Alexander. Born 4 baptized 5 June 1807.
+            Named Jean
+        }
+    }
+    jean1810-baptism {
+        $opr-ref-link[b-1810-507-000-0040-0356 "Janet + Jean Harvey"]{
+            Janet + Jean | Lawful twin Daughters to William Harvie Weaver Dalintober
+            and Elizabeth Alexander. Born 16th
+            baptized 17th August 1810. Janet + Jean
+        }
+    }
     thomas1857-death {
         $sp-ref-link[d-1857-507-00-0139 0047 "Thomas Harvey"]
+    }
+    agnes1814-baptism {
+        $opr-ref[b-1814-507-000-0040-0418 "Agnes Harvey"]
+    }
+    susan1818-baptism {
+        $opr-ref-link[b-1818-507-000-0040-0482 "Susan Harvie"]{
+            Susan | Lawful Daughter to William Harvie Weaver Dalintober
+            and Elizabeth Alexander. Born 31st Octi
+            baptized 4th November 1818 Named Susan
+        }
     }
 }
--- people/harvey-william-1799-greenlees.sh
+++ people/harvey-william-1799-greenlees.sh
@@ -1,9 +1,20 @@
 name William Harvey
 gender male
 tags 'Argyll:Campbeltown' immigrant:usa:illinois:boone
+external {
+    familysearch K4RC-26Q
+    wikitree Harvie-840
+    findagrave 53401630
+}
 birth -date 04-jan-1799 
-baptism -date 06-jan-1799 -location campbeltown
+baptism -date 06-jan-1799 -location campbeltown -ref william1799-baptism
 death -date 22-jun-1872 -location caledonia,boone,illinois,usa
+body {
+    He was a tanner.
+}
+body {
+    Immigrations before 17-jun-1841.  It appears they missed the Census of 1841
+}
 partner {
     name Mary Greenlees
     gender female
@@ -13,6 +24,17 @@ partner {
     marriage -date 03-jul-1827 -location campbeltown
     body {
          She was the daughter of John Greenlees and Anne Ferguson.
+    }
+    child {
+        name William Harvey
+        gender male
+        baptism -date 14-apr-1828 -location campbeltown
+        death -date 23-nov-1909 -location ' Rockford, Winnebago County, Illinois,usa'
+        partner {
+            name Elizabeth Harvie
+            gender female
+            marriage -date 09-aug-1870 -location southend
+        }
     }
     child {
         name Matthew Harvey
@@ -31,11 +53,38 @@ partner {
         }
     }
     child {
-        name Daniel G Harvery
+        name Robert Harvey
         gender male
+        baptism -date 19-oct-1837 -location campbeltown -ref robert1837-baptism
+    }
+    child {
+        name Margaret Harvey
+        gender female
+        baptism -date 22-mar-1840 -location campbeltown -ref margaret1840-baptism
+    }
+    child {
+        name Daniel Greenlee Harvery
+        gender male
+        baptism -date 25-oct-1850 -location argyle,winnebago,illinois,usa
+        death -date 12-feb-1939 -location rockford,winnebago,illinois,usa
         body {
             Author of $source-link[argyle-settlement]{The Argyle Settlement in History and Story}.
         }
     }
 }
+footnotes {
+    william1799-baptism {
+        $opr-ref[b-1799-507-000-0040-0178 "William Harvie"]
+    }
+    robert1837-baptism {
+        $opr-ref-link[b-1837-507-000-0070-0112 "Robert Harvey"]{
 
+        }
+    }
+    margaret1840-baptism {
+        $opr-ref-link[b-1840-507-000-0070-0169 "Margaret Harvey"]{
+            
+
+        }
+    }
+}
