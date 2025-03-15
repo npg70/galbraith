131a40c1d18ac93a00a82de2dc6fca5bd1e885b0
nickg <217179+client9@users.noreply.github.com>
2025-03-15 16:45:45 -0700

    clean up, finish up

--- people/galbraith-callum-1896.sh
+++ people/galbraith-callum-1896.sh
@@ -10,18 +10,23 @@ external {
     findagrave 99513883
 }
 birth -date 17-aug-1896 -location kilbery -ref malcolm1896-birth
-death -date 24-oct-1916 -location 'Cork Military Hositpial,Dublin,Ireland'
-burial -location 'dublin,ireland'
+death -date 24-oct-1918 -location 'Cork Military Hositpial,Dublin,Ireland'
    
-todo major {
-    finish this up.  has two legit findagrave markers
+body {
+    His death is recored by $elink[https://ww.cwgc.org/]{Commonwealth War Graves Commission} under  "$elink[https://www.cwgc.org/find-records/find-war-dead/casualty-details/900085/malcolm-galbraith/]{Malcolm Galbraith}", record 900085.
 }
 body {
-    https://www.cwgc.org/find-records/find-war-dead/casualty-details/900085/malcolm-galbraith/
+    Callum has two burial markers. The first is the millitary one in Dublin$ref[burial1], and the other family marker in Gigha$ref[burial2].
 }
 
 footnotes {
     malcolm1896-birth {
         $sp-ref-link[b-1896-533-02-0004 0002  "Malcolm Galbraith"] 
+    }
+    burial1 {
+    Find a Grave, database and images (https://www.findagrave.com/memorial/99513883/callum-galbraith: accessed March 15, 2025), memorial page for Priv Callum Galbraith (unknown–24 Oct 1918), Find a Grave Memorial ID 99513883, citing Grangegorman Cork Memorial Headstones, Cabra, County Dublin, Ireland; Maintained by John (contributor 47032041).
+    }
+    burial2 {
+        Find a Grave, database and images (https://www.findagrave.com/memorial/115118805/callum-galbraith: accessed March 15, 2025), memorial page for Pte Callum Galbraith (1896–24 Oct 1918), Find a Grave Memorial ID 115118805, citing Kilchattan Old Graveyard, Isle of Gigha, Argyll and Bute, Scotland; Maintained by Chips (contributor 47271788).
     }   
 }
