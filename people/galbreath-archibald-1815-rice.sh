86ce0baefde08ae25f37f52afa6b4339ba624ec8
nickg <217179+client9@users.noreply.github.com>
2025-03-27 09:04:13 -0700

    extend line

--- people/galbreath-archibald-1815-rice.sh
+++ people/galbreath-archibald-1815-rice.sh
@@ -1,14 +1,87 @@
 name Archibald Galbreath
 gender male
-tags Argyll:Campbeltown Immigrant:USA:Ohio
-todo major { Stub }
+tags Argyll:Campbeltown Immigrant:USA:Ohio Argyll:Peninver
+external {
+    familysearch KZV2-6RM
+    findagrave 255430338
+}
 birth -date 19-feb-1815 -location peninver
 baptism -date 22-feb-1815 -location campbeltown -ref archibald1815-baptism
-death -location 'Point Pleasant,Clermont,Ohio,USA'
+death -location -date 20-feb-1876 'Point Pleasant,Clermont,Ohio,USA'
 partner {
     name Eliza Rice
     gender female
     marriage -date 13-Jul-1848 -location Mason,Kentucky,USA
+
+    child {
+        name Marion Galbreath
+        gender female
+        birth -date 09-nov-1850 -location 'point pleasant,clermont,ohio,usa'
+
+        death -date 23-dec-1853
+    }
+    child galbreath-samuel-newton-1853-johnson
+    child {
+        name Nancy Galbreath
+        gender female
+        birth -date 'about 1854' -location 'point pleasant,clermont,ohio,usa'
+
+        death -date 'about 1890'
+    }
+    child {
+        name Lousia Galbreath
+        gender female
+        birth -date 11-feb-1856 -location 'point pleasant,clermont,ohio,usa'
+
+        death -date 17-mar-1882
+    }
+    child {
+        name Douglas David Galbreath
+        gender male
+        birth -date 21-oct-1857 -location 'point pleasant,clermont,ohio,usa'
+
+        death -date 31-dec-1876
+        body {
+            Last seen in the 1870 Census.  No records of marriage or children.
+        }
+    }
+    child {
+        name Charles Edward Galbreath
+        gender male
+        birth -date 23-mar-1859 -location 'point pleasant,clermont,ohio,usa'
+
+        body {
+            Last seen in the 1880 Census. 
+        }
+    }
+    child {
+        name William Harvey Galbreath
+        gender male
+        birth -date 21-dec-1861 -location 'point pleasant,clermont,ohio,usa'
+
+        death -date 24-sep-1884
+    }
+    child {
+        name Laura Emily Galbreath
+        gender female
+        birth -date 17-Oct-1863 -location 'point pleasant,clermont,ohio,usa'
+
+        death -date 7-Oct-1895
+    }
+    child {
+        name Robert Archibald Galbreath
+        gender male
+        birth -date 16-jan-1865 -location 'point pleasant,clermont,ohio,usa'
+
+        death -date 11-nov-1934 -note umn
+    }
+    child {
+        name Anna Eliza Galbreath
+        gender female
+        birth -date 14-dec-1867 -location 'point pleasant,clermont,ohio,usa'
+
+        death -date 26-aug-1901
+    }
 }
 footnotes {
     archibald1815-baptism {
---
+++ people/galbreath-samuel-newton-1853-johnson.sh
@@ -0,0 +1,61 @@
+name Samuel Newton Galbreath
+gender male
+tags USA:Ohio:Clermont
+external {
+    familysearch LVXX-S1R
+    findagrave 133958480
+}
+birth -date 20-may-1852 -location 'point pleasant,clermont,ohio,usa'
+death -date 13-oct-1918 -location 'point pleasant,clermont,ohio,usa'
+partner {
+   name Jennie Holmes Johnson
+   gender female
+   birth -date 11-dec-1861 -location Jackson,Indiana,USA
+   death -date 29-feb-1960 -location Cincinnati,Hamiliton,Ohio,USA
+   marriage -date 12-sep-1888 -location 'Columbus, Bartholomew, Indiana, USA'
+   parent {
+      name James Johnson
+      gender male
+      partner {
+         name Adaline V Benton
+         gender female
+      }
+  }
+  child {
+    name _____ Galbreath
+    gender male
+    birth -date 1889 -location 'point pleasant,clermont,ohio,usa'
+    death -date 02-mar-1890 
+  }
+  child {
+    name Ethel Mae Galbreath
+    gender female
+    birth -date 01-may-1892 -location 'point pleasant,clermont,ohio,usa'
+    death -date 17-oct-1976 -location 'cincinnati,hamiliton,ohio,usa' -note umn
+  }
+  child {
+    name Lawrence Benton Galbreath
+    gender male
+    birth -date 09-oct-1895 -location 'point pleasant,clermont,ohio,usa'
+    death -date 17-may-1973 -location 'cincinnati,hamiliton,ohio,usa' -note umn
+  }
+  child {
+    name Zella Viola Galbreath
+    gender female
+    birth -date 29-dec-1898 -location 'point pleasant,clermont,ohio,usa'
+    death -date 25-may-1983 -location 'cincinnati,hamiliton,ohio,usa'
+    partner {
+        name Robert Calvin Dunbar
+        gender male
+        birth -date 1903
+        death -date 1981
+        marriage -date 1928
+    }
+  }
+  child {
+    name Frank Hubert Galbreath
+    gender male
+    birth -date 13-dec-1900 -location 'point pleasant,clermont,ohio,usa'
+    death -date 18-sep-1958 -location 'detroit,wayne,michigan,usa'
+  }
+}
