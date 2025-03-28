4e74fd9ecb35ac207dc6361c7291561bc2c8c892
nickg <217179+client9@users.noreply.github.com>
2025-03-28 12:25:04 -0700

    cleanups

--- people/galbreath-archibald-1842-omay.sh
+++ people/galbreath-archibald-1842-omay.sh
@@ -1,4 +1,5 @@
 name Archibald Galbreath
+gender male
 tags Argyll:Campbeltown
 external {
     familysearch LTZN-9KC
@@ -8,30 +9,43 @@ birth -date 25-aug-1842
 baptism -date 25-sep-1842 -location campbeltown -ref archibald1842-baptism
 death -date 22-sep-1896 -location campbeltown -ref archibald1842-death
 body {
-    His occupation was in the distilling industry.
+    He worked in the distilling industry.
 }
+
 todo census { Present in 1851 Census }
 
 partner {
     name Agnes "O'May"
-    body { She was the daughter of William O'May and Margaret Milloy. }
+    gender female
+    parent {
+        name William "O'May"
+        gender male
+        partner {
+            name Margaret Milloy
+            gender female
+        }
+    }
     birth -date 'about 1845'
     marriage -date 8-dec-1869 -location campbeltown -ref omay-marriage
     death -date 05-aug-1901 -location campbeltown -ref omay-death
+
     child galbraith-william-1870-mcmillan
 
     child galbraith-john-china-1871
 
     child {
         name Mary Galbraith
+        gender female
         birth -date 16-jan-1874 -location campbeltown -ref mary1874-birth
     }
     child {
         name Archibald Galbraith
+        gender male
         birth -date 1-apr-1881 -location campbeltown -ref archibald1881-birth
     }
     child {
         name Agnes Leoran Galbraith
+        gender female
         birth -date 10-jan-1883 -location campbeltown -ref agnes1883-birth
     }
 }
@@ -40,7 +54,7 @@ footnotes {
         $opr-ref-link[b-1842-507-000-0070-0226 "Archibald Galbreth"]
     }
     archibald1842-death {
-        $sp-ref-link[d-1896-507-00-0109 0037 "Archibald Galbraith"]`
+        $sp-ref-link[d-1896-507-00-0109 0037 "Archibald Galbraith"]
     }
     omay-marriage {
         $sp-ref-link[m-1869-507-00-0041 0021 "Archibald Galbraith" "Agnes O'May"]
