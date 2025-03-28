c01a5df1da367e5db946aaa5dad0c76527f37660
nickg <217179+client9@users.noreply.github.com>
2025-03-28 12:07:03 -0700

    more detail on death

--- people/galbraith-archibald-1845-carr.sh
+++ people/galbraith-archibald-1845-carr.sh
@@ -2,7 +2,7 @@ name Archibald Galbreath
 gender male
 tags Argyll:Campbeltown
 baptism -date 6-oct-1845 -location campbeltown -ref archibald1845-baptism
-death -date 1916 -location 'Newton Abbot, Devon, England'
+death -date 02-nov-1916 -location 'Newton Abbot, Devon, England'
 external {
     familysearch M1YS-Y2Q
     findagrave 177759917
@@ -10,32 +10,76 @@ external {
 partner {
     name Florence Eliza Carr
     gender female
-    marriage -date 1883 -location 'Chorlton, Lancashire, England'
+    birth -date 'about 1862'
+    marriage -date 1883 -location 'Chorlton, Lancashire, England' -ref carr-marriage
 }
-todo major {
-   Body needs rewrite.  See orig https://github.com/npg70/galbreath-old/blob/main/people/galbreath-archibald-1845-carr.md
+note {
+    No evidence of children.
 }
-
 body {
-1861 At boarding school Merchiston Academy, Colinton Road, St Cuthbert's, Newington, Midlothian, Scotland https://www.findmypast.com/transcript?id=GBC%2F1861%2F0024162490
+Timeline:
+$ul{
+$li{
+1861 At boarding school Merchiston Academy, Colinton Road, St Cuthbert's, Newington, Midlothian, Scotland$ref[census-1861]
+}
+$li{
+1871 At Brook Field, Lymm, Altrincham, Cheshire, England$ref[census-1871]
+}
+$li{
+1881 At Catterick Hall, Fog Lane, Burnage, Chorlton, Lancashire, England$ref[census-1881]
+}
+$li{
+1883 Marriage to Florence Eliza Carr @ Chorlton, Lancashire, England $ref[carr-marriage]
+}
+$li{
+1891 At Catterick Hall, Fog Lane, Didsbury, Chorlton, Lancashire, England$ref[census-1891]
+}
+$li{
+    1901 At Catterick, The Park, Buxton, Fairfield, Chapel En Le Frith, Derbyshire, England$ref[census-1901]
+}
+$li{
+1911 - no records found
+}
+$li{
+1916 - Death at Newton Abbot, Devon, England,$ref[archibald1845-death]
+}
+}
 
-1871 Brook Field, Lymm, Altrincham, Cheshire, England https://www.findmypast.com/transcript?id=GBC/1871/0018410110&expand=true
-
-1881 Census Catterick Hall, Fog Lane, Burnage, Chorlton, Lancashire, England - https://www.findmypast.com/transcript?id=GBC/1881/0017950203&expand=true
-
-1883 Marriage to Florence Eliza Carr @ Chorlton, Lancashire, England https://www.findmypast.com/transcript?id=BMD%2FM%2F1884%2F1%2FAZ%2F000088%2F024
-
-1891 Census Catterick Hall, Fog Lane, Didsbury, Chorlton, Lancashire, England - https://www.findmypast.com/transcript?id=GBC/1891/0021738500&expand=true Married to Florence E ~ 1862
-
-1901 Census Catterick, The Park, Buxton, Fairfield, Chapel En Le Frith, Derbyshire, England https://www.findmypast.com/transcript?id=GBC%2F1901%2F0019570529
-
-1911 - no
-
-1916 - Death https://www.findmypast.com/transcript?id=BMD%2FD%2F1916%2F4%2FAZ%2F000369%2F083 Newton Abbot, Devon, England
+$p{
+Death notice in Campbeltown Courier, 18-Nov-1916$ref[archibald1845-death2]
+$blockquote{
+$b{Deaths} Suddenly, at Torquay, on the 2nd inst., Archibald Galbraith (Manchester), - son of the late John Galbraith and Mrs Galbraith, North Park, Campbeltown.
+}
+}
 }
 
 footnotes {
     archibald1845-baptism {
         $opr-ref-link[b-1845-507-000-0070-0292 "Archibald Galbreath"]
+    }
+    archibald1845-death {
+        https://www.findmypast.com/transcript?id=BMD%2FD%2F1916%2F4%2FAZ%2F000369%2F083
+    }
+    archibald1845-death2 {
+        https://www.britishnewspaperarchive.co.uk/viewer/bl/0005086/19161118/037/0002
+        Campbeltown Courier - Saturday 18 November 1916
+    }
+    carr-marriage {
+        https://www.findmypast.com/transcript?id=BMD%2FM%2F1884%2F1%2FAZ%2F000088%2F024
+    }
+    census-1861 {
+        https://www.findmypast.com/transcript?id=GBC%2F1861%2F0024162490
+    }
+    census-1871 {
+        https://www.findmypast.com/transcript?id=GBC/1871/0018410110&expand=true
+    }
+    census-1881 {
+       https://www.findmypast.com/transcript?id=GBC/1881/0017950203&expand=true
+    }
+    census-1891 {
+        https://www.findmypast.com/transcript?id=GBC/1891/0021738500&expand=true
+    }
+    census-1901 {
+        https://www.findmypast.com/transcript?id=GBC%2F1901%2F0019570529
     }
 }
