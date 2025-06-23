b67add577056298a5a7709bf9fe1386322183071
Nick Galbreath <client9@users.noreply.github.com>
2025-06-23 22:19:05 +0100

    remove unusued

--- roots.go
+++ roots.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"log"
-	"maps"
 	"path/filepath"
-	"slices"
 	"sort"
 	"strings"
 
@@ -202,102 +199,9 @@ $table{
 	return page
 }
 
-func indexRoots2(db Root, tpage tagpage, outputFile string) ssg.ContentSourceConfig {
-	fulltag := strings.Join(tpage.path, " / ")
-
-	out := bytes.Buffer{}
-	out.WriteString("$h1{" + fulltag + "}")
-	for _, kid := range tpage.tags {
-		out.WriteString(fmt.Sprintf("$tag-link[%s]{%s}", strings.Join(tpage.path, "/"), kid))
-	}
-	out.WriteString("$hr{}\n")
-	for _, r := range tpage.uids {
-		p := db[r]
-		out.WriteString(fmt.Sprintf("$h5{$a[href=/galbraith/people/%s]{%s}}\n",
-			r, WriteTitle(p)))
-		if len(p.Tags) > 0 {
-			out.WriteString("$div[class='ms-3 mb-3']{")
-			for _, tag := range p.Tags {
-				out.WriteString(fmt.Sprintf("$tag-link[%s]{%s}", strings.Join(tpage.path, "/"), tag))
-			}
-			out.WriteString("}\n")
-		}
-		if len(p.Intro) > 0 {
-			out.WriteString("$intro{" + p.Intro + "}\n")
-		}
-	}
-	page := make(ssg.ContentSourceConfig)
-	page["OutputFile"] = outputFile
-	page["TemplateName"] = "tags/baseof.html"
-	page["title"] = "People Tag " + fulltag
-	page["Content"] = out.Bytes()
-	return page
-}
-
-type tagpage struct {
-	path []string
-	uids []string
-	tags []string
-}
-
 // Current Path
 // All matching childen
 // Tags for next path
-
-func tagmap2(db Root, page tagpage, depth int) []tagpage {
-	if depth == 4 {
-		return nil
-	}
-	// this is a map of TAG --> USERID
-	tmap := make(map[string][]string)
-	for _, uid := range page.uids {
-		// for each tag the user has
-		for _, tag := range db[uid].Tags {
-
-			// split into parts
-			tag = strings.ToLower(tag)
-			tp := strings.Split(tag, ":")
-
-			// foo, foo:bar, foo:bar:ding
-			for i := 1; i <= len(tp); i++ {
-				tg := strings.Join(tp[0:i], ":")
-				// log.Printf("trying: path=%q, tag=%q --> %v", page.path, tg, inLabelPath(page.path, tg))
-				if !inLabelPath(page.path, tg) {
-					tmap[tg] = append(tmap[tg], uid)
-				}
-			}
-		}
-	}
-	children := slices.Sorted(maps.Keys(tmap))
-	page.tags = children
-	result := []tagpage{page}
-
-	// we now have a set of all tags used with given UIDs
-	for tag, newuids := range tmap {
-		next := tagpage{
-			path: append(page.path, tag),
-			uids: newuids,
-		}
-		result = append(result, tagmap2(db, next, depth+1)...)
-	}
-	return result
-}
-
-func tagStart(db Root) []tagpage {
-
-	// all is all people
-	all := []string{}
-	for uid := range db {
-		all = append(all, uid)
-	}
-	sort.Strings(all)
-
-	next := tagpage{
-		path: []string{"all"},
-		uids: all,
-	}
-	return tagmap2(db, next, 0)
-}
 
 func tagmap(db Root) map[string][]string {
 
