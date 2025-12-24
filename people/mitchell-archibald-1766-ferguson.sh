d2ad4c9bbaca94f765478e476ffde731d1ae4e5d
Nick Galbreath <client9@users.noreply.github.com>
2025-12-24 17:40:24 +0900

    update

--- people/mitchell-archibald-1766-ferguson.sh
+++ people/mitchell-archibald-1766-ferguson.sh
@@ -15,9 +15,117 @@ body {
 partner {
     name Isabella Ferguson
     gender female
+	parent {
+		name Hugh Ferguson
+		gender male
+		partner {
+			name Mary Mitchell
+			gender female
+		}
+	}
     birth -date 22-aug-1774 
     baptism -date 24-aug-1774 -location campbeltown
     death -date 24-aug-1849 -location campbeltown
 
     child mitchell-mary-1799-sheddan
+
+	child {
+		name Archibald Mitchell
+		gender male
+		birth -date 17-mar-1803
+		baptism -date 20-mar-1803 -location campbeltown -ref archibald1803-baptism
+		death -date 02-mar-1863 -location campbeltown -note umn
+	}
+	child {
+		name James Mitchell
+		gender male
+		birth -date 20-mar-1805
+		baptism -date 02-apr-1805 -location campbeltown -ref james1805-baptism
+	}
+	child {
+		name Jean Mitchell
+		gender female
+		birth -date 27-mar-1807
+		baptism -date 29-mar-1807 -location campbeltown -ref jean1807-baptism
+		death -note dy
+	}
+	
+	child {
+		name Jean Mitchell
+		gender female
+		baptism -date 09-apr-1809 -location campbeltown -ref jean1809-baptism
+		death -date 30-may-1880 -location campbeltown
+		partner {
+			name William McKenzie
+			gender male
+			birth -date 1806
+			death -date 1878
+		}
+	}
+	child {
+		name John Mitchell
+		gender male
+		birth -date 04-aug-1811
+		baptism -date 07-aug-1811 -location campbeltown -ref john1811-baptism
+		death -date 11-may-1892 -location campbeltown
+		partner {
+			name Mary Wylie
+			gender female
+			birth -date 1818
+			death -date 1913
+		}
+	}
+	child {
+		name Isabell Mitchell
+		gender female
+		birth -date 12-jun-1814
+		baptism -date 15-jun-1814 -location campbeltown -ref isabell1814-baptism
+		death -date 18-apr-1892
+		partner {
+			name James Campbell
+			gender male
+			birth -date 1815
+			death -date 1894
+		}
+	}
+	child {
+		name William Mitchell
+		gender male
+		birth -date 11-apr-1819
+		baptism -date 14-apr-1819 -location campbeltown -ref william1819-baptism
+		death -date 24-aug-1887 -location paisley,renfrew
+		partner {
+			name Helen Greenlees
+			gender female
+			baptism -date 16-jan-1823 -location campbeltown
+			death -date 07-jan-1902 -location campbeltown
+		}
+	}
+}
+footnotes {
+	archibald1803-baptism {
+		$opr-ref[b-1803-507-000-0040-0245 "Archibald Mitchell"]
+	}
+	james1805-baptism {
+		$opr-ref[b-1805-507-000-0040-0274 "James Mitchell"]
+	}
+	jean1807-baptism {
+		$opr-ref[b-1807-507-000-0040-00300 "Jean Mitchell"]
+	}
+	jean1809-baptism {
+		$opr-ref-link[b-1809-507-000-0040-0333 "Jean Mitchell"]{
+			Jean | Lawful Daughter to Archibald Mitchell Malster in Campbeltown
+			& Isobell Ferguson born 6th baptized 9th April 1809. Named Jean
+		}
+	}
+	john1811-baptism {
+		$opr-ref[b-1811-507-000-0040-0372 "John Mitchell"]
+	}
+	isabell1814-baptism {
+		$opr-ref[b-1814-507-000-0040-0415 "Isabell Mitchell"]
+	}
+	william1819-baptism {
+		Father listed as "ARCHD." not "ARCHIBALD"
+		$opr-ref[b-1819-507-000-0040-0490 "William Mitchell"]
+	}
 }
